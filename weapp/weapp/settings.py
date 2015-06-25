d4136d31a8456ed11c9493e61e3f41bc41e139fc
jianzhe <jiangzhe@weizoom.com>
2015-06-25 11:40:14 +0000

    恢复setting文件
    git-svn-id: http://s11/svn/weapp/web/trunk_2.0@15512 152f85a6-bef7-4f42-898c-89626e871b9b

--- weapp/weapp/settings.py
+++ weapp/weapp/settings.py
@@ -54,7 +54,7 @@ DATABASES = {
         'USER': 'weapp',                      # Not used with sqlite3.
         'PASSWORD': 'weizoom',                  # Not used with sqlite3.
         # Set to empty string for localhost. Not used with sqlite3.
-        'HOST': 'db.weizoom.com',
+        'HOST': 'db.weapp.com',
         # Set to empty string for default. Not used with sqlite3.
         'PORT': '',
         'CONN_MAX_AGE': 100
