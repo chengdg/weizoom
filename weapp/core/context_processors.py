eb0a6d731f19e53982d44cec19f8434c40cbd817
ycguo <guoyucheng@weizoom.com>
2015-06-26 02:04:32 +0000

    [update]: *修改bug http://weappop.weizoom.com/watchdog/show/4439413/
    git-svn-id: http://s11/svn/weapp/web/trunk_2.0@15523 152f85a6-bef7-4f42-898c-89626e871b9b

--- weapp/core/context_processors.py
+++ weapp/core/context_processors.py
@@ -385,7 +385,7 @@ def homepage_workspace_info(request):
 	if not request.user_profile:
 		return {}
 		
-	return {'homepage_workspace_info': 'workspace_id=%s&project_id=0' % request.user_profile.homepage_workspace_id}
+	return {'homepage_workspace_info': 'workspace_id=%s&project_id=0&webapp_owner_id=%s' % (request.user_profile.homepage_workspace_id, request.webapp_owner_id)}
 		
 #===============================================================================
 # page_help_document ： 获取页面帮助文档
