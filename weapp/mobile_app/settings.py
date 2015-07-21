954e4d429a8eafc5d684bdee84826e2ae4cc5a3e
likunlun <likunlun@weizoom.com>
2015-07-21 02:55:10 +0000

    #增加apk
    git-svn-id: http://s11/svn/weapp/web/trunk_2.0@16920 152f85a6-bef7-4f42-898c-89626e871b9b

--- weapp/mobile_app/settings.py
+++ weapp/mobile_app/settings.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-APP_VERSION = '2.1'
-APP_DOWNLOAD_URL = 'http://weapp.weizoom.com/mobile_static/app/weizoom.2.1.apk'
+APP_VERSION = '2.4'
+APP_DOWNLOAD_URL = 'http://weapp.weizoom.com/mobile_static/app/weizoom.2.4.apk'
