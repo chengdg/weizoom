6b480c63fd613c0b1cb3b79bd0ab9c7775208e95
jianzhe <jiangzhe@weizoom.com>
2015-07-23 12:51:58 +0000

    修复好评置顶加积分
    git-svn-id: http://s11/svn/weapp/web/trunk_2.0@17154 152f85a6-bef7-4f42-898c-89626e871b9b

--- weapp/mall/product_review_api_view.py
+++ weapp/mall/product_review_api_view.py
@@ -166,6 +166,14 @@ def update_product_review_status(request):
     from modules.member.integral import increase_member_integral
     from modules.member import models as member_models
     if product_review_id:
+        if status == '1' or status == '2':
+            review = mall_models.ProductReview.objects.filter(id=product_review_id)
+            if len(review) == 1 and review[0].status == '0':
+                settings = member_models.IntegralStrategySttings.objects.get(webapp_id=request.user_profile.webapp_id)
+                if settings.review_increase > 0:
+                    member = member_models.Member.objects.get(id=review[0].member_id)
+                    increase_member_integral(member, settings.review_increase, '商品好评返利')
+
         if status == '2':
             product_review = mall_models.ProductReview.objects.get(id=product_review_id)
             top_reviews = mall_models.ProductReview.objects.filter(product_id=product_review.product_id, status=int(status)).order_by("top_time")
@@ -177,14 +185,6 @@ def update_product_review_status(request):
                 mall_models.ProductReview.objects.filter(id=product_review_id).update(status=int(status), top_time=datetime.now())
         else:
             mall_models.ProductReview.objects.filter(id=product_review_id).update(status=int(status), top_time=mall_models.DEFAULT_DATETIME)
-
-        if status == '1' or status == '2':
-            review = mall_models.ProductReview.objects.filter(id=product_review_id)
-            if len(review) == 1 and int(review[0].status) == 0:
-                settings = member_models.IntegralStrategySttings.objects.get(webapp_id=request.user_profile.webapp_id)
-                if settings.review_increase > 0:
-                    member = member_models.Member.objects.get(id=review[0].member_id)
-                    increase_member_integral(member, settings.review_increase, '商品好评返利')
         return create_response(200).get_response()
     else:
         return create_response(500).get_response()
