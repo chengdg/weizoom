78925bdb81c08cbe8b1ff99471601d0856eb6f26
jianzhe <jiangzhe@weizoom.com>
2015-07-25 06:17:31 +0000

    清理过期文件
    git-svn-id: http://s11/svn/weapp/web/trunk_2.0@17215 152f85a6-bef7-4f42-898c-89626e871b9b

--- migrate/12.0/migrate.bat
+++
@@ -1,9 +0,0 @@
-#!/sbin/bash
-
-echo "migrate database"
-mysql -u weapp --password=weizoom weapp < migrate.sql
-echo "run deploy_product_model command"
-cd ../../weapp
-python manage.py syncdb
-python manage.py deploy_product_model
-python manage.py deploy_refactor_template
--- migrate/12.0/migrate.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE webapp_workspace ADD COLUMN template_name varchar(125) default 'default';
-ALTER TABLE webapp_workspace ADD COLUMN backend_template_name varchar(125) default 'default';
-ALTER TABLE mall_config ADD COLUMN is_enable_bill tinyint(1) default 0;
-ALTER TABLE mall_order_has_product ADD COLUMN product_model_name varchar(256) default 'standard';
-ALTER TABLE mall_shopping_cart ADD COLUMN product_model_name varchar(125) default 'standard';
