1283e9aee2dada43b878043e0233ab2cff6b1708
Eugene <237192896@qq.com>
2015-08-05 17:18:00 +0800

    修复二维码获取不到的bug

--- weapp/market_tools/tools/channel_qrcode/mobile_views.py
+++ weapp/market_tools/tools/channel_qrcode/mobile_views.py
@@ -72,11 +72,16 @@ def get_settings(request):
         return render_to_response('%s/channel_qrcode/webapp/channel_qrcode_context.html' % TEMPLATE_DIR, c)
 
 def get_new_settings(request):
-    user_id = request.GET.get('webapp_owner_id', 0)
+    user_id = request.webapp_owner_id
     ticketid = request.GET.get('ticketid', 0)
     member = request.member
     if ticketid:
         setting = MemberChannelQrcode.objects.get(id=ticketid)
+        if not setting.ticket:
+            ticket = _get_ticket(user_id)
+            setting.ticket = ticket
+            setting.save()
+
         show_head = False
         if setting.member_id == request.member.id:
             show_head = True
@@ -102,61 +107,33 @@ def get_new_settings(request):
                 new_url = '%s&ticketid=%s' % (request.get_full_path(), qrcode.id)
                 return HttpResponseRedirect(new_url)
             else:
-                user = User.objects.get(id=user_id)
                 setting = MemberChannelQrcodeSettings.objects.get(owner_id=user_id)
-                mp_user = get_binding_weixin_mpuser(user)
-                mpuser_access_token = get_mpuser_accesstoken(mp_user)
+                ticket = _get_ticket(user_id)
                 new_qrcode = MemberChannelQrcode.objects.create(
                     owner_id=user_id,
                     member_channel_qrcode_setting_id=setting.id,
                     member_id=member.id,
-                    ticket=""
+                    ticket=ticket
                 )
-                if mp_user.is_certified and mp_user.is_service and mpuser_access_token.is_active:
-                    print "---------create ticket--------"
-                    weixin_api = get_weixin_api(mpuser_access_token)
-                    qrcode_ticket = weixin_api.create_qrcode_ticket(new_qrcode.id, QrcodeTicket.PERMANENT)
-                    print "---bug1------", qrcode_ticket
-                    try:
-                        ticket = qrcode_ticket.ticket
-                    except:
-                        ticket = ''
-                    new_qrcode.ticket = ticket
-                    new_qrcode.save()
 
         c = RequestContext(request, {
-                'page_title': u'代言人二维码',})
-        return render_to_response('%s/channel_qrcode/webapp/channel_qrcode_context.html' % TEMPLATE_DIR, c)
+                'page_title': u'代言人二维码',
+                'show_head': True})
+        return render_to_response('%s/channel_qrcode/webapp/channel_qrcode_img.html' % TEMPLATE_DIR, c)
 
-    # if request.member:
-    #     setting = ChannelQrcodeSettings.objects.filter(bing_member_id=request.member.id, is_bing_member=True)
-    #     if setting.count() > 0:
-    #         setting = setting[0]
-    #         member = Member.objects.get(id=request.member.id)
-    #         member.user_name = member.username_for_html
-    #         setting.count = ChannelQrcodeHasMember.objects.filter(channel_qrcode_id=setting.id).count()
-
-    #         c = RequestContext(request, {
-    #             'page_title': u'代言人二维码',
-    #             'member': member,
-    #             'setting': setting,
-    #             'is_hide_weixin_option_menu': False,
-    #             'head_img': get_mp_head_img(user_id)
-    #         })
-    #         # if user_id == '467':
-    #             # from django.http import HttpResponseRedirect
-    #             # response = HttpResponseRedirect('https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=%s' % setting.ticket)
-    #             # return response
-    #         #return render_to_response('%s/channel_qrcode/webapp/channel_qrcode_img.html' % TEMPLATE_DIR, c)
-    #         # else:
-    #         return render_to_response('%s/channel_qrcode/webapp/channel_qrcode.html' % TEMPLATE_DIR, c)
-
-    #         new_url = '%s&%s' (request.get_full_path(), )
-    #         HttpResponseRedirect()
-    #     else:
-    #         c = RequestContext(request, {
-    #             'page_title': u'代言人二维码',})
-    #     return render_to_response('%s/channel_qrcode/webapp/channel_qrcode_context.html' % TEMPLATE_DIR, c)
-    # else:
-    #     pass
-
+def _get_ticket(user_id):
+    mp_user = get_binding_weixin_mpuser(user_id)
+    mpuser_access_token = get_mpuser_accesstoken(mp_user)
+    weixin_api = get_weixin_api(mpuser_access_token)
+    if mp_user.is_certified and mp_user.is_service and mpuser_access_token.is_active:
+        try:
+            qrcode_ticket = weixin_api.create_qrcode_ticket(user_id, QrcodeTicket.PERMANENT)
+            return qrcode_ticket.ticket
+        except:
+            return _get_ticket(user_id)
+    else:
+        try:
+            qrcode_ticket = weixin_api.create_qrcode_ticket(user_id, QrcodeTicket.PERMANENT)
+            return qrcode_ticket.ticket
+        except:
+            return ''
