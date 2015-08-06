43ab3f2768c9e98335c6058a62a6e039643d81b3
江哲 <jiangzhe@weizoom.com>
2015-08-06 13:49:46 +0800

    尝试修复微信授权

--- weapp/weixin/user/util.py
+++ weapp/weixin/user/util.py
@@ -13,8 +13,8 @@ def get_component_info_from(request):
 		component_info = ComponentInfo.objects.filter(app_id='wxba6fccbdcccbea49')[0]
 	elif request_host == 'docker.test.weizzz.com':
 		component_info = ComponentInfo.objects.filter(app_id='wx9b89fe19768a02d2')[0]
-	elif request_host == 'red.weapp.weizzz.com':
-		component_info = ComponentInfo.objects.filter(app_id='wxa823309fb403f661')[0]
+	elif request_host == 'red.weapp.weizzz.com' or request_host == 'nj.weapp.weizzz.com':
+		component_info = ComponentInfo.objects.filter(app_id='wxb04d4cdf32b59823')[0]
 	else:
 		component_info = ComponentInfo.objects.filter(app_id='wx8209f1f63f0b1d26')[0]
 
