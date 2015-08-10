6dae23a38d264bdba03cad8fdff09120aacd80da
liupeiyu <liupeiyu@weizoom.com>
2015-08-10 17:32:58 +0800

    [add] 加入 快递订阅请求 的 任务，由于网络或其它原因，未发送成功的再次发送

--- weapp/services/service_hourly.sh
+++ weapp/services/service_hourly.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 # 每小时调用的service
+
+DATE="`date +%Y%m%d`"
+TIME="`date +%Y%m%d%H%M%S`"
+ROOT_DIR="/weapp/web/weapp"
+#ROOT_DIR="`pwd`"
+LOG_DIR="$ROOT_DIR/../log/services/$DATE"
+mkdir -p $LOG_DIR
+
+LOG="$LOG_DIR/hourly_$TIME.log"
+echo "called time: $TIME" > $LOG
+echo "LOG_DIR=$LOG_DIR"
+
+echo "ROOT_DIR=$ROOT_DIR"
+cd $ROOT_DIR
+echo "========================================================" >> $LOG
+
+# add more services here
+echo ">> calling 'services.send_express_poll_service.tasks.send_express_poll_request'" >> $LOG
+echo "--------------------------------------------------------" >> $LOG
+python services/send_task.py "services.send_express_poll_service.tasks.send_express_poll_request" {} "{}" >> $LOG 2>&1
