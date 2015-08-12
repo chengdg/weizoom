23be3214f83ab43ebe7b340cb665e5e2f32c14d4
duhao <duhao@weizoom.com>
2015-08-12 09:43:17 +0800

    [update]数据罗盘-会员统计新增会员和会员总数去掉自动关注的会员

--- weapp/stats/util.py
+++ weapp/stats/util.py
@@ -9,7 +9,7 @@ from django.db.models.query_utils import Q
 
 from core import dateutil
 from core import paginator
-from modules.member.models import Member, MemberSharedUrlInfo, MemberInfo, WebAppUser, SOURCE_BY_URL, SOURCE_MEMBER_QRCODE
+from modules.member.models import Member, MemberSharedUrlInfo, MemberInfo, WebAppUser, SOURCE_BY_URL, SOURCE_MEMBER_QRCODE, CANCEL_SUBSCRIBED, SUBSCRIBED
 from market_tools.tools.member_qrcode.models import MemberQrcode, MemberQrcodeLog
 from mall.models import Order, belong_to, ORDER_STATUS_PAYED_SUCCESSED, ORDER_STATUS_PAYED_NOT_SHIP, ORDER_STATUS_PAYED_SHIPED, ORDER_STATUS_SUCCESSED, ORDER_SOURCE_OWN
 from mall.models import OrderHasProduct, Product, PRODUCT_SHELVE_TYPE_ON
@@ -197,7 +197,8 @@ def get_date2new_member_count(webapp_id, low_date, high_date, date_formatter = N
 					# is_subscribed = True, 
 					#is_for_buy_test = False, 
 					is_for_test = False, 
-					created_at__range=(low_date, high_date)
+					created_at__range=(low_date, high_date), 
+					status__in = (CANCEL_SUBSCRIBED, SUBSCRIBED)
 				)
 	#初始化数据
 	date2new_member_count = {}
@@ -237,14 +238,16 @@ def get_total_member_count(webapp_id, end_time = None):
 						# is_subscribed = True, 
 						#is_for_buy_test = False, 
 						is_for_test = False,
-						created_at__lte = end_time
+						created_at__lte = end_time, 
+						status__in = (CANCEL_SUBSCRIBED, SUBSCRIBED)
 					).count()
 	else:
 		total_count = Member.objects.filter(
 						webapp_id = webapp_id, 
 						# is_subscribed = True, 
 						#is_for_buy_test = False, 
-						is_for_test = False
+						is_for_test = False, 
+						status__in = (CANCEL_SUBSCRIBED, SUBSCRIBED)
 					).count()
 	return total_count
 
