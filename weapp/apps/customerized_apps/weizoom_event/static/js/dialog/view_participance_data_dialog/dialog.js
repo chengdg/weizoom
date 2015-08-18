4bbfe18559fc9524af4c7798c5ded3a1b977d85e
Asia <aixiang@narzh.com>
2015-08-18 18:56:51 +0800

    test

---
+++ weapp/apps/customerized_apps/weizoom_event/static/js/dialog/view_participance_data_dialog/dialog.js
@@ -0,0 +1,51 @@
+/*
+Copyright (c) 2011-2012 Weizoom Inc
+*/
+
+/**
+ * 对话框
+ */
+ensureNS('W.dialog.app.weizoom_event');
+W.dialog.app.weizoom_event.ViewParticipanceDataDialog = W.dialog.Dialog.extend({
+	events: _.extend({
+	}, W.dialog.Dialog.prototype.events),
+	
+	templates: {
+		dialogTmpl: '#app-weizoom_event-viewParticipanceDataDialog-dialog-tmpl'
+	},
+	
+	onInitialize: function(options) {
+	},
+	
+	beforeShow: function(options) {
+	},
+	
+	onShow: function(options) {
+		this.activityId = options.activityId;
+	},
+	
+	afterShow: function(options) {
+		if (this.activityId) {
+			W.getApi().call({
+				app: 'apps/weizoom_event',
+				resource: 'weizoom_event_participance',
+				scope: this,
+				args: {
+					id: this.activityId
+				},
+				success: function(data) {
+					this.$dialog.find('.modal-body').text(data);
+				},
+				error: function(resp) {
+				}
+			})
+		}
+	},
+	
+	/**
+	 * onGetData: 获取数据
+	 */
+	onGetData: function(event) {
+		return {};
+	}
+});
---
+++ weapp/apps/customerized_apps/weizoom_event/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import weizoom_event
+import weizoom_event_participance
+import weizoom_event_participances
+import m_weizoom_event
+import weizoom_event_status
+import weizoom_events
+
