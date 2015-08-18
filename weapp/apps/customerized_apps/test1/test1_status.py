cc418aa7c99db774ec0689c51e742bc313419158
Asia <aixiang@narzh.com>
2015-08-18 18:29:08 +0800

    test

---
+++ weapp/apps/customerized_apps/test1/test1_status.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import json
+from datetime import datetime
+
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.db.models import F
+from django.contrib.auth.decorators import login_required
+
+from core import resource
+from core import paginator
+from core.jsonresponse import create_response
+
+import models as app_models
+import export
+from apps import request_util
+
+class test1Status(resource.Resource):
+	app = 'apps/test1'
+	resource = 'test1_status'
+	
+	@login_required
+	def api_post(request):
+		"""
+		响应POST
+		"""
+		target_status = request.POST['target']
+		if target_status == 'stoped':
+			target_status = app_models.STATUS_STOPED
+		elif target_status == 'running':
+			target_status = app_models.STATUS_RUNNING
+		elif target_status == 'not_start':
+			target_status = app_models.STATUS_NOT_START
+		app_models.test1.objects(id=request.POST['id']).update(set__status=target_status)
+		
+		response = create_response(200)
+		return response.get_response()
---
+++ weapp/apps/customerized_apps/test1/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import test1
+import test1_participance
+import test1_participances
+import m_test1
+import test1_status
+import test1s
+
