59cc36f4bc8b0d2f7df404dc7ec6107411ae79fd
xin <418618239@qq.com>
2015-08-21 17:44:09 +0800

    bug底部导航二级位置

--- weapp/termite/static/v3/gmu/widget/weshopBottomNav.js
+++ weapp/termite/static/v3/gmu/widget/weshopBottomNav.js
@@ -54,7 +54,7 @@ gmu.define('BottomNav', {
 
          }else {
          	$subMenuContainer.css('margin-left',-width/2 - menuWidth);
-         	$otherSubMenuContainer.eq(1).css('margin-left',-width/2 + menuWidth/2);
+         	$otherSubMenuContainer.eq(1).css('margin-left',-width/2);
          	$otherSubMenuContainer.eq(2).css({
          		'right':8,
          		'left':'auto',
