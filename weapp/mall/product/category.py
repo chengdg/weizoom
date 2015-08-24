cad8b95918d8ccc474ffe622aecca6bb8dbd03ea
江哲 <jiangzhe@weizoom.com>
2015-08-24 14:55:17 +0800

    修复添加商品分组无商品的BUG

--- weapp/mall/product/category.py
+++ weapp/mall/product/category.py
@@ -63,9 +63,9 @@ class CategoryList(resource.Resource):
             name_query = request.GET.get('name')
 
             #获取商品集合
-            products = mall_models.Product.objects.filter(
+            products = list(mall_models.Product.objects.filter(
                 owner=request.manager, is_deleted=False).exclude(
-                shelve_type=mall_models.PRODUCT_SHELVE_TYPE_RECYCLED)
+                shelve_type=mall_models.PRODUCT_SHELVE_TYPE_RECYCLED))
             if name_query:
                 products = [
                     product for product in products if name_query in product.name
