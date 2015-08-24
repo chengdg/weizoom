4714aa5e75910ac6f4389022b753c47d8a194474
liupeiyu <liupeiyu@weizoom.com>
2015-08-24 10:03:06 +0800

    修改代码

--- weapp/termite2/models.py
+++ weapp/termite2/models.py
@@ -55,65 +55,13 @@ class TemplateGlobalNavbar(models.Model):
 		verbose_name_plural = '全局导航'
 
 
-	def get_pages(self):
-		if self._pages:
-			return self._pages
-
-		pages = PageHasGlobalNavbar.objects.filter(owner=self.owner, global_navbar=self, is_enable=True)
-		if pages.count() > 0:
-			self._pages = {}
-			for p in pages:
-				self._pages[p.page_type] = True
-			return self._pages
-		else:
-			return {}
-
 	@staticmethod
 	def get_object(user_id):
-		global_navbar, _ = TemplateGlobalNavbar.objects.get_or_create(
-			owner_id=user_id
-		)
-		return global_navbar
-
-
-
-TEMPLATE_GLOBAL_NAVBAR_WEPAGE = 'wepage'
-TEMPLATE_GLOBAL_NAVBAR_HOME_page = 'home_page'
-
-class PageHasGlobalNavbar(models.Model):
-	'''
-	页面对应的全局导航
-	'''
-	owner = models.ForeignKey(User, related_name='owned_page_has_global_navbar')
-	global_navbar = models.ForeignKey(TemplateGlobalNavbar)
-	page_type = models.CharField(max_length=50, default='', verbose_name='空间类型')
-	is_enable = models.BooleanField(default=False, verbose_name='是否启用')
-	created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
-
-	class Meta(object):
-		db_table = 'page_has_global_navbar'
-		verbose_name = '页面对应的全局导航'
-		verbose_name_plural = '页面对应的全局导航'
-
-	@staticmethod
-	def get_object(user, page_type, global_navbar):
-		page_global_navbar, _ = PageHasGlobalNavbar.objects.get_or_create(
-			owner = user,
-			global_navbar = global_navbar,
-			page_type = page_type
-		)
-		return page_global_navbar
-
-
-	@staticmethod
-	def get_global_navbar(user, page_type):
-		try:			
-			page_global_navbar = PageHasGlobalNavbar.objects.get(
-				owner = user,
-				page_type = page_type,
-				is_enable = True
+		if user_id > 0:
+			global_navbar, _ = TemplateGlobalNavbar.objects.get_or_create(
+				owner_id=user_id
 			)
-			return page_global_navbar.global_navbar
-		except:
+			return global_navbar
+		else:
 			return None
 
