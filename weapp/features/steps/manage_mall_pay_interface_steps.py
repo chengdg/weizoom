13df6a05623a2d207291a94a942eeb89fb0c24a5
高立琦 <gaoliqi@weizoom.com>
2015-08-25 14:10:29 +0800

    给target_pay_interface初始值

--- weapp/features/steps/manage_mall_pay_interface_steps.py
+++ weapp/features/steps/manage_mall_pay_interface_steps.py
@@ -48,6 +48,7 @@ def step_impl(context, user):
     elif expected['type'] == u'支付宝':
         pay_interface_type = PAY_INTERFACE_ALIPAY
     # db_pay_interface = PayInterface.objects.get(owner_id=context.webapp_owner_id, type=pay_interface_type)
+    target_pay_interface = ''
     for pay_interface in response.context['pay_interfaces']:
         if pay_interface.type == pay_interface_type:
             target_pay_interface = pay_interface
