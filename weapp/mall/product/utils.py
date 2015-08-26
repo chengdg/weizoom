80cc484a7259c41549c3283e3159c427fb65df18
duhao <duhao@weizoom.com>
2015-08-26 16:40:20 +0800

    创建商品的的默认库存由-1改为0，解决bug 3741

--- weapp/mall/product/utils.py
+++ weapp/mall/product/utils.py
@@ -59,6 +59,8 @@ def extract_product_model(request):
             models.PRODUCT_STOCK_TYPE_UNLIMIT)
         )
         stocks = request.POST.get('stocks')
+        # stocks = int(stocks) if stocks else -1
+        #默认商品数改为0 duhao 2015-08-26
         stocks = int(stocks) if stocks else -1
         user_code = request.POST.get('user_code', '').strip()
         standard_model = {
