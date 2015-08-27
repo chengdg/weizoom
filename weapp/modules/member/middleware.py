ad8bf4e5615c9f7b0b08cb77cb8d1ecd27b46d3b
bert <1597594211@qq.com>
2015-08-27 14:30:17 +0800

    删掉测试代码

--- weapp/modules/member/middleware.py
+++ weapp/modules/member/middleware.py
@@ -949,10 +949,6 @@ class OAUTHMiddleware(object):
 		#对于非webapp请求和非pc商城地方请求不进行处理
 		if (not request.is_access_webapp) and (not request.is_access_pcmall):
 			return None
-
-		if 'model=address&action=list' in request.META['HTTP_REFERER'] or 'model=address&action=add' in request.META['HTTP_REFERER']::
-			return None
-
 		# if is_request_for_api(request):
 		#  	return None
 		if request.webapp_owner_info and \
