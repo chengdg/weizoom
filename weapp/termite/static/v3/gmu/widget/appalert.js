c26230e9deb88dce3cbb4408ce6465a7da09bfcf
cl <chenlei@narzh.com>
2015-08-28 19:08:50 +0800

    mod url

--- weapp/termite/static/v3/gmu/widget/appalert.js
+++ weapp/termite/static/v3/gmu/widget/appalert.js
@@ -67,7 +67,7 @@ var initDom = function(options) {
             var text = $link.text();
             if (text.replace(/(^\s*)|(\s*$)/g,'') === '个人中心' && !W.member_id){
                 $('.wui-appParticipantResult-Cover').html('<div class="wui-qrcode">' +
-                '<img height="195px" width="195px" src="{% if auth_appid_info %}{{auth_appid_info.qrcode_url}}{% endif %}">' +
+                '<img height="195px" width="195px" src="'+W.qrcodeUrl+'">' +
                 '</div><div>');
             }
             else{
