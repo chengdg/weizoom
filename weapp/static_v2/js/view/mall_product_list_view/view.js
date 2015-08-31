e6c14a4fbcc8b220e9d39d04a67a2bff6444f54c
江哲 <jiangzhe@weizoom.com>
2015-09-01 00:17:11 +0800

    修复BUG 4365 修改多规格商品有多余弹窗

--- weapp/static_v2/js/view/mall_product_list_view/view.js
+++ weapp/static_v2/js/view/mall_product_list_view/view.js
@@ -193,7 +193,6 @@ W.view.mall.ProductListView = Backbone.View.extend({
                 args: W.toFormData({'model_infos': newModelInfos}),
                 scope: this,
                 success: function(data) {
-                    alert('sss')
                     //遍历table中的model，如果该model在newModelInfos中出现，则:
                     //model.stock_type = newModelInfo.stock_type
                     //model.stocks = newModelInfo.stocks
