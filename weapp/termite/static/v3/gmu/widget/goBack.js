38051b2f3d73fb1bf40de71396c3d3387d24a674
Asia <aixiang@narzh.com>
2015-09-07 18:19:27 +0800

    尝试修复goBack不刷新页面的问题

--- weapp/termite/static/v3/gmu/widget/goBack.js
+++ weapp/termite/static/v3/gmu/widget/goBack.js
@@ -18,7 +18,13 @@ gmu.define('GoBack', {
 			if(window.history.length == 1){
 				window.location.href = './?woid='+W.webappOwnerId+'&module=mall&model=products&action=list';
 			}else{
-				window.history.go(-1);
+				//获取上一页的url，再通过href转向(修复history.back(-1)不刷新页面的问题)
+				var preUrl = document.referrer;
+				if(!preUrl){//当前页如果不是通过超链接进来的话，会得到null
+					window.history.back(-1);
+				}else{
+					window.location.href = preUrl;
+				}
 			}
 		});
 	}
