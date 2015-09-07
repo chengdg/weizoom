294d2b08b9b913d0261ec500fa02b21c5cd7425e
lihanyi <lihanyi@narzh.com>
2015-09-07 15:30:16 +0800

    add try,except

--- weapp/apps/customerized_apps/vote/m_vote.py
+++ weapp/apps/customerized_apps/vote/m_vote.py
@@ -37,94 +37,96 @@ class Mvote(resource.Resource):
 		"""
 		响应GET
 		"""
-		if 'id' in request.GET:
-			id = request.GET['id']
-			isPC = request.GET.get('isPC',0)
-			isMember = False
-			auth_appid_info = None
-			thumbnails_url = '/termite_static/img/component/lottery/roulette_title.png'
-			if not isPC:
-				isMember = request.member and request.member.is_subscribed
-				if not isMember:
-					from weixin.user.util import get_component_info_from
-					component_info = get_component_info_from(request)
-					auth_appid = weixin_models.ComponentAuthedAppid.objects.filter(component_info=component_info, user_id=request.GET['webapp_owner_id'])[0]
-					auth_appid_info = weixin_models.ComponentAuthedAppidInfo.objects.filter(auth_appid=auth_appid)[0]
-			participance_data_count = 0
-			if 'new_app:' in id:
-				project_id = id
-				activity_status = u"未开始"
+		try:
+			if 'id' in request.GET:
+				id = request.GET['id']
+				isPC = request.GET.get('isPC',0)
+				isMember = False
+				auth_appid_info = None
+				thumbnails_url = '/termite_static/img/component/lottery/roulette_title.png'
+				if not isPC:
+					isMember = request.member and request.member.is_subscribed
+					if not isMember:
+						from weixin.user.util import get_component_info_from
+						component_info = get_component_info_from(request)
+						auth_appid = weixin_models.ComponentAuthedAppid.objects.filter(component_info=component_info, user_id=request.GET['webapp_owner_id'])[0]
+						auth_appid_info = weixin_models.ComponentAuthedAppidInfo.objects.filter(auth_appid=auth_appid)[0]
+				participance_data_count = 0
+				if 'new_app:' in id:
+					project_id = id
+					activity_status = u"未开始"
+				else:
+					#termite类型数据
+					record = app_models.vote.objects.get(id=id)
+					activity_status = record.status_text
+
+					now_time = datetime.today().strftime('%Y-%m-%d %H:%M')
+					data_start_time = record.start_time.strftime('%Y-%m-%d %H:%M')
+					data_end_time = record.end_time.strftime('%Y-%m-%d %H:%M')
+					if record.status <= 1:
+						if data_start_time <= now_time and now_time < data_end_time:
+							record.update(set__status=app_models.STATUS_RUNNING)
+							activity_status = u'进行中'
+						elif now_time >= data_end_time:
+							record.update(set__status=app_models.STATUS_STOPED)
+							activity_status = u'已结束'
+
+					project_id = 'new_app:vote:%s' % record.related_page_id
+					if request.member:
+						participance_data_count = app_models.voteParticipance.objects(belong_to=id, member_id=request.member.id).count()
+					if participance_data_count == 0 and request.webapp_user:
+						participance_data_count = app_models.voteParticipance.objects(belong_to=id, webapp_user_id=request.webapp_user.id).count()
+
+				is_already_participanted = (participance_data_count > 0)
+				if  is_already_participanted:
+					member_id = request.member.id
+					vote_detail,result_list = get_result(id,member_id)
+					c = RequestContext(request, {
+						'vote_detail': vote_detail,
+						'record_id': id,
+						'page_title': '微信投票',
+						'app_name': "vote",
+						'resource': "vote",
+						'q_vote': result_list,
+						'hide_non_member_cover': True, #非会员也可使用该页面
+						'isMember': isMember,
+						'auth_appid_info': auth_appid_info
+					})
+					return render_to_response('vote/templates/webapp/result_vote.html', c)
+				else:
+					request.GET._mutable = True
+					request.GET.update({"project_id": project_id})
+					request.GET._mutable = False
+					html = pagecreater.create_page(request, return_html_snippet=True)
+					pagestore = pagestore_manager.get_pagestore('mongo')
+					page = pagestore.get_page(record.related_page_id, 1)
+					permission = page['component']['components'][0]['model']['permission']
+					c = RequestContext(request, {
+						'record_id': id,
+						'member_id': request.member.id if request.member else "",
+						'activity_status': activity_status,
+						'is_already_participanted': is_already_participanted,
+						'page_title': '微信投票',
+						'page_html_content': html,
+						'app_name': "vote",
+						'resource': "vote",
+						'hide_non_member_cover': True, #非会员也可使用该页面
+						'isPC': isPC,
+						'isMember': isMember,
+						'auth_appid_info': auth_appid_info,
+						'permission': permission,
+						'share_page_desc': record.name,
+				'share_img_url': thumbnails_url
+					})
+					return render_to_response('workbench/wepage_webapp_page.html', c)
 			else:
-				#termite类型数据
-				record = app_models.vote.objects.get(id=id)
-				activity_status = record.status_text
-
-				now_time = datetime.today().strftime('%Y-%m-%d %H:%M')
-				data_start_time = record.start_time.strftime('%Y-%m-%d %H:%M')
-				data_end_time = record.end_time.strftime('%Y-%m-%d %H:%M')
-				if record.status <= 1:
-					if data_start_time <= now_time and now_time < data_end_time:
-						record.update(set__status=app_models.STATUS_RUNNING)
-						activity_status = u'进行中'
-					elif now_time >= data_end_time:
-						record.update(set__status=app_models.STATUS_STOPED)
-						activity_status = u'已结束'
-
-				project_id = 'new_app:vote:%s' % record.related_page_id
-				if request.member:
-					participance_data_count = app_models.voteParticipance.objects(belong_to=id, member_id=request.member.id).count()
-				if participance_data_count == 0 and request.webapp_user:
-					participance_data_count = app_models.voteParticipance.objects(belong_to=id, webapp_user_id=request.webapp_user.id).count()
-
-			is_already_participanted = (participance_data_count > 0)
-			if  is_already_participanted:
-				member_id = request.member.id
-				vote_detail,result_list = get_result(id,member_id)
+				record = None
 				c = RequestContext(request, {
-					'vote_detail': vote_detail,
-					'record_id': id,
-					'page_title': '微信投票',
-					'app_name': "vote",
-					'resource': "vote",
-					'q_vote': result_list,
-					'hide_non_member_cover': True, #非会员也可使用该页面
-					'isMember': isMember,
-					'auth_appid_info': auth_appid_info
+					'record': record
 				})
-				return render_to_response('vote/templates/webapp/result_vote.html', c)
-			else:
-				request.GET._mutable = True
-				request.GET.update({"project_id": project_id})
-				request.GET._mutable = False
-				html = pagecreater.create_page(request, return_html_snippet=True)
-				pagestore = pagestore_manager.get_pagestore('mongo')
-				page = pagestore.get_page(record.related_page_id, 1)
-				permission = page['component']['components'][0]['model']['permission']
-				c = RequestContext(request, {
-					'record_id': id,
-					'member_id': request.member.id if request.member else "",
-					'activity_status': activity_status,
-					'is_already_participanted': is_already_participanted,
-					'page_title': '微信投票',
-					'page_html_content': html,
-					'app_name': "vote",
-					'resource': "vote",
-					'hide_non_member_cover': True, #非会员也可使用该页面
-					'isPC': isPC,
-					'isMember': isMember,
-					'auth_appid_info': auth_appid_info,
-					'permission': permission,
-					'share_page_desc': record.name,
-					'share_img_url': thumbnails_url
-				})
-				return render_to_response('workbench/wepage_webapp_page.html', c)
-		else:
-			record = None
-			c = RequestContext(request, {
-				'record': record
-			})
-			
-			return render_to_response('vote/templates/webapp/m_vote.html', c)
+				return render_to_response('vote/templates/webapp/m_vote.html', c)
+		except:
+			pass
 
 
 class resultVote(resource.Resource):
