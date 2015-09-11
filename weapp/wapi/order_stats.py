db35938707ee5857dcdae1711e9363e07c882d9d
高立琦 <gaoliqi@weizoom.com>
2015-09-11 15:26:06 +0800

    实现获取商品分组列表的API

--- weapp/wapi/order_stats.py
+++ weapp/wapi/order_stats.py
@@ -3,41 +3,13 @@
 有关订单的统计数据
 """
 
-#import json
-#from datetime import datetime
-#from django.http import HttpResponseRedirect
-#from django.template import RequestContext
-#from django.shortcuts import render_to_response
-#from django.db.models import F
-
 from core import resource
-#from core import paginator
-#from core.jsonresponse import create_response
-#from core.charts_apis import create_line_chart_response
-
-#from mall.models import *
-#from utils import dateutil as util_dateutil
-#import pandas as pd
-#from core import dateutil
-#from webapp import models as webapp_models
-#from webapp import statistics_util as webapp_statistics_util
-#from core.charts_apis import *
-#from django.conf import settings
-#import stats.util as stats_util
-#from stats.models import BrandValueHistory
-
-#from core.exceptionutil import unicode_full_stack
-#from watchdog.utils import watchdog_error, watchdog_warning
-
 from wapi.decorators import wapi_access_required
 from wapi.wapi_utils import create_json_response
 
-#from stats.manage.brand_value_utils import get_brand_value
-
-#from utils import dateutil as utils_dateutil
-
 from mall import models as mall_models
 from modules.member import models as member_models
+
 
 class MemberStats(resource.Resource):
 	app = 'wapi'
