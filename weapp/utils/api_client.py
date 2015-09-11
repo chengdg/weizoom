6416dfc8f797fde501abc5b9bd557329219210b8
高立琦 <gaoliqi@weizoom.com>
2015-09-11 20:46:30 +0800

    增加商城API的访问工具: mall_api.

---
+++ weapp/utils/api_client.py
@@ -0,0 +1,145 @@
+#coding: utf8
+"""
+访问WAPI的client
+
+"""
+
+from weapp.settings import WAPI_SECRET_ACCESS_TOKEN, WAPI_HOST
+import json
+import requests
+
+_api_client = None
+
+def wapi():
+	global _api_client
+	if _api_client is None:
+		_api_client = ApiClient()
+	return _api_client
+
+
+def set_wapi_client(client):
+	"""
+	用于BDD测试模式
+	"""
+	global _api_client
+	_api_client = TestApiClient(client)
+	return
+
+
+class TestApiClient:
+	"""
+	用于BDD测试用的ApiClient
+	"""
+
+	def __init__(self, client, access_token=WAPI_SECRET_ACCESS_TOKEN):
+		self.access_token = access_token
+		self.client = client
+		return
+
+	def get(self, addr, params=None):
+		if params is None:
+			params={'access_token': self.access_token}
+		else:
+			params['access_token'] = self.access_token
+		url = "/%s" % (addr)
+		print("GET: {}".format(url))
+		req = self.client.get(url, params)
+		return req.content
+
+
+	def post(self, addr, params=None):
+		if params is None:
+			params={'access_token': self.access_token}
+		else:
+			params['access_token'] = self.access_token
+		url = "/%s" % (addr)
+		print("POST: {}".format(url))
+		req = self.client.post(url, params)
+		return req.content
+
+
+	def put(self, addr, params=None):
+		if params is None:
+			params={'access_token': self.access_token}
+		else:
+			params['access_token'] = self.access_token
+		params['_method'] = 'put'
+		url = "/%s" % (addr)
+		print("PUT: {}".format(url))
+		req = self.client.post(url, params)
+		return req.content
+
+
+	def get_json(self, addr, params=None):
+		text = self.get(addr, params)
+		json_obj = json.loads(text)
+		if json_obj is not None:
+			if json_obj["code"] == 200:
+				return json_obj["data"]
+			print("response: {}".format(text))
+			raise Exception("Error Message: %s (code: %d)" % (json_obj.get('errMsg'), json_obj.get('code')))
+		else:
+			print("response: {}".format(text))
+			raise Exception("Not valid JSON!")
+
+
+class ApiClient:
+
+	def __init__(self, host=WAPI_HOST, access_token=WAPI_SECRET_ACCESS_TOKEN):
+		self.access_token = access_token
+		self.hostname = host
+		#self.conn = httplib.HTTPConnection(self.hostname)
+		self.headers = {
+			"Content-type": "application/x-www-form-urlencoded",
+			"Accept": "text/plain",
+			"User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0",
+			#"Cache-Control": "no-cache",
+			}
+		return
+
+
+	def get(self, addr, params=None):
+		if params is None:
+			params={'access_token': self.access_token}
+		else:
+			params['access_token'] = self.access_token
+		url = "%s/%s" % (self.hostname, addr)
+		print("URL: {}".format(url))
+		req = requests.get(url, params=params, headers=self.headers)
+		return req.text
+
+
+	def post(self, addr, params=None):
+		if params is None:
+			params={'access_token': self.access_token}
+		else:
+			params['access_token'] = self.access_token
+		url = "%s/%s" % (self.hostname, addr)
+		print("URL: {}".format(url))
+		req = requests.post(url, params=params, headers=self.headers)
+		return req.text
+
+
+	def put(self, addr, params=None):
+		if params is None:
+			params={'access_token': self.access_token}
+		else:
+			params['access_token'] = self.access_token
+		params['_method'] = 'put'
+		url = "%s/%s" % (self.hostname, addr)
+		print("PUT: {}".format(url))
+		req = requests.post(url, params=params, headers=self.headers)
+		return req.text
+
+
+	def get_json(self, addr, params=None):
+		text = self.get(addr, params)
+		json_obj = json.loads(text)
+		if json_obj is not None:
+			if json_obj["code"] == 200:
+				return json_obj["data"]
+			print("response: {}".format(text))
+			raise Exception("Error Message: %s (code: %d)" % (json_obj.get('errMsg'), json_obj.get('code')))
+		else:
+			print("response: {}".format(text))
+			raise Exception("Not valid JSON!")
---
+++ weapp/utils/mall_api.py
@@ -0,0 +1,137 @@
+#coding: utf8
+"""
+微商城访问接口(通过WAPI)
+"""
+
+import os,sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wglass.settings')
+#for path in sys.path:	print("{}".format(path))
+
+
+#from core.exceptionutil import unicode_full_stack
+
+from utils.api_client import wapi
+
+
+def get_order_stats(webapp_id, start_date=None, end_date=None):
+	"""
+	获取多项品牌价值
+	"""
+	params = {
+		'wid': webapp_id,
+		'date_start': start_date,
+		'date_end': end_date
+	}
+	results = wapi().get_json('wapi/api/order_stats/', params)
+	return results
+
+
+def get_webapp_id(username):
+	params = {
+		'username': username
+	}
+	results = wapi().get_json('wapi/api/webapp_id/', params)
+	return results.get('webapp_id')
+
+
+def get_member_count(webapp_id, date_start=None, date_end=None):
+	"""
+	获取会员总数
+	"""
+	params = {
+		'wid': webapp_id,
+		'date_start': date_start,
+		'date_end': date_end
+	}
+	results = wapi().get_json('wapi/api/member_stats/', params)
+	return results.get('member_count')
+
+
+def get_product_categories(uid):
+	"""
+	获取商品分组列表
+	"""
+	params = {
+		'uid': uid
+	}
+	results = wapi().get_json('wapi/api/product_categories/', params)
+	return results['categories']
+
+
+def get_product_category(category_id):
+	"""
+	获取一个分类详情
+	"""
+	params = {
+		'id': category_id
+	}
+	category = wapi().get_json('wapi/api/product_category/', params)
+	return category
+
+
+def update_product_category(category_id, name):
+	"""
+	获取商品分组列表
+	"""
+	params = {
+		'id': category_id,
+		'name': name
+	}
+	wapi().post('wapi/api/product_category/', params)
+	return
+
+
+def add_product_category(uid, name):
+	"""
+	创建商品分类
+	"""
+	params = {
+		'uid': uid,
+		'name': name
+	}
+	response = wapi().put('wapi/api/product_category/', params)
+	print("RESPONSE: {}".format(response))
+	return
+
+
+
+if __name__ == "__main__":
+	idlist = [
+	"ainicoffee",
+	"aliguo",
+	"wugutang",
+	"judou",
+	"heshibaineng",
+	"zhonghaitou",
+	"tianmashengwu",
+	"guangruishipin",
+	"yingguan",
+	"LaRhea",
+	"hanjin",
+	"fxkj",
+	"tianreyifang",
+	"amigo",
+	"hongfan",
+	"gangshanxigu",
+
+	"changjiufangzhi",
+
+
+	"tide",
+
+	"tianreyifang",
+
+	"heruntiancheng",
+	"dongfangwodeming",
+	"huajitang"
+	]
+
+	print("USERNAME\tMEMBER_CNT\tORDER_CNT\tPAIED_MEMBER\tTOTAL_PAYMENT")
+	for username in idlist:
+		webapp_id = get_webapp_id(username)
+		results = get_order_stats(webapp_id, "2015-08-01", "2015-08-31")
+		member_count = get_member_count(webapp_id, "2015-08-01", "2015-08-31")
+
+		#print("results: {}".format(results))
+		print("{}\t{}\t{}\t{}\t{}".format(username, member_count, results['order_count'], results['member_count'], results['total_payment']))
