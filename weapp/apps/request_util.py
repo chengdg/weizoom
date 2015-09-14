53dfb82b1f5157a09aca1f09869407e4eddb8ac2
lihanyi <lihanyi@narzh.com>
2015-09-14 17:54:44 +0800

    修改图片存储位置

--- weapp/apps/request_util.py
+++ weapp/apps/request_util.py
@@ -36,19 +36,12 @@ def get_fields_to_be_save(request):
 		fields['prize'] = json.loads(fields['prize'])
 
 	if 'termite_data' in fields:
-		# sample_tm = fields['termite_data']
-		# for item in sample_tm:
-		# 	if sample_tm[item]['type']=='appkit.uploadimg':
-		# 		print '22222'
-		# for item in fields['termite_data']:
-		# print fields['termite_data']['type']
-				# print '2222'
 		fields['termite_data'] = json.loads(fields['termite_data'])
-
-	# if 'uploadImg' in request.POST:
-	# 	att_url = []
-	# 	fields['uploadImg'] = json.loads(fields['uploadImg'])
-	# 	for picture in fields['uploadImg']:
-	# 		att_url.append(save_base64_img_file_local_for_webapp(request, picture))
-	# 	fields['att_url'] = att_url
+		att_url = []
+		for item in fields['termite_data']:
+			if fields['termite_data'][item]['type']=='appkit.uploadimg':
+				fields['uploadImg'] = json.loads(fields['termite_data'][item]['value'])
+				for picture in fields['uploadImg']:
+					att_url.append(save_base64_img_file_local_for_webapp(request, picture))
+				fields['termite_data'][item]['value'] = att_url
 	return fields
