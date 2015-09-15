d58744bf537fb34d6d99b7d02f65ec67ffa1add0
Mark24 <mark.zhangyoung@gmail.com>
2015-09-15 16:02:34 +0800

    调试

--- weapp/apps/customerized_apps/survey/static/js/dialog/view_participance_data_dialog/dialog.js
+++ weapp/apps/customerized_apps/survey/static/js/dialog/view_participance_data_dialog/dialog.js
@@ -40,14 +40,27 @@ W.dialog.app.survey.ViewParticipanceDataDialog = W.dialog.Dialog.extend({
 					var template = Handlebars.compile($(that.templates['resultTmpl']).html());
 					$('.xui-app_survey-Dialog .modal-body').html(template(data));
 
+
 					var att_name = data.att_url.item_name;
 					var att_array = data.att_url.item_value;
 					var att_val = "";
 					for(var i=0;i<att_array.length;i++){
-						att_val = att_val+'<img src=\"'+att_array[i]+'\">'
+						att_val = att_val+'<img class=\"xa-uploadimg\" src=\"'+att_array[i]+'\">'
 					}
 					var att_html = '<tr><td>'+att_name+':</td><td>'+att_val+'</td></tr>';
 					$('.modal-body .table.table-bordered').append(att_html);
+
+
+					$('img.xa-uploadimg').click(function(){
+						var that = this;
+						$('.xa-uploadimg_box').append($(that).clone(true));
+						$('.xa-uploadimg_box').removeClass('inactive').addClass('active');
+						$('img.xa-uploadimg').unbind();
+					});
+
+					$('.xa-uploadimg_box .xa-close_btn').click(function(){
+						$('.xa-uploadimg_box').removeClass('active');
+					})
 
 				},
 				error: function(resp) {
