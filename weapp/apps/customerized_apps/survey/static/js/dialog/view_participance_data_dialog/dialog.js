78ed0911564ac72949f52aa56ead08f5ecde0e99
Mark24 <mark.zhangyoung@gmail.com>
2015-09-17 15:52:53 +0800

    查看大图Chrome兼容

--- weapp/apps/customerized_apps/survey/static/js/dialog/view_participance_data_dialog/dialog.js
+++ weapp/apps/customerized_apps/survey/static/js/dialog/view_participance_data_dialog/dialog.js
@@ -64,7 +64,7 @@ W.dialog.app.survey.ViewParticipanceDataDialog = W.dialog.Dialog.extend({
 							var img_arr = $(that).parent().find('img').clone(true);
 							var img_id_index = parseInt($(that).attr('id').split('-')[1]);
 
-							$(this).parents().find('.xa-uploadimg_box').append("<div><div class='xa-uploadimg_div'></div><div><img class='xa-close_btn' src='/static_v2/img/close_btn.png'></div><div><div class='xa-arr xa-arr_left'></div><div class='xa-arr xa-arr_right'></div>").fadeIn('fast');
+							$(this).parents().find('.xa-uploadimg_box').append("<div class='xa-uploadimg_div'></div><div><img class='xa-close_btn' src='/static_v2/img/close_btn.png'></div><div class='xa-arr xa-arr_left'></div><div class='xa-arr xa-arr_right'></div>").fadeIn('fast');
 							$('.xa-uploadimg_div').append($(that).clone(true));
 							if(img_len==1){
 								$('.xa-arr').css('display','none');
