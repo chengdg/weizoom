af7c063e2db0b322cd2c619a812d68edbc5e1dd2
lihanyi <lihanyi@narzh.com>
2015-09-17 15:46:14 +0800

    mod 总参与人数

--- weapp/apps/customerized_apps/survey/survey_statistics.py
+++ weapp/apps/customerized_apps/survey/survey_statistics.py
@@ -31,7 +31,7 @@ class surveyStatistics(resource.Resource):
 		if 'id' in request.GET:
 			survey_id =request.GET['id']
 			all_participances = app_models.surveyParticipance.objects(belong_to=survey_id)
-			total_count = all_participances.count()
+			total_participance = all_participances.count()
 
 			q_vote ={}
 			result_list = []
@@ -121,7 +121,7 @@ class surveyStatistics(resource.Resource):
 			'second_navs': export.get_customerized_apps(request),
 			'second_nav_name': 'surveies',
 			'titles': result_list,
-			'total_count': total_count,
+			'total_participance': total_participance,
 			'project_id': project_id,
 			'survey_id':survey_id
 
