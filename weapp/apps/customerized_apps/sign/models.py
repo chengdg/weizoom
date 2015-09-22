2e615483783b3c2f96222af6add607f720643e5e
Asia <aixiang@narzh.com>
2015-09-22 09:42:33 +0800

    更新签到codebase

--- weapp/apps/customerized_apps/sign/models.py
+++ weapp/apps/customerized_apps/sign/models.py
@@ -11,7 +11,10 @@ class SignParticipance(models.Document):
 	tel = models.StringField(default="", max_length=100)
 	termite_data = models.DynamicField(default="") #termite数据
 	prize = models.DynamicField(default="") #活动奖励
-	created_at = models.DateTimeField() #创建时间
+	created_at = models.DateTimeField() #创建时间&第一次签到时间
+	total_count = models.IntField(default=0) #总签到天数
+	serial_count = models.IntField(default=0) #连续签到天数
+	top_serial_count = models.IntField(default=0) #最高连续签到天数
 
 	meta = {
 		'collection': 'sign_sign_participance'
@@ -24,8 +27,11 @@ STATUS_STOPED = 2
 class Sign(models.Document):
 	owner_id = models.LongField() #创建人id
 	name = models.StringField(default="", max_length=100) #名称
-	start_time = models.DateTimeField() #开始时间
-	end_time = models.DateTimeField() #结束时间
+	# start_time = models.DateTimeField() #开始时间
+	# end_time = models.DateTimeField() #结束时间
+	share = models.DynamicField(default="") #分享设置
+	reply = models.DynamicField(default="") #自动回复设置
+	signment = models.DynamicField(default="") #签到设置
 	status = models.IntField(default=0) #状态
 	participant_count = models.IntField(default=0) #参与者数量
 	related_page_id = models.StringField(default="", max_length=100) #termite page的id
