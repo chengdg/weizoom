317859e019bc6c03035fdcd50a501a1bfd728ee9
Eugene <237192896@qq.com>
2015-10-12 19:34:38 +0800

    添加打印

--- weapp/mall/signal_handler.py
+++ weapp/mall/signal_handler.py
@@ -429,7 +429,12 @@ def coupon_pre_save_order(pre_order, order, products, product_groups, owner_id=N
     if promotion_models.RedEnvelopeParticipences.objects.filter(coupon_id=coupon[0].id).count() > 0:
         print "22222222222222222222=========================="
         red_envelope2member = promotion_models.RedEnvelopeParticipences.objects.get(coupon_id=coupon[0].id)
-        print "333333333333333333", red_envelope2member
+        print "333333333333333333", promotion_models.RedEnvelopeParticipences.objects.filter(
+                    red_envelope_rule_id=red_envelope2member.red_envelope_rule_id,
+                    red_envelope_relation_id=red_envelope2member.red_envelope_relation_id,
+                    member_id=red_envelope2member.introduced_by,
+                    introduced_by=0
+        )
         promotion_models.RedEnvelopeParticipences.objects.filter(
                     red_envelope_rule_id=red_envelope2member.red_envelope_rule_id,
                     red_envelope_relation_id=red_envelope2member.red_envelope_relation_id,
