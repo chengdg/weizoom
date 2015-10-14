f0f52873638a5144fd6d23f8db00149c5b73606c
Mark24 <mark.zhangyoung@gmail.com>
2015-10-14 18:12:08 +0800

    签到：关键字，插入正常，待添加删除，编辑等

--- weapp/static_v2/js/view/apps_prize_keyword/view.js
+++ weapp/static_v2/js/view/apps_prize_keyword/view.js
@@ -8,6 +8,7 @@
 ensureNS('W.view.apps');
 W.view.apps.PrizeKeyword = Backbone.View.extend({
 	el: '',
+	keyword_div:'',
 
 	events: {
 		'keypress .xa-app-add': 'onPressEnter',
@@ -21,9 +22,7 @@ W.view.apps.PrizeKeyword = Backbone.View.extend({
         this.md = "精确匹配";
         this.type = 0;
         this.template = Handlebars.compile($("#addkey-view-tmpl-src").html());
-		this.prize_keypress_num = 0;
-		this.prize_keypress_arr =[];
-		//this.$add_keyword_btn = $(options.add_btn);
+		this.$keyword_div = $(options.keyword_div);
 		this.render();
 	},
 	render: function() {
@@ -38,60 +37,46 @@ W.view.apps.PrizeKeyword = Backbone.View.extend({
 
 	onPressEnter: function (e) {
 		if (e.keyCode == 13) {
-			this.prize_keypress_num += 1;
-			this.prize_keypress_arr.push(this.prize_keypress_num);
-			////this.$el.unbind();
-			//prize_keypress_num += 1;
-			////替换相邻多个空格为一个
-			//if(prize_keypress_num==1){
-			//var keyword = this.$('.xa-app-add').val().trim().replace(/\s+/g, ' ');
-			//if(keyword == '') {
-			//	this.$('.xa-errorHint').html('关键字不能为空！');
-			//	return;
-			//}
-            //
-			//if(keyword.length > 15) {
-			//	this.$('.xa-errorHint').html('单个关键词字数不能超过15个字');
-			//	return;
-			//}
-			//
-			//var keywords = [];
-			//$('#' + this.id).find($('.xa-editeTable .data-keyword')).each(function(){
-			//	keywords.push($(this).text().trim());
-			//});
-			//this.$el.find($('.data-keyword')).each(function(){
-			//	keywords.push($(this).text().trim());
-			//});
-            //
-			////判断关键词个数是否超过10个
-			//if (keywords.length > (10 - 1)) {
-			//	W.showHint('error', '关键词个数不能超过10个');
-			//	return;
-			//}
-            //
-			//var args = {
-			//	keyword: keyword,
-			//	keywords: JSON.stringify(keywords)
-			//};
-			//console.log('---------------');
-            //
-            //var _this = this;
-			//console.log(_this);
-			//var pattern = {
-			//			keyword: _this.$('.xa-app-add').val(),
-			//			mode: _this.md,
-			//			type: _this.type
-			//		};
-			//var key_el = $('.xui-propertyView-app-ReplyGroup .propertyGroup_property_horizontalField .propertyGroup_property_input span');
-            //
-			////$('.xa-keywords').append(_this.template(pattern));
-            //
-			//	key_el.append(_this.template(pattern));
-			//	_this.$('.xa-app-add').val("");
-			//}
-			//console.log(prize_keypress_num);
-		console.log(this.prize_keypress_num);
-		console.log(this.prize_keypress_arr);
+			//替换相邻多个空格为一个
+			var keyword = this.$('.xa-app-add').val().trim().replace(/\s+/g, ' ');
+			if(keyword == '') {
+				W.showHint('error','关键字不能为空！');
+				return;
+			}
+
+			if(keyword.length > 5) {
+				W.showHint('error','单个关键词字数不能超过5个字');
+				return;
+			}
+
+			var keywords = [];
+			$('#' + this.id).find($('.xa-editeTable .data-keyword')).each(function(){
+				keywords.push($(this).text().trim());
+			});
+			this.$el.find($('.data-keyword')).each(function(){
+				keywords.push($(this).text().trim());
+			});
+
+			//判断关键词个数是否超过4个
+			if (keywords.length > (4 - 1)) {
+				W.showHint('error', '关键词个数不能超过4个');
+				return;
+			}
+
+			var args = {
+				keyword: keyword,
+				keywords: JSON.stringify(keywords)
+			};
+
+            var _this = this;
+			var pattern = {
+						keyword: _this.$('.xa-app-add').val(),
+						mode: _this.md,
+						type: _this.type
+					};
+			//$('.xa-keywords').append(_this.template(pattern));
+			this.$keyword_div.append(_this.template(pattern));
+			_this.$('.xa-app-add').val("");
 		}
 
 	},
@@ -107,46 +92,88 @@ W.view.apps.PrizeKeyword = Backbone.View.extend({
 	},
 
 	onClickSubmit: function(){
-		this.$('.xa-errorHint').html('');
-		//替换相邻多个空格为一个
-		var keyword = this.$('.xa-app-add').val().trim().replace(/\s+/g, ' ');
 
-		if(keyword == '') {
-			var temp = this.$el.find('.xa-keywords').html();
-			$('#' + this.id).find('.xa-patternapend').append(temp);
-			if(this.$el.find('.data-keyword').length == 0) {
-				W.showHint('error', '关键词不能为空');
+			var keyword = this.$('.xa-app-add').val().trim().replace(/\s+/g, ' ');
+			if(keyword == '') {
+				W.showHint('error','关键字不能为空！');
 				return;
 			}
-			$('#' + this.id).find('.xa-hint').hide();
+
+			if(keyword.length > 5) {
+				W.showHint('error','单个关键词字数不能超过5个字');
+				return;
+			}
+
+			var keywords = [];
+			$('#' + this.id).find($('.xa-editeTable .data-keyword')).each(function(){
+				keywords.push($(this).text().trim());
+			});
+			this.$el.find($('.data-keyword')).each(function(){
+				keywords.push($(this).text().trim());
+			});
+
+			//判断关键词个数是否超过4个
+			if (keywords.length > (4 - 1)) {
+				W.showHint('error', '关键词个数不能超过4个');
+				return;
+			}
+
+			var args = {
+				keyword: keyword,
+				keywords: JSON.stringify(keywords)
+			};
+
+            var _this = this;
+			var pattern = {
+						keyword: _this.$('.xa-app-add').val(),
+						mode: _this.md,
+						type: _this.type
+					};
+			//$('.xa-keywords').append(_this.template(pattern));
+			this.$keyword_div.append(_this.template(pattern));
+			_this.$('.xa-app-add').val("");
 			this.hide();
-			return;
-		}
 
-		if(keyword.length > 15) {
-			W.showHint('error', '单个关键词字数不能超过15个字');
-			return;
-		}
-
-		var keywords = [];
-		$('#' + this.id).find($('.xa-editeTable .data-keyword')).each(function(){
-			keywords.push($(this).text().trim());
-		});
-		this.$el.find($('.data-keyword')).each(function(){
-			keywords.push($(this).text().trim());
-		});
-
-		//判断关键词个数是否超过10个
-		if (keywords.length > (10 - 1)) {
-			W.showHint('error', '关键词个数不能超过10个');
-			return;
-		}
-		
-		var args = {
-			keyword: keyword,
-			keywords: JSON.stringify(keywords)
-		};
-	    var _this = this;
+        //this.$('.xa-errorHint').html('');
+        ////替换相邻多个空格为一个
+        //var keyword = this.$('.xa-app-add').val().trim().replace(/\s+/g, ' ');
+        //
+        //if(keyword == '') {
+			//var temp = this.$el.find('.xa-keywords').html();
+			//$('#' + this.id).find('.xa-patternapend').append(temp);
+			//if(this.$el.find('.data-keyword').length == 0) {
+			//	W.showHint('error', '关键词不能为空');
+			//	return;
+			//}
+			//$('#' + this.id).find('.xa-hint').hide();
+			//this.hide();
+			//return;
+        //}
+        //
+        //if(keyword.length > 15) {
+			//W.showHint('error', '单个关键词字数不能超过15个字');
+			//return;
+        //}
+        //
+        //var keywords = [];
+        //$('#' + this.id).find($('.xa-editeTable .data-keyword')).each(function(){
+			//keywords.push($(this).text().trim());
+        //});
+        //this.$el.find($('.data-keyword')).each(function(){
+			//keywords.push($(this).text().trim());
+        //});
+        //
+        ////判断关键词个数是否超过10个
+        //if (keywords.length > (10 - 1)) {
+			//W.showHint('error', '关键词个数不能超过10个');
+			//return;
+        //}
+        //
+        //var args = {
+			//keyword: keyword,
+			//keywords: JSON.stringify(keywords)
+        //};
+	    //var _this = this;
 		//W.getApi().call({
 		//	app: 'new_weixin',
 		//	resource: 'keyword_rules',
