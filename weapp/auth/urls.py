32ae2d705e6cfdefe82f8933ffea93def9c22671
duhao <duhao@weizoom.com>
2015-10-19 16:41:05 +0800

    去除权限auth相关代码，已注释掉

--- weapp/auth/urls.py
+++ weapp/auth/urls.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
+# #duhao 20151019全部注释
+# from django.conf.urls import *
 
-from django.conf.urls import *
+# import account_views
+# import account_api_views
+# import role_views
+# import role_api_views
+# import permission_api_views
 
-import account_views
-import account_api_views
-import role_views
-import role_api_views
-import permission_api_views
-
-urlpatterns = patterns('',
-)
+# urlpatterns = patterns('',
+# )
