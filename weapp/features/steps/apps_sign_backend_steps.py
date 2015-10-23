51b2311d6732e36b314a12284b87dea818d72bc3
Mark24 <mark.zhangyoung@gmail.com>
2015-10-23 14:42:02 +0800

    add apps_sign_backend_steps

---
+++ weapp/features/steps/apps_sign_backend_steps.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'mark24'
+
+from behave import *
+from test import bdd_util
+
+from features.testenv.model_factory import *
+import steps_db_util
+#from mall import module_api as mall_api
+from mall.promotion import models as  promotion_models
+from modules.member import module_api as member_api
+from utils import url_helper
+import datetime as dt
+from mall.promotion.models import CouponRule
+from weixin.message.material import models as material_models
