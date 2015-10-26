1d5104d8ad6265454bf6373d2304264c4c4b2242
lei <352773277@qq.com>
2015-10-26 10:07:05 +0800

    [f_mall_4859][lei]解决需求 4859 404和无商品页面跳转更改

--- weapp/core/middleware.py
+++ weapp/core/middleware.py
@@ -98,7 +98,6 @@ class LocalCacheMiddleware(object):
 		webapp_cache.local_cache = {}
 		return response
 
-
 # #===============================================================================
 # # WebappOwnerCacheMiddleware : 管理webapp owner相关信息的中间件
 # #===============================================================================
