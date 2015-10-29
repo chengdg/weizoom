46f576b62d41593fa755e27d58a5006d5275a5a9
江哲 <jiangzhe@weizoom.com>
2015-10-29 20:34:37 +0800

    修复积分优惠促销标题为空时会覆盖商品促销标题的BUG

--- weapp/mall/module_api.py
+++ weapp/mall/module_api.py
@@ -460,7 +460,8 @@ def get_product_detail_for_cache(webapp_owner_id, product_id, member_grade_id=No
 				# integral_sale.end_date = integral_sale.end_date.strftime('%Y-%m-%d %H:%M:%S')
 				# integral_sale.created_at = integral_sale.created_at.strftime('%Y-%m-%d %H:%M:%S')
 				# integral_sale.start_date = integral_sale.start_date.strftime('%Y-%m-%d %H:%M:%S')
-				product.promotion_title = integral_sale.promotion_title
+				if integral_sale.promotion_title:
+					product.promotion_title = integral_sale.promotion_title
 				product.integral_sale = integral_sale.to_dict('detail', 'type_name')
 			else:
 				product.integral_sale = None
