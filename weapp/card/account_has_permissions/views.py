37bb25c89b12c82df1a1921ea677c0fdb9f09c55
zhupenghui <443581214@qq.com>
2015-11-03 08:53:04 +0800

    quanxian

--- weapp/card/account_has_permissions/views.py
+++ weapp/card/account_has_permissions/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 
 from core.restful_url_route import view
-
+from market_tools.tools.weizoom_card.models import *
 from card import export
 
 ########################################################################
@@ -14,12 +14,15 @@ from card import export
 @login_required
 @view(app='card', resource='permissions', action='get')
 def edit_weizoom_card_account_permission(request):
+    weizoomcardpermission=WeiZoomCardPermission.objects.filter(user_id=request.user.id)
+    can_change_shop_config=weizoomcardpermission[0].can_change_shop_config
     c = RequestContext(request, {
         'first_nav_name': export.MALL_CARD_FIRST_NAV,
         'second_navs': export.get_card_second_navs(request),
-        'second_nav_name': export.MALL_CARD_MANAGER_NAV
+        'second_nav_name': export.MALL_CARD_MANAGER_NAV,
+        'can_change_shop_config': can_change_shop_config,
     })
-    return render_to_response('card/editor/edit_account_has_permissions.html', c)
+    return render_to_response('card/editor/edit_account_has_permissions.html',c )
 
 @login_required
 @view(app='card', resource='cardmanager', action='get')
