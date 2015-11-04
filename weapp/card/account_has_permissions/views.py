e314fd34cacb4991fcb0eaf0306b5f8d8a789781
zhupenghui <443581214@qq.com>
2015-11-04 13:28:49 +0800

    修改activebug

--- weapp/card/account_has_permissions/views.py
+++ weapp/card/account_has_permissions/views.py
@@ -32,6 +32,6 @@ def edit_weizoom_card_account_permission(request):
     c = RequestContext(request, {
         'first_nav_name': export.MALL_CARD_FIRST_NAV,
         'second_navs': export.get_card_second_navs(request),
-        'second_nav_name': export.MALL_CARD_MANAGER_NAV
+        'second_nav_name': export.MALL_CARD_PERMISSIONS_NAV
     })
     return render_to_response('card/editor/list_weizoom_card_manager.html', c)
