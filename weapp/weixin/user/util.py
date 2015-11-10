605c756ae6faffca4448341d619edfc997efc511
郭玉成 <1597594211@qq.com>
2015-11-10 10:12:12 +0800

    修改docker第三方授权

--- weapp/weixin/user/util.py
+++ weapp/weixin/user/util.py
@@ -14,13 +14,13 @@ def get_component_info_from(request):
 	elif request_host == 'weixin.weapp.weizzz.com':
 		component_info = ComponentInfo.objects.filter(app_id='wxba6fccbdcccbea49')[0]
 	elif request_host == 'docker.test.weizzz.com':
-		component_info = ComponentInfo.objects.filter(app_id='wx9b89fe19768a02d2')[0]
+		component_info = ComponentInfo.objects.filter(app_id='wx984abb2d00cc47b8')[0]
 	elif request_host == 'red.weapp.weizzz.com':
 		component_info = ComponentInfo.objects.filter(app_id='wxa823309fb403f661')[0]
 	elif request_host == 'nj.weapp.weizzz.com':
 		component_info = ComponentInfo.objects.filter(app_id='wxb04d4cdf32b59823')[0]
 	elif request_host == 'yanshi.weizoom.com':
-		component_info = ComponentInfo.objects.filter(app_id='wx984abb2d00cc47b8')[0]
+		component_info = ComponentInfo.objects.filter(app_id='wxe8bdf8eb296716f2')[0]
 	else:
 		component_info = ComponentInfo.objects.filter(app_id='wx8209f1f63f0b1d26')[0]
 
