3ce8cab01f6129d78d645f81ee63ae80bb5cfb70
victor <gaoliqi@weizoom.com>
2015-11-19 01:32:34 +0000

    从wapi_for_bigdata同步mall/outline/util.py

--- weapp/mall/outline/utils.py
+++ weapp/mall/outline/utils.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from mall import models as mall_models
 from django.db.models import Q
+
+from mall import models as mall_models
+from core.charts_apis import create_line_chart_response
+from datetime import timedelta, datetime
+from core import dateutil
 
 
 def get_to_be_shipped_order_infos(request):
@@ -62,3 +66,83 @@ def get_to_be_shipped_order_infos(request):
         "count": total_to_be_shipped_order_count,
         "orders_list": orders_list
     }
+
+
+def get_purchase_trend(webapp_id, low_date, high_date):
+    """
+    购买趋势折线图
+    从outline.py中迁移而来，为了便于供wapi中调用
+    duhao 20151026
+    """
+    date_fmt = "%Y-%m-%d"
+    today = dateutil.get_today()
+    if (not low_date) or (not high_date):
+        high_date = datetime.strptime(today, date_fmt).date()
+        low_date = datetime.strptime(dateutil.get_previous_date('today', 6), date_fmt).date()
+
+    if type(low_date) == unicode:
+        low_date = datetime.strptime(low_date, date_fmt).date()
+    if type(high_date) == unicode:
+        high_date = datetime.strptime(high_date, date_fmt).date()
+
+    date_list = [date.strftime(date_fmt) for date in dateutil.get_date_range_list(low_date, high_date)]
+    #当最后一天是今天时，折线图中不显示最后一天的数据 duhao 2015-09-17
+    #当起止日期都是今天时，数据正常显示
+    
+    if len(date_list) > 1 and date_list[-1] == today:
+        del date_list[-1]
+
+    date2count = dict()
+    date2price = dict()
+
+    # 11.20从查询mall_purchase_daily_statistics变更为直接统计订单表，解决mall_purchase_daily_statistics遗漏统计订单与统计时间不一样导致的统计结果不同的问题。
+    orders = mall_models.Order.objects.belong_to(webapp_id).filter(
+        created_at__range=(low_date, (high_date) + timedelta(days=1)))
+    statuses = set([mall_models.ORDER_STATUS_PAYED_SUCCESSED, mall_models.ORDER_STATUS_PAYED_NOT_SHIP,
+                    mall_models.ORDER_STATUS_PAYED_SHIPED, mall_models.ORDER_STATUS_SUCCESSED])
+    # orders = [order for order in orders if (order.type != 'test') and (order.status in statuses)]
+    for order in orders:
+        if (order.type != 'test') and (order.status in statuses):
+            # date = dateutil.normalize_date(order.created_at)
+            date = order.created_at.strftime(date_fmt)
+            if order.webapp_id != webapp_id:
+                order_price = mall_models.Order.get_order_has_price_number(order) + order.postage
+            else:
+                order_price = order.final_price + order.weizoom_card_money
+
+            if date in date2count:
+                old_count = date2count[date]
+                date2count[date] = old_count + 1
+            else:
+                date2count[date] = 1
+
+            if date in date2price:
+                old_price = date2price[date]
+                date2price[date] = old_price + order_price
+            else:
+                date2price[date] = order_price
+
+    count_trend_values = []
+    price_trend_values = []
+    for date in date_list:
+        count_trend_values.append(date2count.get(date, 0))
+        price_trend_values.append(round(date2price.get(date, 0.0), 2))
+    result = create_line_chart_response(
+        '',
+        '',
+        date_list,
+        [{
+            "name": "销售额",
+            "values": price_trend_values
+        }, {
+            "name": "订单数",
+            "values": count_trend_values
+        }],
+        use_double_y_lable = True,
+        get_json = True
+    )
+    result['yAxis'][0]['name'] = '销售额'
+    result['yAxis'][1]['name'] = '订单数'
+    result['yAxis'][1]['splitLine'] = {'show':False}
+
+    return result
