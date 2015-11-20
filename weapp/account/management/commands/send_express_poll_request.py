b22324981b2a557e11a22ef20378b1e0e7e8c5d2
liupeiyu <liupeiyu@weizoom.com>
2015-11-20 15:11:44 +0800

    [快递100] 改进订阅信息的service,部分状态不再发送请求

--- weapp/account/management/commands/send_express_poll_request.py
+++ weapp/account/management/commands/send_express_poll_request.py
@@ -9,6 +9,13 @@ from datetime import datetime
 
 """
 重新发送 由于网络或其它原因未发送成功的快递订阅请求
+
+EXPRESS_NOT_PULL_STATUSES = [
+	EXPRESS_PULL_REFUSE_STATUS,
+	EXPRESS_PULL_INFO_ERROR_STATUS,
+	EXPRESS_PULL_REPEAT_STATUS
+]
+express.status 在EXPRESS_NOT_PULL_STATUSES 内的不再发送订阅请求
 """
 
 class Command(BaseCommand):
@@ -22,6 +29,7 @@ class Command(BaseCommand):
 
 		print "----------------start"
 
+		skip_count = 0
 		count = 0
 		expresses = ExpressHasOrderPushStatus.objects.filter(receive_count=0)
 		for express in expresses:
@@ -40,6 +48,12 @@ class Command(BaseCommand):
 			if orders.count() > 0:
 				order = orders[0]
 
+				# 跳过
+				if express.status in EXPRESS_NOT_PULL_STATUSES:
+					skip_count = skip_count + 1
+					print u'-------- skip status in 700, 701, 501; id={}'.format(express.id)
+					continue
+
 				if express.abort_receive_message and len(express.abort_receive_message) > 0:
 					now = datetime.now()
 					minute = (now - express.abort_receive_at).seconds/60
@@ -50,13 +64,13 @@ class Command(BaseCommand):
 						continue
 						
 				is_success = ExpressPoll(order).get_express_poll()
-				if not is_success:
-					try:
+				try:					
+					if not is_success:
 						print u"!!!! error send express poll express_id:{}, number:{}, order_id:{}".format(express.id, express.express_number, order.id)
-					except:
-						print u"!!!! error send express poll express_id:{}, order_id:{}".format(express.id, order.id)
-				else:
-					print u"success send express poll express_id:{}, number:{}, order_id:{}".format(express.id, express.express_number, order.id)
-					count = count + 1
+					else:
+						count = count + 1					
+						print u"success send express poll express_id:{}, number:{}, order_id:{}".format(express.id, express.express_number, order.id)
+				except:
+					print u"!!!!!!!! system error express_id:{}".format(express.id)
 
-		return u"OK send express length is {}".format(count)
+		return u"OK send express length is count={}; skip_count={}".format(count, skip_count)
