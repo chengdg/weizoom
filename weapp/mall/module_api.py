e5eeb2f14474b68503096cad606c34496b696f45
duhao <duhao@weizoom.com>
2015-11-23 16:44:20 +0800

    完善订单已完成时间的记录功能

--- weapp/mall/module_api.py
+++ weapp/mall/module_api.py
@@ -2250,6 +2250,8 @@ def update_order_status(user, action, order, request=None):
 		actions = action.split('-')
 		if operation_name:
 			operation_name = u'{} {}'.format(operation_name, (actions[1] if len(actions) > 1 else ''))
+		else:
+			operation_name = actions[1] if len(actions) > 1 else ''
 		#更新红包引入消费金额的数据 by Eugene
 		if order.coupon_id and promotion_models.RedEnvelopeParticipences.objects.filter(coupon_id=order.coupon_id, introduced_by__gt=0).count() > 0:
 			red_envelope2member = promotion_models.RedEnvelopeParticipences.objects.get(coupon_id=order.coupon_id)
