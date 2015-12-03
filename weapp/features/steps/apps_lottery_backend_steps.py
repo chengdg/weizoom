c1275733a0373dc8bf0ef9edb59c61f105198163
Mark24 <mark.zhangyoung@qq.com>
2015-12-03 16:01:08 +0800

    新建功能OK

--- weapp/features/steps/apps_lottery_backend_steps.py
+++ weapp/features/steps/apps_lottery_backend_steps.py
@@ -12,7 +12,7 @@ from mall.promotion import models as  promotion_models
 from modules.member import module_api as member_api
 from utils import url_helper
 import datetime as dt
-from market_tools.tools.channel_qrcode.models import ChannelQrcodeSettings
+from mall.promotion.models import CouponRule
 from weixin.message.material import models as material_models
 from apps.customerized_apps.lottery import models as lottery_models
 import termite.pagestore as pagestore_manager
@@ -34,25 +34,204 @@ def __debug_print(content,type_tag=True):
 		pass
 
 
-# def __get_lotteryPageJson(args):
-# 	"""
-# 	传入参数，获取模板
-# 	"""
-# 	__page_temple = {
+def __get_lotteryPageJson(args):
+	"""
+	传入参数，获取模板
+	"""
+	__debug_print(args)
+	__page_temple = {
+		"type": "appkit.page",
+		"cid": 1,
+		"pid": None,
+		"auto_select": False,
+		"selectable": "yes",
+		"force_display_in_property_view": "no",
+		"has_global_content": "no",
+		"need_server_process_component_data": "no",
+		"is_new_created": True,
+		"property_view_title": "背景",
+		"model": {
+			"id": "",
+			"class": "",
+			"name": "",
+			"index": 1,
+			"datasource": {
+				"type": "api",
+				"api_name": ""
+			},
+			"content_padding": "15px",
+			"title": "index",
+			"event:onload": "",
+			"uploadHeight": "568",
+			"uploadWidth": "320",
+			"site_title": "微信抽奖",
+			"background": ""
+		},
+		"components": [
+			{
+				"type": "appkit.lotterydescription",
+				"cid": 2,
+				"pid": 1,
+				"auto_select": False,
+				"selectable": "yes",
+				"force_display_in_property_view": "no",
+				"has_global_content": "no",
+				"need_server_process_component_data": "no",
+				"property_view_title": "微信抽奖",
+				"model": {
+					"id": "",
+					"class": "",
+					"name": "",
+					"index": 2,
+					"datasource": {
+						"type": "api",
+						"api_name": ""
+					},
+					"title": args['title'],
+					"start_time": args['start_time'],
+					"end_time": args['end_time'],
+					"valid_time": args['valid_time'],
+					"description": args['description'],
+					"expend": args['expend'],
+					"delivery": args['delivery'],
+					"delivery_setting": args['delivery_setting'],
+					"limitation": args['limitation'],
+					"chance": args['chance'],
+					"allow_repeat": args['allow_repeat'],
+					"items": [
+						4,
+						5,
+						6
+					]
+				},
+				"components": [
+					{
+						"type": "appkit.lotteryitem",
+						"cid": 4,
+						"pid": 2,
+						"auto_select": False,
+						"selectable": "no",
+						"force_display_in_property_view": "no",
+						"has_global_content": "no",
+						"need_server_process_component_data": "no",
+						"property_view_title": "",
+						"model": {
+							"id": "",
+							"class": "",
+							"name": "",
+							"index": 3,
+							"datasource": {
+								"type": "api",
+								"api_name": ""
+							},
+							"title": "一等奖",
+							"prize_count": args['prize_settings'][0]['prize_count'],
+							"prize": args['prize_settings'][0]['prize'],
+							"image": args['prize_settings'][0]['image']
+						},
+						"components": []
+					},
+					{
+						"type": "appkit.lotteryitem",
+						"cid": 5,
+						"pid": 2,
+						"auto_select": False,
+						"selectable": "no",
+						"force_display_in_property_view": "no",
+						"has_global_content": "no",
+						"need_server_process_component_data": "no",
+						"property_view_title": "",
+						"model": {
+							"id": "",
+							"class": "",
+							"name": "",
+							"index": 3,
+							"datasource": {
+								"type": "api",
+								"api_name": ""
+							},
+							"title": "二等奖",
+							"prize_count": args['prize_settings'][1]['prize_count'],
+							"prize": args['prize_settings'][1]['prize'],
+							"image": args['prize_settings'][1]['image']
+						},
+						"components": []
+					},
+					{
+						"type": "appkit.lotteryitem",
+						"cid": 6,
+						"pid": 2,
+						"auto_select": False,
+						"selectable": "no",
+						"force_display_in_property_view": "no",
+						"has_global_content": "no",
+						"need_server_process_component_data": "no",
+						"property_view_title": "",
+						"model": {
+							"id": "",
+							"class": "",
+							"name": "",
+							"index": 3,
+							"datasource": {
+								"type": "api",
+								"api_name": ""
+							},
+							"title": "三等奖",
+							"prize_count": args['prize_settings'][2]['prize_count'],
+							"prize": args['prize_settings'][2]['prize'],
+							"image": args['prize_settings'][2]['image']
+						},
+						"components": []
+					}
+				]
+			},
+			{
+				"type": "appkit.submitbutton",
+				"cid": 3,
+				"pid": 1,
+				"auto_select": False,
+				"selectable": "no",
+				"force_display_in_property_view": "no",
+				"has_global_content": "no",
+				"need_server_process_component_data": "no",
+				"property_view_title": "",
+				"model": {
+					"id": "",
+					"class": "",
+					"name": "",
+					"index": 99999,
+					"datasource": {
+						"type": "api",
+						"api_name": ""
+					},
+					"text": "提交"
+				},
+				"components": []
+			}
+		]
+	}
+	return json.dumps(__page_temple)
 
-# 		}
-# 	return json.dumps(__page_temple)
+def __bool2Bool(bo):
+	"""
+	JS字符串布尔值转化为Python布尔值
+	"""
+	bool_dic = {'true':True,'false':False,'True':True,'False':False}
+	if bo:
+		result = bool_dic[bo]
+	else:
+		result = None
+	return result
 
-# def __bool2Bool(bo):
-# 	"""
-# 	JS字符串布尔值转化为Python布尔值
-# 	"""
-# 	bool_dic = {'true':True,'false':False,'True':True,'False':False}
-# 	if bo:
-# 		result = bool_dic[bo]
-# 	else:
-# 		result = None
-# 	return result
+def __name2Bool(name):
+	"""
+	"是"--> True
+	"""
+	name_dic = {u'是':"true",u'不是':"false"}
+	if name:
+		return name_dic[name]
+	else:
+		return None
 
 # def __date_delta(start,end):
 # 	"""
@@ -65,22 +244,148 @@ def __debug_print(content,type_tag=True):
 # 	end = dt.datetime.strptime(end, "%Y-%m-%d").date()
 # 	return (end-start).days
 
-# def __date2time(date_str):
-# 	"""
-# 	字符串 今天/明天……
-# 	转化为字符串 "%Y-%m-%d %H:%M"
-# 	"""
-# 	cr_date = date_str
-# 	p_time = "{} 00:00".format(bdd_util.get_date_str(cr_date))
-# 	return p_time
+def __date2time(date_str):
+	"""
+	字符串 今天/明天……
+	转化为字符串 "%Y-%m-%d %H:%M"
+	"""
+	cr_date = date_str
+	p_time = "{} 00:00".format(bdd_util.get_date_str(cr_date))
+	return p_time
 
-# def __datetime2str(dt_time):
-# 	"""
-# 	datetime型数据，转为字符串型，日期
-# 	转化为字符串 "%Y-%m-%d %H:%M"
-# 	"""
-# 	dt_time = dt.datetime.strftime(dt_time, "%Y-%m-%d %H:%M")
-# 	return dt_time
+def __datetime2str(dt_time):
+	"""
+	datetime型数据，转为字符串型，日期
+	转化为字符串 "%Y-%m-%d %H:%M"
+	"""
+	dt_time = dt.datetime.strftime(dt_time, "%Y-%m-%d %H:%M")
+	return dt_time
+
+def __limit2name(limit):
+	"""
+	传入积分规则，返回名字
+	"""
+	limit_dic={
+	"once_per_user":u"一人一次",
+	"once_per_day":u"一天一次",
+	"twice_per_day":u"一天两次",
+	"no_limit":u"不限"
+	}
+	if limit:
+		return limit_dic[limit]
+	else:
+		return ""
+
+def __name2limit(name):
+	"""
+	传入积分名字，返回积分规则
+	"""
+	name_dic={
+		u"一人一次":"once_per_user",
+		u"一天一次":"once_per_day",
+		u"一天两次":"twice_per_day",
+		u"不限":"no_limit"
+	}
+	if name:
+		return name_dic[name]
+	else:
+		return ""
+
+def __name2type(name):
+	type_dic = {
+		u"积分":"integral",
+		u"优惠券":"coupon",
+		u"实物":"entity"
+	}
+	if name:
+		return type_dic[name]
+	else:
+		return ""
+
+def __delivery2Bool(name):
+	d_dic ={
+		u"所有用户":"false",
+		u'仅限未中奖用户':"true"
+	}
+
+	if name:
+		return d_dic[name]
+	else:
+		return ""
+
+def __get_coupon_json(coupon_rule_name):
+	"""
+	获取优惠券json
+	"""
+	coupon_rule = promotion_models.CouponRule.objects.get(name=coupon_rule_name)
+	coupon ={
+		"id":coupon_rule.id,
+		"count":coupon_rule.count,
+		"name":coupon_rule.name
+	}
+	return coupon
+
+def __get_coupon_rule_id(coupon_rule_name):
+	"""
+	获取优惠券id
+	"""
+	coupon_rule = promotion_models.CouponRule.objects.get(name=coupon_rule_name)
+	return coupon_rule.id
+
+def __prize_settings_process(prize_settings):
+	"""
+	处理prize_settings
+
+	Tag为page，返回page的prize字典
+	Tage为lottery,返回lottery_lottery的prize字典
+	"""
+
+	page_prize_list = []
+	lottery_prize_list = []
+
+	if prize_settings:
+		index = 0
+		plist = [u'一等奖',u'二等奖',u'三等奖']
+		for prize_setting in prize_settings:
+			#Page
+			page_prize_dic = {}
+			page_prize_dic['title'] = prize_setting.get("title","")
+			page_prize_dic['prize_count'] = prize_setting.get("prize_counts","")
+			page_prize_dic['image'] = prize_setting.get("pic","")
+
+			page_prize_dic['prize'] = {}
+			prize_type = __name2type(prize_setting.get("prize_type"))
+			if prize_type == "integral":
+				prize_data = prize_setting.get("integral")
+			elif prize_type == "coupon":
+				coupon_name = prize_setting.get("coupon")
+				coupon_id = __get_coupon_rule_id(coupon_name)
+				prize_data = {
+					"id":coupon_id,
+					"name":coupon_name
+				}
+			elif prize_type == "entity":
+				prize_data = prize_setting.get("gift","")
+			else:
+				prize_data = ""
+			page_prize_dic['prize']["type"] = prize_type
+			page_prize_dic['prize']["data"] = prize_data
+
+			page_prize_list.append(page_prize_dic)
+
+			#lottery_lottery
+			lottery_prize_dic = {}
+			lottery_prize_dic[plist[index]] = {
+				"title":prize_setting.get("title",""),
+				"prize_count":prize_setting.get("prize_counts",""),
+				"prize_type":prize_type,
+				"prize_data":prize_data
+			}
+			index += 1
+
+		return (page_prize_list,lottery_prize_dic)
+	else:
+		return []
 
 # def __lottery_name2id(name):
 # 	"""
@@ -147,57 +452,42 @@ def __Create_Lottery(context,text,user):
 
 	valid_time = "%s~%s"%(start_time,end_time)
 
-	timing_status = text.get("is_show_countdown","")
-
-	timing_value_day = __date_delta(start_date,end_date)
-
-	description = text.get("desc","")
-	reply_content = text.get("reply")
-	material_image = text.get("share_pic","")
-	background_image = text.get("background_pic","")
-
-	qrcode_name = text.get("qr_code","")
-	if qrcode_name:
-		qrcode_id = ChannelQrcodeSettings.objects.get(owner_id=context.webapp_owner_id, name=qrcode_name).id
-		qrcode_i_url = '/new_weixin/qrcode/?setting_id=%s' % str(qrcode_id)
-		qrcode_response = context.client.get(qrcode_i_url)
-		qrcode_info = qrcode_response.context['qrcode']
-		qrcode_ticket_url = "https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket={}".format(qrcode_info.ticket)
-		qrcode = {"ticket":qrcode_ticket_url,"name":qrcode_info.name}
-	else:
-		qrcode = {"ticket":"","name":""}
-
-	zhcolor = text.get("background_color","冬日暖阳")
-	color = __name2color(zhcolor)
-
-	rules = text.get("rules","")
+	desc = text.get('desc','')#描述
+	reduce_integral = text.get('reduce_integral',0)#消耗积分
+	send_integral = text.get('send_integral',0)#参与送积分
+	send_integral_rules = text.get('send_integral_rules',"")#送积分规则
+	lottery_limit = __name2limit(text.get('lottery_limit',u'一人一次'))#抽奖限制
+	win_rate = text.get('win_rate','0%').split('%')[0]#中奖率
+	is_repeat_win = __name2Bool(text.get('is_repeat_win',"true"))#重复中奖
+	expect_prize_settings_list = text.get('prize_settings',[])
+	page_prize_settings,lottery_prize_settings = __prize_settings_process(expect_prize_settings_list)
 
 	page_args = {
 		"title":title,
 		"start_time":start_time,
 		"end_time":end_time,
 		"valid_time":valid_time,
-		"timing_status":timing_status,
-		"timing_value_day":timing_value_day,
-		"description":description,
-		"reply_content":reply_content,
-		"qrcode":qrcode,
-		"material_image":material_image,
-		"background_image":background_image,
-		"color":color,
-		"rules":rules
+		"description":desc,#描述
+		"expend":reduce_integral,#消耗积分
+		"delivery":send_integral,#参与送积分
+		"delivery_setting":__delivery2Bool(send_integral_rules),#送积分规则
+		"limitation":lottery_limit,#抽奖限制
+		"chance":win_rate,#中奖率
+		"allow_repeat":is_repeat_win,#重复中奖
+		"prize_settings":page_prize_settings
 	}
-
 	#step1：登录页面，获得分配的project_id
-	get_pw_response = context.client.get("/apps/lottery/lottery/")
-	pw_args_response = get_pw_response.context
-	project_id = pw_args_response['project_id']#(str){new_app:lottery:0}
+	get_lottery_response = context.client.get("/apps/lottery/lottery/")
+	lottery_args_response = get_lottery_response.context
+	project_id = lottery_args_response['project_id']#(str){new_app:lottery:0}
+	__debug_print(project_id)
 
 	#step2: 编辑页面获得右边的page_json
 	dynamic_url = "/apps/api/dynamic_pages/get/?design_mode={}&project_id={}&version={}".format(design_mode,project_id,version)
 	dynamic_response = context.client.get(dynamic_url)
 	dynamic_data = dynamic_response.context#resp.context=> data ; resp.content => Http Text
 
+	print 'OOOOOOOOOOOOOOKKKKKKKKKKKK!!!!!!!!!!!!!!'
 	#step3:发送Page
 	page_json = __get_lotteryPageJson(page_args)
 
@@ -217,10 +507,13 @@ def __Create_Lottery(context,text,user):
 		"name":title,
 		"start_time":start_time,
 		"end_time":end_time,
-		"timing":timing_status,
-		"reply_content":reply_content,
-		"material_image":material_image,
-		"qrcode":json.dumps(qrcode),
+		"expend":reduce_integral,#消耗积分
+		"delivery":send_integral,#参与送积分
+		"delivery_setting":__delivery2Bool(send_integral_rules),#送积分规则
+		"limitation":lottery_limit,#抽奖限制
+		"chance":win_rate,#中奖率
+		"allow_repeat":is_repeat_win,#重复中奖
+		"prize_settings":lottery_prize_settings,
 		"related_page_id":related_page_id
 	}
 	lottery_url ="/apps/lottery/api/lottery/?design_mode={}&project_id={}&version={}".format(design_mode,project_id,version)
@@ -422,14 +715,15 @@ def __Create_Lottery(context,text,user):
 
 
 
-@when(u'{user}新建抽奖活动')
+@when(u'{user}新建微信抽奖活动')
 def step_impl(context,user):
 	text_list = json.loads(context.text)
 	for text in text_list:
 		__Create_Lottery(context,text,user)
 
-# @then(u'{user}获得抽奖活动列表')
-# def step_impl(context,user):
+@then(u'{user}获得微信抽奖活动列表')
+def step_impl(context,user):
+	pass
 # 	design_mode = 0
 # 	count_per_page = 10
 # 	version = 1
