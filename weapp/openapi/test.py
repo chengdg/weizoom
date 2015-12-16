026a29bcfebf1e9f3e919acd3c6d4e009a57bfff
lei <352773277@qq.com>
2015-12-16 10:24:54 +0800

    老A接口增加分页支持

--- weapp/openapi/test.py
+++ weapp/openapi/test.py
@@ -4,26 +4,26 @@ import requests
 import json
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36'}
 
-# get_order =requests.post("http://weapp.weizoom.com/openapi/auth",data={'username':'ceshi01','password':'weizoom'},headers=headers)
-get_order =requests.post("http://dev.weapp.com/openapi/auth",data={'username':'jobs','password':'test'},headers=headers)
+# # get_order =requests.post("http://weapp.weizoom.com/openapi/auth",data={'username':'ceshi01','password':'weizoom'},headers=headers)
+get_order =requests.post("http://red.weapp.weizzz.com/openapi/auth",data={'username':'malla','password':'test'},headers=headers)
 print get_order.text
 
 # {"access_token": "WZT10_ff3fdc78a066b087044396990ce2c3a0"}
 
 # orders = requests.post("http://weapp.weizoom.com/openapi/order_info",data={"access_token": "WZT10_a8bcf60d2b4b6b35397eb2341ef94f09",'order_id':'20151208170349865'},headers=headers)
-orders = requests.post("http://dev.weapp.com/openapi/order_list",data={"access_token": "WZT10_f3518da15f89543e213d24ed0aa04b4f",'order_id':'20151211101850874'},headers=headers)
+orders = requests.post("http://red.weapp.weizzz.com/openapi/order_list",data={"access_token": "WZT10_ff3fdc78a066b087044396990ce2c3a0",'order_status':3,'cur_page':1},headers=headers)
 
 print orders.text
-
-orders = requests.post("http://dev.weapp.com/openapi/order_list",data={"access_token":"WZT10_f3518da15f89543e213d24ed0aa04b4f",
-																			  'found_begin_time':'2015-12-05 1:00:17','found_end_time':'2015-12-12 14:37:17'
- },headers=headers)
 #
-print len(json.loads(orders.text)['data'])
-
-orders = requests.post("http://dev.weapp.com/openapi/order_ship",data={"access_token":"WZT10_f3518da15f89543e213d24ed0aa04b4f",
-																	   'order_id':'20151211102941784','logistics_number':'1212121212121',
-																	   'logistics_name':'rufengda'
- },headers=headers)
+# orders = requests.post("http://dev.weapp.com/openapi/order_list",data={"access_token":"WZT10_f3518da15f89543e213d24ed0aa04b4f",
+# 																			  'found_begin_time':'2015-12-05 1:00:17','found_end_time':'2015-12-12 14:37:17'
+#  },headers=headers)
+# #
+# print len(json.loads(orders.text)['data'])
 #
-print orders.text
+# orders = requests.post("http://dev.weapp.com/openapi/order_ship",data={"access_token":"WZT10_f3518da15f89543e213d24ed0aa04b4f",
+# 																	   'order_id':'20151211102941784','logistics_number':'1212121212121',
+# 																	   'logistics_name':'rufengda'
+#  },headers=headers)
+# #
+# print orders.text
