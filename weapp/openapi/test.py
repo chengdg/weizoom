42063ef519851af8dbc63358a9137a6e50d8f072
lei <352773277@qq.com>
2015-12-17 18:28:37 +0800

    修改商品编码的特殊字符限定

--- weapp/openapi/test.py
+++ weapp/openapi/test.py
@@ -5,13 +5,13 @@ import json
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36'}
 
 # # get_order =requests.post("http://weapp.weizoom.com/openapi/auth",data={'username':'ceshi01','password':'weizoom'},headers=headers)
-get_order =requests.post("http://red.weapp.weizzz.com/openapi/auth",data={'username':'malla','password':'test'},headers=headers)
+get_order =requests.post("http://weapp.weizoom.com/openapi/auth",data={'username':'ceshi01','password':'weizoom'},headers=headers)
 print get_order.text
 
 # {"access_token": "WZT10_ff3fdc78a066b087044396990ce2c3a0"}
 
 # orders = requests.post("http://weapp.weizoom.com/openapi/order_info",data={"access_token": "WZT10_a8bcf60d2b4b6b35397eb2341ef94f09",'order_id':'20151208170349865'},headers=headers)
-orders = requests.post("http://red.weapp.weizzz.com/openapi/order_list",data={"access_token": "WZT10_ff3fdc78a066b087044396990ce2c3a0",'order_status':3,'cur_page':1},headers=headers)
+orders = requests.post("http://weapp.weizoom.com/openapi/order_list",data={"access_token": "WZT10_a8bcf60d2b4b6b35397eb2341ef94f09",'order_id':'20151217150256618','cur_page':1},headers=headers)
 
 print orders.text
 #
