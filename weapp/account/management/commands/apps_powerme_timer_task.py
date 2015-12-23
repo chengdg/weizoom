3a061d9c99e517c2a64d9e9496595db318e7aafc
likunlun <likunlun@weizoom.com>
2015-12-23 18:35:58 +0800

    修改更新助力脚本

--- weapp/account/management/commands/apps_powerme_timer_task.py
+++ weapp/account/management/commands/apps_powerme_timer_task.py
@@ -20,32 +20,30 @@ class Command(BaseCommand):
 		print 'powerme timer task start...'
 		start_time = time.time()
 
-		powerlog_bid2data = {p.belong_to: p for p in app_models.PowerLog.objects()}
 		need_del_powerlogs_ids = []
 		for powerme in app_models.PowerMe.objects():
 			record_id = str(powerme.id)
 			print '================update data start %s' % record_id
 			#统计助力值
-			if powerlog_bid2data.has_key(record_id):
-				power_logs = powerlog_bid2data[record_id]
-				power_member_ids = [p.power_member_id for p in power_logs]
-				member_id2subscribe = {m.id: m.is_subscribed for m in Member.objects.filter(id__in=power_member_ids)}
-				power_logs = [p for p in power_logs if member_id2subscribe[p.power_member_id]]
-				power_log_ids = [p.id for p in power_logs]
-				need_power_member_ids = [p.be_powered_member_id for p in power_logs]
-				#计算助力值
-				need_power_member_id2power = {}
-				for m_id in need_power_member_ids:
-					if not need_power_member_id2power.has_key(m_id):
-						need_power_member_id2power[m_id] = 1
-					else:
-						need_power_member_id2power[m_id] += 1
-				for m_id in need_power_member_id2power.keys():
-					app_models.PowerMeParticipance.objects(belong_to=record_id,member_id=m_id).update(inc__power=need_power_member_id2power[m_id])
-				#更新已关注会员的助力详情记录
-				detail_power_member_ids = [p.power_member_id for p in power_logs]
-				app_models.PoweredDetail.objects(belong_to=record_id, power_member_id__in=detail_power_member_ids).update(set__has_powered=True)
-				need_del_powerlogs_ids += power_log_ids
+			power_logs = app_models.PowerLog.objects(belong_to=record_id)
+			power_member_ids = [p.power_member_id for p in power_logs]
+			member_id2subscribe = {m.id: m.is_subscribed for m in Member.objects.filter(id__in=power_member_ids)}
+			power_logs = [p for p in power_logs if member_id2subscribe[p.power_member_id]]
+			power_log_ids = [p.id for p in power_logs]
+			need_power_member_ids = [p.be_powered_member_id for p in power_logs]
+			#计算助力值
+			need_power_member_id2power = {}
+			for m_id in need_power_member_ids:
+				if not need_power_member_id2power.has_key(m_id):
+					need_power_member_id2power[m_id] = 1
+				else:
+					need_power_member_id2power[m_id] += 1
+			for m_id in need_power_member_id2power.keys():
+				app_models.PowerMeParticipance.objects(belong_to=record_id,member_id=m_id).update(inc__power=need_power_member_id2power[m_id])
+			#更新已关注会员的助力详情记录
+			detail_power_member_ids = [p.power_member_id for p in power_logs]
+			app_models.PoweredDetail.objects(belong_to=record_id, power_member_id__in=detail_power_member_ids).update(set__has_powered=True)
+			need_del_powerlogs_ids += power_log_ids
 			print '================update data end %s' % record_id
 			#统计助力排名
 			print '================set cache start %s' % record_id
