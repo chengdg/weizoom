5e1814ea65c7c5a45bc8aa44121ce363556076c6
陈蕾 <chenlei@narzh.com>
2015-12-28 17:16:06 +0800

    百宝箱投票 1、重构统计代码 2、样式调整

--- weapp/apps/customerized_apps/vote/vote_statistic.py
+++ weapp/apps/customerized_apps/vote/vote_statistic.py
@@ -25,75 +25,15 @@ class voteStatistic(resource.Resource):
 		响应GET
 		"""
 		if 'id' in request.GET:
-			all_participances = app_models.voteParticipance.objects(belong_to=request.GET['id'])
-			total_count = all_participances.count()
-			titles_list = []
-			title2itemCount = {}
-			title_valid_dict = {}
-			total_title_valid_dict ={}
-			title_type_dict = {}
-			title_image_dict = {}
-			for p in all_participances:
-				for title, data in p.termite_data.items():
-					title_type = u''
-					if data['type'] in ['appkit.selection', 'appkit.imageselection', 'appkit.textselection']:
-						is_valid = False
-						data_value = data['value']
-						v_title_index = 0
-						for item_k in sorted(data_value.keys()):
-							value = data_value[item_k]
-							select_title = item_k.split('_')[1]
-							if len(str(v_title_index))< 2:
-								select_title = '0%s_%s' % (str(v_title_index), select_title)
-							else:
-								select_title = '%s_%s' % (str(v_title_index),select_title)
-							if value['isSelect']:
-								is_valid = True
-							if title2itemCount.has_key(title):
-								if title2itemCount[title].has_key(select_title):
-									title2itemCount[title][select_title] += 1 if value['isSelect'] else 0
-								else:
-									title2itemCount[title][select_title] = 1 if value['isSelect'] else 0
-							else:
-								title2itemCount[title] = {}
-								title2itemCount[title][select_title] = 1 if value['isSelect'] else 0
-							if value.has_key('image'):
-								title_image_dict[title+select_title] = value['image']
-							title_type = value['type']
-							v_title_index += 1
-						if is_valid:
-							if total_title_valid_dict.has_key(title):
-								total_title_valid_dict[title] += 1
-							else:
-								total_title_valid_dict[title] = 1
-							if title_valid_dict.has_key(title):
-								title_valid_dict[title] += 1
-							else:
-								title_valid_dict[title] = 1
-						else:
-							if not total_title_valid_dict.has_key(title):
-								total_title_valid_dict[title] = 0
-							if not title_valid_dict.has_key(title):
-								title_valid_dict[title] = 0
-						title_type_dict[title] = title_type
+			all_vote_participances = app_models.voteParticipance.objects(belong_to=request.GET['id'])
+			total_count = all_vote_participances.count()
+			titles_list = get_vote_title_select_datas(request)
+			for data in titles_list:
+				data['title_name'] = data['title_name'].split('_')[1]
+				data_values = data['title_value']
+				for data_value in data_values:
+					data_value['item_name'] = data_value['item_name'].split('_')[1]
 
-			for title in sorted(title2itemCount.keys()):
-				single_title_dict = {}
-				single_title_dict['title_name'] = title.split('_')[1]
-				single_title_dict['title_valid_count'] = title_valid_dict[title]
-				single_title_dict['type'] = u'单选' if title_type_dict[title] == 'radio' else u'多选'
-				single_title_dict['title_value'] = []
-				for item in sorted(title2itemCount[title].keys()):
-					item_value = title2itemCount[title][item]
-					single_item_value = {}
-					single_item_value['item_name'] = item.split('_')[1]
-					single_item_value['counter'] = item_value
-					if title_image_dict.has_key(title+item):
-						single_item_value['image'] = title_image_dict[title+item]
-					single_item_value['percent'] = '%d%s' % (item_value / float(total_title_valid_dict[title]) * 100 if total_title_valid_dict[title] else 0, '%')
-					single_title_dict['title_value'].append(single_item_value)
-
-				titles_list.append(single_title_dict)
 
 			project_id = 'new_app:vote:%s' % request.GET.get('related_page_id', 0)
 		else:
@@ -125,7 +65,7 @@ class voteStatistic_Export(resource.Resource):
 		"""
 		不同类型分页统计
 		"""
-		export_id = request.GET.get('export_id')
+		export_id = request.GET.get('id')
 		trans2zh = {u'phone':u'手机',u'email':u'邮箱',u'name':u'姓名',u'tel':u'电话'}
 
 		# app_name = voteStatistic_Export.app
@@ -143,124 +83,40 @@ class voteStatistic_Export(resource.Resource):
 		#Excel Process Part
 		try:
 			import xlwt
-			data = app_models.voteParticipance.objects(belong_to=export_id)
-			total = data.count()
-			member_id2termite_data={}
-			for item in data:
-				if item['member_id'] not in member_id2termite_data:
-					member_id2termite_data[item['member_id']] = {'created_at':item['created_at'],'termite_data':item['termite_data']}
+			titles_list = get_vote_title_select_datas(request)
+			for data in titles_list:
+				data['title_name'] = data['title_name'].split('_')[1]
+				data_values = data['title_value']
+				for data_value in data_values:
+					data_value['item_name'] = data_value['item_name'].split('_')[1]
 
-			#select sheet
-			select_data = {}
-			select_static ={}
-			qa_static = {}
-			for item in member_id2termite_data:
-				record = member_id2termite_data[item]
-				time = record['created_at']
-				for termite in record['termite_data']:
-					termite_dic = record['termite_data'][termite]
-					if termite_dic['type'] in ['appkit.selection', 'appkit.imageselection', 'appkit.textselection']:
-						if termite not in select_data:
-							select_data[termite] = [termite_dic['value']]
-						else:
-							select_data[termite].append(termite_dic['value'])
-					# if termite_dic['type']=='appkit.qa':
-					# 	if termite not in qa_static:
-					# 		qa_static[termite]=[{'created_at':time,'answer':termite_dic['value']}]
-					# 	else:
-					# 		qa_static[termite].append({'created_at':time,'answer':termite_dic['value']})
-			#select-data-processing
-			title_valid_dict = {}
-			for select in select_data:
-				for s_list in select_data[select]:
-					is_valid = False
-
-					v_title_index = 0
-					for s_k in sorted(s_list.keys()):
-						select_title = s_k.split('_')[1]
-						if len(str(v_title_index))< 2:
-							select_title = '0%s_%s' % (str(v_title_index), select_title)
-						else:
-							select_title = '%s_%s' % (str(v_title_index),select_title)
-
-						if select not in select_static:
-							select_static[select]={
-								'title': {}
-							}
-						if select_title not in select_static[select]['title']:
-							select_static[select]['title'][select_title]={'count': 0}
-						if s_list[s_k]['isSelect'] == True:
-							select_static[select]['title'][select_title]['count'] += 1
-							is_valid = True
-						if s_list[s_k].has_key('image'):
-							select_static[select]['image'] = True
-							select_static[select]['title'][select_title]['image'] = s_list[s_k]['image']
-						v_title_index += 1
-					if is_valid:
-						if title_valid_dict.has_key(select):
-							title_valid_dict[select] += 1
-						else:
-							title_valid_dict[select] = 1
-					else:
-						if not title_valid_dict.has_key(select):
-							title_valid_dict[select] = 0
-			#workbook/sheet
 			wb = xlwt.Workbook(encoding='utf-8')
-
-			#select_sheet
-			if select_static:
-				ws = wb.add_sheet(u'选择题')
-				header_style = xlwt.XFStyle()
-				select_num = 0
-				row = col =0
-				for s in sorted(select_static.keys()):
-					select_num += 1
-					ws.write(row,col,'%d.'%select_num+s.split('_')[1]+u'(有效参与人数%d人)'% title_valid_dict[s])
-					if select_static[s].has_key('image'):
-						ws.write(row,col+1,u'选项图片')
-						ws.write(row,col+2,u'参与人数/百分百')
-					else:
-						ws.write(row,col+1,u'参与人数/百分百')
-					row += 1
-					all_select_num = 0
-					s_i_num = 0
-					for s_i in sorted(select_static[s]['title'].keys()):
-						s_num = select_static[s]['title'][s_i]['count']
-						if s_num :
-							all_select_num += s_num
-					for s_i in sorted(select_static[s]['title'].keys()) :
-						s_num = select_static[s]['title'][s_i]['count']
-						ws.write(row,col,s_i.split('_')[1])
-						per = s_num*1.0/title_valid_dict[s]*100 if title_valid_dict[s] else 0
-						if select_static[s]['title'][s_i].has_key('image'):
-							ws.write(row,col+1,select_static[s]['title'][s_i]['image'])
-							ws.write(row,col+2,u'%d人/%.1f%%'%(s_num,per))
+			select_num = 0
+			row = col =0
+			ws = wb.add_sheet(u'选择题')
+			for data in titles_list:
+				select_num += 1
+				ws.write(row,col,'%d.'%select_num+data['title_name']+u'(有效参与人数%d人)'% data['title_valid_count'])
+				is_top_tilte = False
+				for data_value in data['title_value']:
+					if not is_top_tilte:
+						if data_value.has_key('image'):
+							ws.write(row,col+1,u'选项图片')
+							ws.write(row,col+2,u'参与人数/百分百')
 						else:
-							ws.write(row,col+1,u'%d人/%.1f%%'%(s_num,per))
-						row += 1
-						s_i_num += 1
-					ws.write(row,col,u'')
-					ws.write(row,col+1,u'')
+							ws.write(row,col+1,u'参与人数/百分百')
+						is_top_tilte = True
 					row += 1
-					ws.write(row,col,u'')
-					ws.write(row,col+1,u'')
-					row += 1
-			# #qa_sheet
-			# if qa_static:
-			# 	qa_num = 0
-			# 	for q in qa_static:
-			# 		qa_num += 1
-			# 		row = col = 0
-			# 		ws = wb.add_sheet(u'问题%d'%qa_num)
-			# 		header_style = xlwt.XFStyle()
-			#
-			# 		ws.write(row,col,u'提交时间')
-			# 		ws.write(row,col+1,q.split('_')[1]+u'(有效参与人数%d)'%total)
-			#
-			# 		for item in qa_static[q]:
-			# 			row +=1
-			# 			ws.write(row,col,item['created_at'].strftime("%Y/%m/%d %H:%M"))
-			# 			ws.write(row,col+1,item['answer'])
+					ws.write(row,col,data_value['item_name'])
+					if data_value.has_key('image'):
+						ws.write(row,col+1,data_value['image'])
+						ws.write(row,col+2,'%d人/%s' % (data_value['counter'],data_value['percent']))
+					else:
+						ws.write(row,col+1,'%d人/%s' % (data_value['counter'],data_value['percent']))
+				row += 1
+				ws.write(row,col,u'')
+				ws.write(row,col+1,u'')
+				row += 1
 			try:
 				wb.save(export_file_path)
 			except:
@@ -274,3 +130,117 @@ class voteStatistic_Export(resource.Resource):
 			response = create_response(500)
 
 		return response.get_response()
+
+
+def get_vote_title_select_datas(request):
+	'''
+	投票数据例如：
+	{
+	"_id" : ObjectId("567b5134ed8d712214296cb2"),
+	"webapp_user_id" : NumberLong(2),
+	"member_id" : NumberLong(2),
+	"belong_to" : "567a75d2ed8d711854c0be10",
+	"tel" : "",
+	"termite_data" : {
+		"04_name" : {"type" : "appkit.textlist","value" : "asdaasdad"},
+		"01_单选的i哦" : {"type" : "appkit.imageselection","display_type" : "list","value" : {
+				"00_1" : {"isSelect" : false,"mt" : "15px","image" : "/static/upload/13_20151218/1450426957004_630.jpg","type" : "radio"},
+				"01_1" : {"isSelect" : false,"mt" : "15px","image" : "/static/upload/13_20151218/1450426935062_309.jpg","type" : "radio"},
+				"02_1" : {"isSelect" : true,"mt" : "15px","image" : "/static/upload/13_20151218/145042695002_639.jpg","type" : "radio"}}},
+		"05_phone" : {"type" : "appkit.textlist","value" : "15896321478"},
+		"00_asd" : {"type" : "appkit.selection","display_type" : "","value" : {
+				"00_1" : {"isSelect" : false,"type" : "radio"},
+				"01_2" : {"isSelect" : false,"type" : "radio"},
+				"06_3" : {"isSelect" : true,"type" : "radio"}}},
+		"02_多选的哦" : {"type" : "appkit.imageselection","display_type" : "table","value" : {
+				"01_2" : {"isSelect" : false,"mt" : "0px","image" : "/static/upload/13_20151218/145042695002_639.jpg","type" : "checkbox"},
+				"00_2" : {"isSelect" : false,"mt" : "0px","image" : "/static/upload/13_20151218/1450426935062_309.jpg","type" : "checkbox"},
+				"02_2" : {"isSelect" : true,"mt" : "0px","image" : "/static/upload/13_20151218/1450426982081_980.jpg","type" : "checkbox"}}}},
+	"prize" : {"type" : "no_prize","data" : null},
+	"created_at" : ISODate("2015-12-24T09:58:12.907Z")}
+	@param request: request参数
+	变量名：
+		all_vote_participances: 所有投票数据
+		all_title_select_list: 所有标题-选项及对应百分比信息
+		title: 标题
+		select_data_value: 某个标题下的所有选项信息
+		select_title: 每个选项的标题
+		value: 每个选项是否被选中及其他信息
+	@return: all_title_select_list
+	'''
+	all_vote_participances = app_models.voteParticipance.objects(belong_to=request.GET['id'])
+	all_title_select_list = []
+	select_title2itemCount = {}
+	title_valid_dict = {}
+	total_title_valid_dict ={}
+	title_type_dict = {}
+	title_image_dict = {}
+	title_disp_type = {}
+	member_dict = {}
+	for p in all_vote_participances:
+		for title, select_datas in p.termite_data.items():
+			title_type = u''
+			if select_datas['type'] in ['appkit.selection', 'appkit.imageselection', 'appkit.textselection']:
+				is_Select = False
+				select_data_value = select_datas['value']
+				select_title_index = 0
+				for select_title in sorted(select_data_value.keys()):
+					value = select_data_value[select_title]
+					select_title = select_title.split('_')[1]
+					if len(str(select_title_index))< 2:
+						select_title = '0%s_%s' % (str(select_title_index), select_title)
+					else:
+						select_title = '%s_%s' % (str(select_title_index),select_title)
+					if value['isSelect']:
+						is_Select = True
+					if select_title2itemCount.has_key(title):
+						if select_title2itemCount[title].has_key(select_title):
+							select_title2itemCount[title][select_title] += 1 if value['isSelect'] else 0
+						else:
+							select_title2itemCount[title][select_title] = 1 if value['isSelect'] else 0
+					else:
+						select_title2itemCount[title] = {}
+						select_title2itemCount[title][select_title] = 1 if value['isSelect'] else 0
+					if value.has_key('image'):
+						title_image_dict[title+select_title] = value['image']
+					title_type = value['type']
+					member_dict[title+select_title] = p.member_id
+					select_title_index += 1
+				if select_datas.has_key('display_type'):
+					title_disp_type[title] = select_datas['display_type']
+				if is_Select:
+					if total_title_valid_dict.has_key(title):
+						total_title_valid_dict[title] += 1
+					else:
+						total_title_valid_dict[title] = 1
+					if title_valid_dict.has_key(title):
+						title_valid_dict[title] += 1
+					else:
+						title_valid_dict[title] = 1
+				else:
+					if not total_title_valid_dict.has_key(title):
+						total_title_valid_dict[title] = 0
+					if not title_valid_dict.has_key(title):
+						title_valid_dict[title] = 0
+				title_type_dict[title] = title_type
+
+	for title in sorted(select_title2itemCount.keys()):
+		single_title_dict = {}
+		single_title_dict['title_name'] = title
+		single_title_dict['title_valid_count'] = title_valid_dict[title]
+		single_title_dict['type'] = u'单选' if title_type_dict[title] == 'radio' else u'多选'
+		single_title_dict['display_type'] = single_title_dict['display_type'] = title_disp_type.get(title,None)
+		single_title_dict['title_value'] = []
+		for select_title in sorted(select_title2itemCount[title].keys()):
+			item_value = select_title2itemCount[title][select_title]
+			single_item_value = {}
+			single_item_value['member_id'] = member_dict[title+select_title]
+			single_item_value['item_name'] = select_title
+			single_item_value['counter'] = item_value
+			if title_image_dict.has_key(title+select_title):
+				single_item_value['image'] = title_image_dict[title+select_title]
+			single_item_value['percent'] = '%d%s' % (item_value / float(total_title_valid_dict[title]) * 100 if total_title_valid_dict[title] else 0, '%')
+			single_title_dict['title_value'].append(single_item_value)
+
+		all_title_select_list.append(single_title_dict)
+	return  all_title_select_list
