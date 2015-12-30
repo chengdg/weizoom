38e9f3c6a368e9fa43e9a6dd4858cdc80a054181
lihanyi <lihanyi@narzh.com>
2015-12-30 10:50:25 +0800

    微助力BDD：将更新助力值排名的脚本放在steps内

--- weapp/features/steps/apps_powerme_frontend_steps.py
+++ weapp/features/steps/apps_powerme_frontend_steps.py
@@ -12,12 +12,121 @@ from modules.member import module_api as member_api
 from utils import url_helper
 import datetime as dt
 import termite.pagestore as pagestore_manager
-from apps.customerized_apps.powerme.models import PowerMe, PowerMeParticipance, PowerMeControl
+from apps.customerized_apps.powerme.models import PowerMe, PowerMeParticipance, PowerMeControl,PowerLog,PoweredDetail
 from weixin.message.material import models as material_models
 from modules.member.models import Member, SOURCE_MEMBER_QRCODE
 from utils.string_util import byte_to_hex
 import json
 import re
+
+import time
+from django.core.management.base import BaseCommand, CommandError
+from utils.cache_util import SET_CACHE
+from modules.member.models import Member
+
+
+class Command(BaseCommand):
+	help = 'start powerme stats task'
+	args = ''
+
+	def handle(self, **options):
+		print 'powerme timer task start...'
+		start_time = time.time()
+
+		need_del_powerlogs_ids = []
+		powermes = PowerMe.objects(status=1)
+		powerme_ids = [str(p.id) for p in powermes]
+		power_logs = PowerLog.objects(belong_to__in=powerme_ids)
+		power_member_ids = [p.power_member_id for p in power_logs]
+		member_id2subscribe = {m.id: m.is_subscribed for m in Member.objects.filter(id__in=power_member_ids)}
+		record_id2logs = {}
+		for log in power_logs:
+			cur_powerme_id = log.belong_to
+			if cur_powerme_id in record_id2logs:
+				record_id2logs[cur_powerme_id].append(log)
+			else:
+				record_id2logs[cur_powerme_id] = [log]
+
+		powerme_ids = [str(p.id) for p in powermes]
+		participances = PowerMeParticipance.objects(belong_to__in=powerme_ids, has_join=True).order_by('-power', 'created_at')
+		record_id2participances = {}
+		for pa in participances:
+			belong_to = pa.belong_to
+			if not record_id2participances.has_key(belong_to):
+				record_id2participances[belong_to] = [pa]
+			else:
+				record_id2participances[belong_to].append(pa)
+
+		member_ids = [p.member_id for p in participances]
+		member_id2member = {m.id: m for m in Member.objects.filter(id__in=member_ids)}
+
+		for powerme in powermes:
+			record_id = str(powerme.id)
+			print '================update data start %s' % record_id
+			#统计助力值
+			if record_id in record_id2logs:
+				power_logs = record_id2logs[record_id]
+			else:
+				power_logs = []
+			need_power_logs = [p for p in power_logs if member_id2subscribe[p.power_member_id]]
+			power_log_ids = [p.id for p in need_power_logs]
+			need_power_member_ids = [p.be_powered_member_id for p in need_power_logs]
+			#计算助力值
+			need_power_member_id2power = {}
+			for m_id in need_power_member_ids:
+				if not need_power_member_id2power.has_key(m_id):
+					need_power_member_id2power[m_id] = 1
+				else:
+					need_power_member_id2power[m_id] += 1
+			for m_id in need_power_member_id2power.keys():
+				PowerMeParticipance.objects(belong_to=record_id,member_id=m_id).update(inc__power=need_power_member_id2power[m_id])
+			#更新已关注会员的助力详情记录
+			detail_power_member_ids = [p.power_member_id for p in need_power_logs]
+			PoweredDetail.objects(belong_to=record_id, power_member_id__in=detail_power_member_ids).update(set__has_powered=True)
+			need_del_powerlogs_ids += power_log_ids
+			print '================update data end %s' % record_id
+			#统计助力排名
+			print '================set cache start %s' % record_id
+			cache_key = 'apps_powerme_%s' % record_id
+
+			if record_id in record_id2participances.keys():
+				participances = record_id2participances[record_id]
+			else:
+				participances = []
+			# participances = app_models.PowerMeParticipance.objects(belong_to=record_id, has_join=True).order_by('-power', 'created_at')
+			participances_dict = {}
+			participances_list = []
+			total_participant_count = len(participances)
+
+			# member_ids = [p.member_id for p in participances]
+			# member_id2member = {m.id: m for m in Member.objects.filter(id__in=member_ids)}
+			rank = 0 #排名
+			for p in participances:
+				member = member_id2member[p.member_id]
+				rank += 1
+				temp_dict = {
+					'rank': rank,
+					'member_id': p.member_id,
+					'user_icon': member.user_icon,
+					'username': member.username_size_ten,
+					'power': p.power
+				}
+				participances_dict[p.member_id] = temp_dict
+				participances_list.append(temp_dict)
+			# 取前100位
+			participances_list = participances_list[:100]
+			SET_CACHE(cache_key,{
+				'participances_dict': participances_dict,
+				'participances_list': participances_list,
+				'total_participant_count': total_participant_count
+			})
+			print '================set cache end %s' % record_id
+
+		#删除计算过的log
+		PowerLog.objects(id__in=need_del_powerlogs_ids).delete()
+		end_time = time.time()
+		diff = (end_time-start_time)*1000
+		print 'powerme timer task end...expend %s' % diff
 
 def __get_power_me_rule_name(title):
 	material_url = material_models.News.objects.get(title=title).url
@@ -63,11 +172,16 @@ def __get_power_me_rank_informations(context,webapp_owner_id,power_me_rule_id,op
 		response = context.client.get(bdd_util.nginx(redirect_url))
 	if response.status_code == 200:
 		return response
+	else:
+		print('[info] redirect error,response.status_code :')
+		print(response.status_code)
 
 @When(u'更新助力排名')
 def step_impl(context):
-	os.system('cd %s' % settings.PROJECT_HOME)
-	os.system('python manage.py apps_powerme_timer_task')
+	# os.system('cd %s' % settings.PROJECT_HOME)
+	# os.system('python manage.py apps_powerme_timer_task')
+	command = Command()
+	command.handle()
 
 @When(u'{webapp_user_name}点击图文"{title}"进入微助力活动页面')
 def step_impl(context, webapp_user_name, title):
