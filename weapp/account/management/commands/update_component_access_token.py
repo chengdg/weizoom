57a14c65dfb43a97d6acd75caa5bd56280491aef
侯庭飞 <houtingfei@weizoom.com>
2016-01-07 14:33:58 +0800

    授权代码优化

--- weapp/account/management/commands/update_component_access_token.py
+++ weapp/account/management/commands/update_component_access_token.py
@@ -13,9 +13,10 @@ from weixin.user.models import *
 from core.wxapi.agent_weixin_api import WeixinApi, WeixinHttpClient
 from core.upyun_util import upload_qrcode_url_to_upyun
 weixin_http_client = WeixinHttpClient()
-import datetime
 from account.models import UserProfile
 from watchdog.utils import watchdog_error, watchdog_info, watchdog_warning
+from weixin.util import refresh_auth_token, get_authorizer_info
+
 
 class Command(BaseCommand):
 	help = "init global navbar for all user"
@@ -25,11 +26,9 @@ class Command(BaseCommand):
 		for component in ComponentInfo.objects.filter(is_active=True):
 			weixin_api = WeixinApi(None, weixin_http_client)
 			from weixin.message.message_handler.tasks import record_call_weixin_api
-
+			# 获取第三方平台的component_access_token
 			try:
 				result = weixin_api.get_component_token(component.app_id, component.app_secret, component.component_verify_ticket)
-				print result,'---'
-				#watchdog_info('call weixin api: get_component_token , result:{}'.format(result))
 				if result.has_key('errcode'):
 					if result['errcode'] == -1 or result['errcode'] == 995995:
 						result, success = self.__get_component_token_retry(weixin_api, component)
@@ -42,6 +41,8 @@ class Command(BaseCommand):
 				result, success = self.__get_component_token_retry(weixin_api, component)
 			if result != None:
 				record_call_weixin_api.delay('get_component_token', success)
+			else:
+				continue
 
 			component_access_token = result['component_access_token']
 			component.component_access_token = component_access_token
@@ -62,137 +63,18 @@ class Command(BaseCommand):
 
 
 	def __update_auth_appid(self, auth_appid = None, weixin_api = None, component = None, error_auth_appid = []):
-		user_id = auth_appid.user_id
-		print user_id,auth_appid.is_active
-		if auth_appid.is_active is False:
-			UserProfile.objects.filter(user_id=user_id).update(is_mp_registered=False)
-			return
-
-		result = weixin_api.api_authorizer_token(component.app_id, auth_appid.authorizer_appid, auth_appid.authorizer_refresh_token)
-
-		if result.has_key('errcode') and (result['errcode'] == -1 or result['errcode'] == 995995):
-			error_auth_appid.append(auth_appid)
-		#print result['authorizer_access_token']
-		if result.has_key('authorizer_access_token'):
-			authorizer_access_token = result['authorizer_access_token']
-			auth_appid.authorizer_access_token = result['authorizer_access_token']
-			auth_appid.authorizer_refresh_token = result['authorizer_refresh_token']
-			auth_appid.last_update_time = datetime.datetime.now()
-			auth_appid.save()
-
-			if WeixinMpUser.objects.filter(owner_id=user_id).count() > 0:
-				mp_user = WeixinMpUser.objects.filter(owner_id=user_id)[0]
+		return_msg, mp_user = refresh_auth_token(auth_appid, weixin_api, component)
+		if return_msg:
+			if return_msg == 'error':
+				error_auth_appid.append(auth_appid)
 			else:
-				mp_user = WeixinMpUser.objects.create(owner_id=user_id)
-
-			if WeixinMpUserAccessToken.objects.filter(mpuser = mp_user).count() > 0:
-				WeixinMpUserAccessToken.objects.filter(mpuser = mp_user).update(update_time=datetime.datetime.now(), access_token=authorizer_access_token, is_active=True, app_id = auth_appid.authorizer_appid)
-			else:
-				WeixinMpUserAccessToken.objects.filter(mpuser = mp_user).update(
-					mpuser = mp_user,
-					app_id = auth_appid.authorizer_appid,
-					app_secret = '',
-					access_token = authorizer_access_token
-				)
-
-		result = weixin_api.api_get_authorizer_info(component.app_id,auth_appid.authorizer_appid)
-		print result
-		try:
-			watchdog_info('call weixin api: api_get_authorizer_info , result:{}'.format(result))
-		except:
-			pass
-		if result.has_key('authorizer_info'):
-			nick_name = result['authorizer_info'].get('nick_name', '')
-			head_img = result['authorizer_info'].get('head_img', '')
-			service_type_info = result['authorizer_info']['service_type_info'].get('id', '')
-			verify_type_info = result['authorizer_info']['verify_type_info'].get('id', '')
-			user_name = result['authorizer_info'].get('user_name', '')
-			alias = result['authorizer_info'].get('alias', '')
-			qrcode_url = result['authorizer_info'].get('qrcode_url','')
-
-			#authorization_info
-			appid = result['authorization_info'].get('authorizer_appid', '')
-
-			func_info_ids = []
-			func_info = result['authorization_info'].get('func_info')
-			if  isinstance(func_info, list):
-				for funcscope_category in func_info:
-					funcscope_category_id = funcscope_category.get('funcscope_category', None)
-					if funcscope_category_id:
-						func_info_ids.append(str(funcscope_category_id.get('id')))
-
-
-			if ComponentAuthedAppidInfo.objects.filter(auth_appid=auth_appid).count() > 0:
-				auth_appid_info = ComponentAuthedAppidInfo.objects.filter(auth_appid=auth_appid)[0]
-				if auth_appid_info.qrcode_url.find('mmbiz.qpic.cn') > -1 or auth_appid_info.nick_name != nick_name:
-					try:
-						qrcode_url = upload_qrcode_url_to_upyun(qrcode_url, auth_appid.authorizer_appid)
-					except:
-						print '>>>>>>>>>>>>>>>>>>>>upload_qrcode_url_to_upyun error'
-				else:
-					qrcode_url = auth_appid_info.qrcode_url
-
-				ComponentAuthedAppidInfo.objects.filter(auth_appid=auth_appid).update(
-					nick_name=nick_name,
-					head_img=head_img,
-					service_type_info=service_type_info,
-					verify_type_info=verify_type_info,
-					user_name=user_name,
-					alias=alias,
-					qrcode_url=qrcode_url,
-					appid=appid,
-					func_info=','.join(func_info_ids)
-					)
-
-			else:
-				try:
-					watchdog_info('call weixin api: api_get_authorizer_info , result:{}'.format(result))
-					qrcode_url = upload_qrcode_url_to_upyun(qrcode_url, auth_appid.authorizer_appid)
-				except:
-					print '>>>>>>>>>>>>>>>>>>>>upload_qrcode_url_to_upyun error'
-				ComponentAuthedAppidInfo.objects.create(
-					auth_appid=auth_appid,
-					nick_name=nick_name,
-					head_img=head_img,
-					service_type_info=service_type_info,
-					verify_type_info=verify_type_info,
-					user_name=user_name,
-					alias=alias,
-					qrcode_url=qrcode_url,
-					appid=appid,
-					func_info=','.join(func_info_ids)
-					)
-
-			is_service = False
-
-			if int(service_type_info) > 1:
-				is_service = True
-			is_certified = False
-			if int(verify_type_info) > -1:
-				is_certified = True
-
-			print '>>>>>>>>>>>>>>>>>service_type_info',service_type_info
-			print '>>>>>>>>>>>>>>>>>is_certified',is_certified
-			WeixinMpUser.objects.filter(owner_id=user_id).update(is_service=is_service, is_certified=is_certified, is_active=True)
-
-			if mp_user:
-				if MpuserPreviewInfo.objects.filter(mpuser=mp_user).count() > 0:
-					MpuserPreviewInfo.objects.filter(mpuser=mp_user).update(image_path=head_img, name=nick_name)
-				else:
-					MpuserPreviewInfo.objects.create(mpuser=mp_user,image_path=head_img, name=nick_name)
-			user_profile = UserProfile.objects.get(user_id=user_id)
-			if is_service:
-				if (user_profile.is_mp_registered is False) or (user_profile.is_oauth is False):
-					UserProfile.objects.filter(user_id=user_id).update(is_mp_registered=True, is_oauth=True)
-			else:
-				if user_profile.is_oauth:
-					UserProfile.objects.filter(user_id=user_id).update(is_mp_registered=True, is_oauth=False)
+				result = get_authorizer_info(auth_appid, weixin_api, component, mp_user)
+				if not result:
+					error_auth_appid.append(auth_appid)
 
 	def __get_component_token_retry(self, weixin_api=None, component = None):
 		try:
 			result = weixin_api.get_component_token(component.app_id, component.app_secret, component.component_verify_ticket)
-			print 'justing,retry :\n{}'.format(result)
-			#watchdog_info('call weixin api: get_component_token , result:{}'.format(result))
 			if result.has_key('errcode'):
 				success = False
 				watchdog_error('call weixin api: get_component_token , result:{}'.format(result))
--- weapp/weixin/views.py
+++ weapp/weixin/views.py
@@ -3,6 +3,7 @@
 __author__ = 'chuter, bert'
 
 import hashlib
+from BeautifulSoup import BeautifulSoup
 
 from django.http import HttpResponseRedirect, HttpResponse, HttpRequest, Http404
 from django.contrib.auth.models import User
@@ -12,14 +13,13 @@ from account.models import UserProfile
 from core.exceptionutil import unicode_full_stack
 from core.upyun_util import upload_qrcode_url_to_upyun
 
-from watchdog.utils import watchdog_fatal, watchdog_error
-
+from watchdog.utils import watchdog_fatal, watchdog_error, watchdog_info
+from util import get_query_auth, get_authorizer_info
 from weixin.user.models import *
-
-from BeautifulSoup import BeautifulSoup
 from weixin.message_util.WXBizMsgCrypt import WXBizMsgCrypt
-
 import datetime
+
+
 
 
 ########################################################################
@@ -69,7 +69,7 @@ def handle(request, webapp_id):
 			notify_message = u"进行消息处理失败，webapp_id:{} cause:\n{}".format(webapp_id, unicode_full_stack())
 			watchdog_fatal(notify_message)
 			content =None
-		
+
 		if content is None or len(content) == 0:
 			return HttpResponse('')
 		else:
@@ -77,7 +77,6 @@ def handle(request, webapp_id):
 
 
 def receiveauthcode(request):
-	#@if request.body:
 	is_from_simulator = False
 	is_aeskey = False
 	wxiz_msg_crypt = None
@@ -87,210 +86,86 @@ def receiveauthcode(request):
 	nonce = request.GET.get('nonce', None)
 	encrypt_type = request.GET.get('encrypt_type', None)
 	signature = request.GET.get('signature', None)
-	#component_info = ComponentInfo.objects.filter(is_active=True)
+
 	if request.method == "POST":
 		"""
-			增加微众测试接口
+		接受微信服务器每隔10分钟推送过来的component_verify_ticket
 		"""
-		# if 'weizoom_test_data' in request.GET:
-		# 	xml_message = _get_raw_message(request)
-		# else:
 		xml_message = _get_raw_message(request).decode('utf-8')
 		xml_message_for_appid = BeautifulSoup(xml_message)
-		print xml_message
-		if xml_message_for_appid.appid and xml_message_for_appid.appid.text:
-			appid = xml_message_for_appid.appid.text
-			component_info = ComponentInfo.objects.get(app_id=appid)
+		appid = xml_message_for_appid.appid.text if xml_message_for_appid.appid else None
+		if appid:
+			try:
+				component_info = ComponentInfo.objects.get(app_id=appid)
+			except:
+				notify_msg = u"没有app_id=%s的第三方平台账号帐号信息" % appid
+				watchdog_error(notify_msg)
+				return HttpResponse('success')
+
 			wxiz_msg_crypt = WXBizMsgCrypt(component_info.token, component_info.ase_key, component_info.app_id)
 			_,xml_message = wxiz_msg_crypt.DecryptMsg(xml_message, msg_signature, timestamp, nonce)
-			print xml_message
 			xml_message = BeautifulSoup(xml_message)
-			ticket = xml_message.componentverifyticket.text
+			#ticket = xml_message.componentverifyticket.text
 
-			if appid and ticket:
-				if ComponentInfo.objects.filter(app_id=appid).count() > 0:
-					ComponentInfo.objects.filter(app_id=appid).update(component_verify_ticket=ticket, last_update_time=datetime.datetime.now())
-				else:
-					ComponentInfo.objects.create(app_id=appid,component_verify_ticket=ticket)
-				return HttpResponse('success') 
+			if xml_message.componentverifyticket:
+				#接受component_verify_ticket
+				ticket = xml_message.componentverifyticket.text
+				if ticket:
+					component_info.component_verify_ticket = ticket
+					component_info.last_update_time = datetime.datetime.now()
+					component_info.save()
+			elif xml_message.infotype and xml_message.infotype.text == 'unauthorized':
+				#接受取消授权
+				authorizerappid = xml_message.authorizerappid.text
+				component_authed_appids = ComponentAuthedAppid.objects.filter(authorizer_appid=authorizerappid)
+				if component_authed_appids.count() > 0:
+					component_authed_appid = component_authed_appids[0]
+					component_authed_appid.is_active = False
+					component_authed_appid.save()
+					user_id = component_authed_appid.user_id
+					if WeixinMpUser.objects.filter(owner_id=user_id).count() > 0:
+						mp_user = WeixinMpUser.objects.filter(owner_id=user_id)[0]
+						mp_user.is_active = False
+						mp_user.save()
+
+						WeixinMpUserAccessToken.objects.filter(mpuser = mp_user).update(is_active=False)
+					UserProfile.objects.filter(user_id=user_id).update(is_mp_registered=False)
+		return HttpResponse('success')
 	else:
-		auth_code = request.GET.get('auth_code', None)
-		expires_in = request.GET.get('expires_in', None)
-
-		# if auth_code and expires_in:
-		# 	pass
+		# 授权后的回调链接的响应函数，使用auth_code获取授权公众号的授权信息
+		auth_code = request.GET.get('auth_code', None) # 授权码
+		expires_in = request.GET.get('expires_in', None) # 过期时间
 
 		if request.user.is_authenticated() and auth_code:
 			request.user_profile = request.user.get_profile()
 			webapp_id = request.user_profile.webapp_id
 			user_id=request.user.id
-			# component_info = ComponentInfo.objects.filter(is_active=True)[0]
-			# if ComponentAuthedAppid.objects.filter(component_info=component_info, user_id=request.user.id).count() > 0:
-			# 	ComponentAuthedAppid.objects.filter(component_info=component_info, user_id=request.user.id).update(last_update_time=datetime.datetime.now(), auth_code=auth_code)
-			# else:
-			# 	ComponentAuthedAppid.objects.create(component_info=component_info, user_id=request.user.id, auth_code=auth_code)
 			from weixin.user.util import get_component_info_from
-			request_host = request.get_host()
-			component_info = get_component_info_from(request)
-			component_authed_appid = ComponentAuthedAppid.objects.get(component_info=component_info, user_id=request.user.id)
-			component_info = component_authed_appid.component_info
+			component_info = get_component_info_from(request) # 获取第三方开放平台的基本信息
+			component_authed_appid = ComponentAuthedAppid.objects.get(component_info=component_info, user_id=user_id) # 获取用户委托的账户记录
 
-			"""
-			TODO: 使用授权码换取公众号的授权信息 放到 消息队列里 ，并且进行监控， 保证100%成功
-			"""
-			#try:
 			from core.wxapi.agent_weixin_api import WeixinApi, WeixinHttpClient
 			weixin_http_client = WeixinHttpClient()
 			weixin_api = WeixinApi(component_info.component_access_token, weixin_http_client)
-			result = weixin_api.api_query_auth(component_info.app_id, auth_code)
-			print '===============query_auth',result
-			mp_user = None
-			if result.has_key('authorization_info'):
-				authorization_info = result['authorization_info']
-				func_info_ids = []
-				func_info = authorization_info['func_info']
-				if  isinstance(func_info, list):
-					for funcscope_category in func_info:
-						funcscope_category_id = funcscope_category.get('funcscope_category', None)
-						if funcscope_category_id:
-							func_info_ids.append(str(funcscope_category_id.get('id')))
-				print func_info_ids,'=============================='
-				authorizer_appid = authorization_info['authorizer_appid']
-				authorizer_access_token=authorization_info['authorizer_access_token']
-				authorizer_refresh_token=authorization_info['authorizer_refresh_token']
-				
-				if authorizer_appid:
-					ComponentAuthedAppid.objects.filter(user_id=user_id).update(
-						authorizer_appid=authorizer_appid,
-						authorizer_access_token=authorizer_access_token,
-						authorizer_refresh_token=authorizer_refresh_token,
-						last_update_time=datetime.datetime.now(),
-						func_info = ','.join(func_info_ids),
-						is_active = True
-						)
+			return_msg, mp_user = get_query_auth(component_info, weixin_api, auth_code, user_id)
 
-					if WeixinMpUser.objects.filter(owner_id=user_id).count() > 0:
-						mp_user = WeixinMpUser.objects.filter(owner_id=user_id)[0]
-					else:
-						mp_user = WeixinMpUser.objects.create(owner_id=user_id)
-
-					if WeixinMpUserAccessToken.objects.filter(mpuser = mp_user).count() > 0:
-						WeixinMpUserAccessToken.objects.filter(mpuser = mp_user).update(update_time=datetime.datetime.now(), access_token=authorizer_access_token, is_active=True)
-					else:
-						WeixinMpUserAccessToken.objects.create(
-							mpuser = mp_user,
-							app_id = authorizer_appid,
-							app_secret = '',
-							access_token = authorizer_access_token,
-							update_time=datetime.datetime.now()
-						)
-					#UserProfile.objects.filter(id=request.user_profile.id).update(is_mp_registered=True)
-					"""
-						处理公众号绑定过其它系统帐号情况
-					"""
-					update_user_ids = []
-					for authed_appid in ComponentAuthedAppid.objects.filter(~Q(user_id=request.user.id), authorizer_appid=authorizer_appid):
-						authed_appid.is_active = False
-						authed_appid.save()
-						update_user_ids.append(authed_appid.user_id)
-					UserProfile.objects.filter(user_id__in=update_user_ids).update(is_mp_registered=False)
-
-
-			# except:
-			# 	print '授权失败，请重新授权'
-			# 	raise Http404('授权失败，请重新授权')
-					"""
-						处理公众号mp相关信息
-					"""
-					try:
-						result = weixin_api.api_get_authorizer_info(component_info.app_id, authorizer_appid)
-						if result.has_key('authorizer_info'):
-							nick_name = result['authorizer_info'].get('nick_name', '')
-							head_img = result['authorizer_info'].get('head_img', '')
-							service_type_info = result['authorizer_info']['service_type_info'].get('id', '')
-							verify_type_info = result['authorizer_info']['verify_type_info'].get('id', '')
-							user_name = result['authorizer_info'].get('user_name', '')
-							alias = result['authorizer_info'].get('alias', '')
-							qrcode_url = result['authorizer_info'].get('qrcode_url','')
-
-							appid = result['authorization_info'].get('authorizer_appid', '')
-
-							func_info_ids = []
-							func_info = result['authorization_info'].get('func_info')
-							if  isinstance(func_info, list):
-								for funcscope_category in func_info:
-									funcscope_category_id = funcscope_category.get('funcscope_category', None)
-									if funcscope_category_id:
-										func_info_ids.append(str(funcscope_category_id.get('id')))
-							if ComponentAuthedAppidInfo.objects.filter(auth_appid=component_authed_appid).count() > 0:
-								auth_appid_info = ComponentAuthedAppidInfo.objects.filter(auth_appid=component_authed_appid)[0]
-								if auth_appid_info.qrcode_url.find('mmbiz.qpic.cn') > -1 or auth_appid_info.nick_name != nick_name:
-									try:
-										qrcode_url = upload_qrcode_url_to_upyun(qrcode_url, authorizer_appid)
-									except:
-										print '>>>>>>>>>>>>>>>>>>>>upload_qrcode_url_to_upyun error'
-								ComponentAuthedAppidInfo.objects.filter(auth_appid=component_authed_appid).update(
-									nick_name=nick_name,
-									head_img=head_img,
-									service_type_info=service_type_info,
-									verify_type_info=verify_type_info,
-									user_name=user_name,
-									alias=alias,
-									qrcode_url=qrcode_url,
-									appid=appid,
-									func_info=','.join(func_info_ids)
-									)
-							else:
-								try:
-									qrcode_url = upload_qrcode_url_to_upyun(qrcode_url, authorizer_appid)
-								except:
-									print '>>>>>>>>>>>>>>>>>>>>upload_qrcode_url_to_upyun error'
-								ComponentAuthedAppidInfo.objects.create(
-									auth_appid=component_authed_appid,
-									nick_name=nick_name,
-									head_img=head_img,
-									service_type_info=service_type_info,
-									verify_type_info=verify_type_info,
-									user_name=user_name,
-									alias=alias,
-									qrcode_url=qrcode_url,
-									appid=appid,
-									func_info=','.join(func_info_ids)
-									)
-							is_service = False
-							if int(service_type_info) > 1:
-								is_service = True
-							is_certified = False
-							if int(verify_type_info) > -1:
-								is_certified = True
-							WeixinMpUser.objects.filter(owner_id=user_id).update(is_service=is_service, is_certified=is_certified, is_active=True)
-							
-							if is_certified:
-								UserProfile.objects.filter(id=request.user_profile.id).update(is_mp_registered=True, is_oauth=True)
-							else:
-								UserProfile.objects.filter(id=request.user_profile.id).update(is_mp_registered=True, is_oauth=False)
-						
-							try:
-								if mp_user:
-									if MpuserPreviewInfo.objects.filter(mpuser=mp_user).count() > 0:
-										MpuserPreviewInfo.objects.filter(mpuser=mp_user).update(image_path=head_img, name=nick_name)
-									else:
-										MpuserPreviewInfo.objects.create(mpuser=mp_user,image_path=head_img, name=nick_name)
-							except:
-								notify_msg = u"处理公众号mp相关信息:MpuserPreviewInfo, cause:\n{}".format(unicode_full_stack())
-								watchdog_error(notify_msg)		
-							
-					except:
-						notify_msg = u"处理公众号mp相关信息, cause:\n{}".format(unicode_full_stack())
-						watchdog_error(notify_msg)
-
+			# 每个api请求如果失败就重试一次
+			if return_msg == "success":
+				is_success = get_authorizer_info(component_authed_appid, weixin_api, component_info, mp_user)
+				if not is_success:
+					get_authorizer_info(component_authed_appid, weixin_api, component_info, mp_user)
+			else:
+				return_msg, mp_user = get_query_auth(component_info, weixin_api, auth_code, user_id)
+				if return_msg == "success":
+					is_success = get_authorizer_info(component_authed_appid, weixin_api, component_info, mp_user)
+					if not is_success:
+						get_authorizer_info(component_authed_appid, weixin_api, component_info, mp_user)
 			return HttpResponseRedirect('/new_weixin/mp_user/')
 
 		else:
 			raise Http404('请先登录系统，继续完成授权！')
 
-		print auth_code, expires_in, '>>>>>>>'
-	return HttpResponse('') 
+	return HttpResponse('')
 
 
 def _get_raw_message(request):
@@ -311,42 +186,14 @@ def component_handle(request, appid):
 		from weixin.message.message_pipeline import MessagePipeline
 		message_pipeline = MessagePipeline()
 
-	# if ComponentAuthedAppid.objects.filter(authorizer_appid=appid, is_active=True).count() > 0:
-	# 	print '------------in1'
-	# 	user_id = ComponentAuthedAppid.objects.filter(authorizer_appid=appid, is_active=True)[0].user_id
-	# 	user = User.objects.get(id=user_id)
-	# 	request.user_profile = user.get_profile()
-	# 	request.webapp_owner_id = user_id
-	# 	print '------------in2'
-
-
-	# if 'echostr' in request.GET:
-	# 	if is_valid_request(request, request.user_profile.webapp_id):
-	# 		return HttpResponse(request.GET['echostr'])
-	# 	else:
-	# 		return HttpResponse('')
-	# else:
-	# 	if appid == "wx570bc396a51b8ff8":
-	# 		webapp_id = None
-	# 		user_profile = None
-	# 	else:
-	# 		webapp_id = request.user_profile.webapp_id
-	# 		user_profile = request.user_profile
-
-
- 	try:
+	try:
 		content = message_pipeline.handle_component(request, appid)
 	except:
 		notify_message = u"进行消息处理失败，cause:\n{}".format(unicode_full_stack())
 		watchdog_fatal(notify_message)
 		content =None
-	
+
 	if content is None or len(content) == 0:
 		return HttpResponse('')
 	else:
 		return HttpResponse(content, mimetype="application/xml")
-
-	#return HttpResponse('')
-
-
-	
