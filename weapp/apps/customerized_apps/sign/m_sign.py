b40b5d3386b6ad218c1d1c37cc21419f061c835a
Mark24 <mark.zhangyoung@qq.com>
2016-01-08 17:36:12 +0800

    get 迁移到 api-get 之前

--- weapp/apps/customerized_apps/sign/m_sign.py
+++ weapp/apps/customerized_apps/sign/m_sign.py
@@ -31,29 +31,98 @@ class MSign(resource.Resource):
 
     def api_get(request):
         member_info = {}
-        # prize_info = {}
+        prize_info = {}
         record_id = request.GET.get('id',None)
         webapp_owner_id = request.GET.get('webapp_owner_id',None)
         member = request.member
         member_id = member.id
+        isMember = True if member.is_subscribed else False
+
         response = create_response(500)
         if not record_id or not member_id:
             response.errMsg = u"活动信息出错"
             return response.get_response()
+
         record = app_models.Sign.objects(owner_id=webapp_owner_id)
         if record.count()<=0:
             response.errMsg = "is_deleted"
             return response.get_response()
         else:
+            record = record[0]
+            activity_status = record.status_text
             member_info = {
                 'user_name': member.username_for_html if member and member.username_for_html else u'未知',
                 'user_icon': member.user_icon if member and member.user_icon else '/static/img/user-1.jpg',
                 'user_integral': member.integral if member else 0
             }
 
+            prize_settings = record.prize_settings
+            daily_prize = prize_settings['0']
+            serial_prize = {}
+            next_serial_prize = {}
+
+            signers = app_models.SignParticipance.objects(belong_to=str(record_id), member_id=member_id)
+            participance_data_count = signers.count()
+            signer = signers[0] if participance_data_count>0 else None
+
+            max_setting_count = sorted(map(lambda x: int(x), prize_settings.keys()), reverse=True)[0]
+            if signer:
+                #检查是否已签到
+                latest_sign_date = signer.latest_date.strftime('%Y-%m-%d')
+                nowDate = datetime.now().strftime('%Y-%m-%d')
+                #首先检查是否断掉了连续签到条件，状态重置serial_count为1
+                if latest_sign_date == (datetime.now() - dt_datetime.timedelta(days=1)).strftime('%Y-%m-%d'):
+                    temp_serial_count = signer.serial_count
+                else:
+                    temp_serial_count = 0
+                #如果已达到最大设置天数则重置签到
+                if (signer.serial_count == max_setting_count and latest_sign_date != nowDate) or (max_setting_count!=0 and signer.serial_count > max_setting_count):
+                    temp_serial_count = 0
+                    signer.update(set__serial_count=0)
+                    signer.reload()
+
+                if (latest_sign_date == nowDate and signer.serial_count != 0) or (latest_sign_date == nowDate and temp_serial_count != 0):
+                    activity_status = u'已签到'
+                    for name in sorted(map(lambda x: (int(x),x), prize_settings.keys())):
+                        setting = prize_settings[name[1]]
+                        name = name[0]
+                        if int(name) > signer.serial_count:
+                            next_serial_prize = {
+                                'count': int(name),
+                                'prize': setting
+                            }
+                            break
+                elif temp_serial_count >=1:
+                    flag = False
+                    for name in sorted(map(lambda x: (int(x),x), prize_settings.keys())):
+                        setting = prize_settings[name[1]]
+                        name = int(name[0])
+                        if flag or name>signer.serial_count + 1:
+                            next_serial_prize = {
+                                'count': name,
+                                'prize': setting
+                            }
+                            break
+                        if name == signer.serial_count + 1:
+                            serial_prize = {
+                                'count': name,
+                                'prize':setting
+                            }
+                            flag = True
+
+                prize_info = {
+                    'serial_count': signer.serial_count if signer else 0,
+                    'daily_prize': daily_prize,
+                    'serial_prize': serial_prize,
+                    'next_serial_prize': next_serial_prize,
+                }
+
         response = create_response(200)
         response.data = {
-        "member_info":member_info
+            "status":activity_status,
+            "isMember":isMember,
+            "member_info":member_info,
+            "prize_info":prize_info
         }
 
         return response.get_response()
