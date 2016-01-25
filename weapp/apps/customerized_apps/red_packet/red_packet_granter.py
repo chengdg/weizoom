8e1e0154013a7510486f41e55179078878d941c1
aix <aixiang@narzh.com>
2016-01-25 19:48:31 +0800

    update

--- weapp/apps/customerized_apps/red_packet/red_packet_granter.py
+++ weapp/apps/customerized_apps/red_packet/red_packet_granter.py
@@ -42,9 +42,9 @@ class RedPacketGranter(resource.Resource):
 		@return:
 		"""
 		record_id = request.POST.get('id', None)
-		member_id = request.POST.get('member_id', None)
+		member_ids = request.POST.get('member_ids', None)
 		response = create_response(500)
-		if not record_id or not member_id:
+		if not record_id or not member_ids:
 			response.errMsg = u'活动信息出错,请重试~'
 			return response.get_response()
 
@@ -55,31 +55,9 @@ class RedPacketGranter(resource.Resource):
 		else:
 			record = record.first()
 		owner_id = record.owner_id
-
-		member_info = app_models.RedPacketParticipance.objects(
-			belong_to=record_id,
-			member_id=member_id,
-			has_join=True,
-			red_packet_status=True,
-			is_already_paid=False
-		)
-		if member_info.count() <=0:
-			response.errMsg = u'用户信息出错: %s' % member_id
-			return response.get_response()
-		else:
-			member_info = member_info.first()
-
-		#获取应发红包信息
-		price = member_info.red_packet_money
 		wishing = record.wishing
-		SSLKEY_PATH = record.sslkey_path
-		SSLCERT_PATH = record.sslcert_path
 		record_name = record.name
-		remark = ''
-		ip = '127.0.0.1'
 
-		#获取该member_id的固定openid
-		openid = MemberHasSocialAccount.objects.filter(member_id=member_id)[0].account.openid
 		#获取商户的支付配置信息
 		pay_interface = PayInterface.objects.get(type=PAY_INTERFACE_WEIXIN_PAY, owner_id=owner_id)
 		weixin_pay_config = UserWeixinPayOrderConfig.objects.get(id=pay_interface.related_config_id)
@@ -88,22 +66,58 @@ class RedPacketGranter(resource.Resource):
 		appid_info = ComponentAuthedAppidInfo.objects.filter(auth_appid=authed_appid)[0]
 		nick_name = appid_info.nick_name
 
-		red = RedPackMessage(weixin_pay_config.partner_id, weixin_pay_config.app_id, nick_name,
-			nick_name,openid,price,price,price,1, wishing, ip,
-			record_name,
-			remark,
-			weixin_pay_config.partner_key)
+		cert_setting = app_models.RedPacketCertSettings.objects(owner_id=owner_id)
+		if cert_setting.count() > 0:
+			cert_setting = cert_setting.first()
+		else:
+			response.errMsg = u'请首先上传证书文件'
+			return response.get_response()
+		SSLKEY_PATH = cert_setting.key_path
+		SSLCERT_PATH = cert_setting.cert_path
+		if '' == SSLKEY_PATH or '' == SSLCERT_PATH:
+			response.errMsg = u'请首先上传证书文件'
+			return response.get_response()
+		remark = ''
+		try:
+			ip = request.META['REMOTE_ADDR']
+		except:
+			ip = None
 
-		result = red.post_data(SSLKEY_PATH, SSLCERT_PATH)
-		result = BeautifulSoup(result)
-		return_code = result.return_code.text
-		return_result = {
-			'code': return_code,
-			'msg': result.return_msg.text,
-			'xml': red.arrayToXml()
-		}
+		member_info_list = app_models.RedPacketParticipance.objects(
+			belong_to=record_id,
+			member_id__in=member_ids,
+			has_join=True,
+			red_packet_status=True,
+			is_already_paid=False
+		)
+		member_id2status = {m.id: m.is_subscribed for m in Member.objects.filter(id__in=member_ids)}
+		member_id2openid = {m.id: m.account.openid for m in MemberHasSocialAccount.objects.filter(member_id__in=member_ids)}
+		for member_info in member_info_list:
+			member_id = member_info.member_id
+			#非会员不发放
+			if not member_id2status[member_id]:
+				continue
+			price = member_info.red_packet_money
+			#获取该member_id的固定openid
+			openid = member_id2openid[member_id]
 
-		member_info.update(set__return_result=return_result)
+			red = RedPackMessage(weixin_pay_config.partner_id, weixin_pay_config.app_id, nick_name,
+				nick_name,openid,price,price,price,1, wishing, ip,
+				record_name,
+				remark,
+				weixin_pay_config.partner_key)
 
-		response = create_response(200 if return_code=='SUCCESS' else 500)
+			# result = red.post_data(SSLKEY_PATH, SSLCERT_PATH)
+			# result = BeautifulSoup(result)
+			# return_code = result.return_code.text
+			# return_result = {
+			# 	'code': return_code,
+			# 	'msg': result.return_msg.text,
+			# 	'xml': red.arrayToXml()
+			# }
+            #
+			# member_info.update(set__return_result=return_result)
+			#TODO 给该会员发送模板消息
+
+		response = create_response(200)
 		return response.get_response()
