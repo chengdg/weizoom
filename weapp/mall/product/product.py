03e11169928cab8daa288f2b4d58b180740d42d8
郭玉成 <1597594211@qq.com>
2016-01-26 13:50:04 +0800

    修改代码冲突

--- weapp/mall/product/product.py
+++ weapp/mall/product/product.py
@@ -409,7 +409,7 @@ class Product(resource.Resource):
             is_member_product=request.POST.get("is_member_product", False) == 'on',
             supplier=request.POST.get("supplier", 0),
             purchase_price=purchase_price,
-            is_enable_bill=is_enable_bill
+            is_enable_bill=is_enable_bill,
             is_delivery=request.POST.get('is_delivery', False)
         )
         # 设置新商品显示顺序
