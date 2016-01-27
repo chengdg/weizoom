6720697cd40fe6294731f2318ea0128c34006fac
lihanyi <lihanyi@narzh.com>
2016-01-27 15:50:43 +0800

    分数除不尽

--- weapp/features/steps/apps_redpecket_frontend_steps.py
+++ weapp/features/steps/apps_redpecket_frontend_steps.py
@@ -12,7 +12,7 @@ from modules.member import module_api as member_api
 from utils import url_helper
 import datetime as dt
 import termite.pagestore as pagestore_manager
-from apps.customerized_apps.powerme.models import PowerMe, PowerMeParticipance, PowerMeControl,PowerLog,PoweredDetail
+from apps.customerized_apps.red_packet.models import RedPacket, RedPacketParticipance, RedPacketControl,RedPacketLog,RedPacketDetail
 from weixin.message.material import models as material_models
 from modules.member.models import Member, SOURCE_MEMBER_QRCODE
 from utils.string_util import byte_to_hex
@@ -24,5 +24,13 @@ from django.core.management.base import BaseCommand, CommandError
 from utils.cache_util import SET_CACHE
 from modules.member.models import Member
 
+def __get_red_packet_rule_name(title):
+	material_url = material_models.News.objects.get(title=title).url
+	red_packet_rule_name = material_url.split('-')[1]
+	return red_packet_rule_name
 
+def __get_red_packet_rule_id(red_packet_rule_name):
+	return RedPacket.objects.get(name=red_packet_rule_name).id
 
+def __get_channel_qrcode_name(red_packet_rule_id):
+	return RedPacket.objects.get(id=red_packet_rule_id).qrcode['name']
