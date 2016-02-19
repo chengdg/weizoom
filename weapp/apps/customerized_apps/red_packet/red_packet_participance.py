e62e5cff25594ccdc3f3d4175df19e86375878ee
lihanyi <lihanyi@narzh.com>
2016-02-19 13:44:13 +0800

    增加0元红包的保险

--- weapp/apps/customerized_apps/red_packet/red_packet_participance.py
+++ weapp/apps/customerized_apps/red_packet/red_packet_participance.py
@@ -222,10 +222,15 @@ def participate_red_packet(record_id,member_id):
 						return response.get_response()
 				else:
 					red_packet_money = red_packet_info.regular_per_money #普通红包领取定额金额
-				participate_member_info.update(set__has_join=True,set__created_at=datetime.now(),set__red_packet_money=red_packet_money)
+				if red_packet_money <= 0:
+					response = create_response(500)
+					response.errMsg = u'抢红包的人太多啦，请稍后再试~'
+					return response.get_response()
+				else:
+					participate_member_info.update(set__has_join=True,set__created_at=datetime.now(),set__red_packet_money=red_packet_money)
 			else:
 				response = create_response(500)
-				response.errMsg = u'is_run_out'
+				response.errMsg = 'is_run_out'
 				return response.get_response()
 		response = create_response(200)
 		return response.get_response()
