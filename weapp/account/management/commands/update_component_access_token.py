906a90eede94da6f00b100f46aabeaac9df8645e
bert <1597594211@qq.com>
2016-02-29 11:33:43 +0800

    update code service oauth

--- weapp/account/management/commands/update_component_access_token.py
+++ weapp/account/management/commands/update_component_access_token.py
@@ -23,7 +23,7 @@ class Command(BaseCommand):
 	help = "init global navbar for all user"
 	args = ''
 
-	def handle(self, **options):
+	def handle(self, **options):gin
 		for component in ComponentInfo.objects.filter(is_active=True):
 			weixin_api = WeixinApi(None, weixin_http_client)
 			from weixin.message.message_handler.tasks import record_call_weixin_api
