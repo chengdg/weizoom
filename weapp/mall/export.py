4624d9dc39ea043872443c5b22dddf51882f7c07
bert <1597594211@qq.com>
2016-03-01 22:06:48 +0800

    test003

--- weapp/mall/export.py
+++ weapp/mall/export.py
@@ -500,7 +500,7 @@ def get_config_second_navs(request):
         pass
     else:
         user_list_for_supplier_list = ('devceshi', 'wzjx001', 'ceshi001', 'weizoomxs', 'weizoommm', 'weshop', 'weizoomclub', 'weizoomshop', 'weizoombfm',
-            'jobs', 'wz01', 'wz02', 'wz03')
+            'jobs', 'wz01', 'wz02', 'wz03', 'test003')
         if request.user.username not in user_list_for_supplier_list:
             nav = {"navs":CONFIG_NAV['navs'][:-1]}
             second_navs = [nav]
