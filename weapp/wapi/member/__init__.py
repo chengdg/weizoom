a99c332d4205bec199722575f251f0ae712a8806
lei <352773277@qq.com>
2016-03-04 14:30:31 +0800

    增加渠道扫码效果接口

--- weapp/wapi/member/__init__.py
+++ weapp/wapi/member/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 import member_base_buy_info
+import qrcode_effect_info
---
+++ weapp/wapi/member/qrcode_effect_info.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+from core import api_resource
+from core import paginator, dateutil
+from wapi.decorators import param_required
+from django.db.models import Q
+
+from modules.member import models as member_models
+from mall import models as mall_models
+from market_tools.tools.channel_qrcode.models import ChannelQrcodeSettings,ChannelQrcodeHasMember
+
+SKEP_ACCOUNT2WEBAPP_ID = {
+	'jingxuan': '3621',
+	'xuesheng': '3807',
+	'mama': '3806',
+	'club': '3936'
+}
+
+class QrcodeEffectInfo(api_resource.ApiResource):
+	"""
+	获取会员基本信息
+	购买次数，付款金额，最后一次支付时间，客单价，推荐人，创建时间
+	"""
+	app = 'qrcode'
+	resource = 'qrcode_effect_info'
+
+	@param_required(['setting_ids', 'count_per_page', 'cur_page'])
+	def get(args):
+		"""
+		获取商品和供应商信息
+
+		@param setting_id 渠道扫码二维码id
+		@param count_per_page 每页个数
+		@param cur_page 第几页
+		@param is_all 是否获取全部会员，如果否，则获取
+		"""
+		setting_ids = args['setting_ids'].split(',')
+		count_per_page = int(args['count_per_page'])
+		cur_page = int(args['cur_page'])
+		print 'setting_ids:', setting_ids
+		print 'count_per_page:', count_per_page
+		print 'cur_page:', cur_page
+
+		if setting_ids and setting_ids != '':
+			relations = ChannelQrcodeHasMember.objects.filter(channel_qrcode_id__in=setting_ids)
+			setting_id2count = {}
+			for r in relations:
+
+				print setting_id2count,r.member_id
+				if r.is_new:
+					member = member_models.Member.objects.get(id=r.member_id)
+					if r.channel_qrcode_id in setting_id2count:
+						print "zl-------------1",setting_id2count
+						setting_id2count[r.channel_qrcode_id]['count'] += 1
+						setting_id2count[r.channel_qrcode_id]['pay_money'] += member.pay_money
+					else:
+						print "zl-------------2",setting_id2count
+						setting_id2count[r.channel_qrcode_id] = {}
+						setting_id2count[r.channel_qrcode_id]['count'] = 1
+						setting_id2count[r.channel_qrcode_id]['pay_money'] = member.pay_money
+
+		items = []
+		for x,y in setting_id2count.items():
+			items.append({
+				'setting_id': x,
+				'pay_money': '%.2f' % y['pay_money'],
+				'count': y['count']
+			})
+		pageinfo, datas = paginator.paginate(items, cur_page, count_per_page)
+		return {
+			'code' : 200,
+			'items': datas,
+			'page_count': pageinfo.max_page,
+			'page_info':paginator.to_dict(pageinfo)
+		}
