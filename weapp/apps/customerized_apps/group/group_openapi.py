5965491f77370ef26944d3d14b38d7e725a5dd68
lihanyi <lihanyi@narzh.com>
2016-03-09 17:34:20 +0800

    加保险

--- weapp/apps/customerized_apps/group/group_openapi.py
+++ weapp/apps/customerized_apps/group/group_openapi.py
@@ -42,15 +42,23 @@ class GroupBuyInfo(resource.Resource):
 		"""
 		获取团购商品信息
 		"""
+		pid = 0
+		group_buy_price = 0
+		activity_id = ''
+		activity_url = ''
 		group_id = request.GET.get('group_id')
 		webapp_owner_id = request.webapp_owner_info.auth_appid_info.appid
-		group_record = app_models.GroupRelations.objects.get(id=group_id)
-		activity_id = group_record.belong_to
-		activity_url = '/m/apps/group/m_group/?webapp_owner_id='+webapp_owner_id+'&id='+activity_id
+		group_record = app_models.GroupRelations.objects(id=group_id)
+		if group_record.count() > 0:
+			group_record = group_record.first()
+			pid = group_record.product_id
+			group_buy_price = group_record.group_price
+			activity_id = group_record.belong_to
+			activity_url = '/m/apps/group/m_group/?webapp_owner_id='+webapp_owner_id+'&id='+activity_id
 		response = create_response(200)
 		response.data = {
-			'pid': group_record.product_id,
-			'group_buy_price': group_record.group_price,
+			'pid': pid,
+			'group_buy_price': group_buy_price,
 			'group_id': group_id,
 			'activity_id': activity_id,
 			'activity_url': activity_url,
@@ -70,16 +78,21 @@ class CheckGroupBuy(resource.Resource):
 		pid = request.GET.get('pid')
 		is_success = False
 		reason = 'check_group_buy_fail'
-		group_record = app_models.GroupRelations.objects.get(id=group_id)
-		if member_id in group_record.grouped_member_ids:
-			is_success = True
-			reason = 'check_group_buy_success'
+		group_buy_price = 0
+
+		group_record = app_models.GroupRelations.objects(id=group_id)
+		if group_record.count() > 0:
+			group_record = group_record.first()
+			group_buy_price = group_record.group_price
+			if member_id in group_record.grouped_member_ids and group_record.is_valid:
+				is_success = True
+				reason = 'check_group_buy_success'
 		response = create_response(200)
 		response.data = {
 			'is_success': is_success,
 			'reason': reason,
 			'pid': pid,
-			'group_buy_price': group_record.group_price,
+			'group_buy_price': group_buy_price,
 			'group_id': group_id,
 		}
 		return response.get_response()
