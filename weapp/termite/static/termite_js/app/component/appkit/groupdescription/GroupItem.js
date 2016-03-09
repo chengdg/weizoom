5fe00cc999e79c2cebe390c61220fd4948fbcdfd
Mark24 <mark.zhangyoung@qq.com>
2016-03-09 14:54:28 +0800

    天，团，交互

--- weapp/termite/static/termite_js/app/component/appkit/groupdescription/GroupItem.js
+++ weapp/termite/static/termite_js/app/component/appkit/groupdescription/GroupItem.js
@@ -72,6 +72,62 @@ W.component.appkit.GroupItem = W.component.Component.extend({
 					$cur_target.find('.group_type').html(''+value+'人团');
 				}
 			}
+		},
+		group_days:function($node, model, value, $propertyViewNode){
+			var that = this;
+			var GroupArray = $propertyViewNode.parent().children('.propertyGroup_property_dynamicControlField_control');
+			var cidArray = [];
+			for(var i=0;i<GroupArray.length;i++){
+				var cid =$(GroupArray[i]).attr('data-dynamic-cid');
+				cidArray.push(cid);
+			}
+			var localCid = that.cid;
+			var maxCid = Math.max.apply(this,cidArray);
+
+			console.log('localCid:'+localCid);
+			console.log('cidArray:'+cidArray);
+			console.log('maxCid:'+maxCid);
+			if(cidArray.length==1){
+				var $cur_target = $node.find('.wui-i-group1');
+				$cur_target.find('.group_days').html('拼团时间'+value+'天');
+			}else{
+				if(localCid<maxCid){
+					var $cur_target = $node.find('.wui-i-group1');
+					$cur_target.find('.group_days').html('拼团时间'+value+'天');
+				}else{
+					var $cur_target = $node.find('.wui-i-group2');
+					$cur_target.find('.group_days').html('拼团时间'+value+'天');
+				}
+			}
+		},
+		group_price:function($node, model, value, $propertyViewNode){
+			var that = this;
+			var GroupArray = $propertyViewNode.parent().children('.propertyGroup_property_dynamicControlField_control');
+			var cidArray = [];
+			for(var i=0;i<GroupArray.length;i++){
+				var cid =$(GroupArray[i]).attr('data-dynamic-cid');
+				cidArray.push(cid);
+			}
+			var localCid = that.cid;
+			var maxCid = Math.max.apply(this,cidArray);
+
+			console.log('localCid:'+localCid);
+			console.log('cidArray:'+cidArray);
+			console.log('maxCid:'+maxCid);
+			if(cidArray.length==1){
+				var $cur_target = $node.find('.wui-i-group1');
+				$cur_target.find('.group_price').html('团购价：'+value+'元');
+			}else{
+				if(localCid<maxCid){
+					var $cur_target = $node.find('.wui-i-group1');
+					$cur_target.find('.group_price').html('团购价：'+value+'元');
+				}else{
+					var $cur_target = $node.find('.wui-i-group2');
+					$cur_target.find('.group_price').html('团购价：'+value+'元');
+				}
+			}
 		}
+
+
 	}
 });
