c02f3d25cd6ad4a747762ee65aa598f59bc9e093
Eugene <237192896@qq.com>
2016-03-10 15:27:36 +0800

    fix bug

--- weapp/mall/order/util.py
+++ weapp/mall/order/util.py
@@ -718,9 +718,11 @@ def get_detail_response(request):
             child_orders = [order]
         if len(child_orders) > 1 and order.status > ORDER_STATUS_CANCEL:
             order.actions = get_order_actions(order, is_detail_page=True, is_list_parent=True, mall_type=request.user_profile.webapp_type)
-        else:
+        elif len(child_orders) == 1:
             child_orders[0].pay_interface_type = order.pay_interface_type
             order.actions = get_order_actions(child_orders[0], is_detail_page=True, mall_type=request.user_profile.webapp_type)
+        else:
+            order.actions = get_order_actions(order, is_detail_page=True, mall_type=request.user_profile.webapp_type)
         supplier_ids = []
         supplier_user_ids = []
         for child_order in child_orders:
