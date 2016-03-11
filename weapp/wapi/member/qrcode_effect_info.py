d809ffaa0af3870c80bd8cda2fb8918fcc255f32
lei <352773277@qq.com>
2016-03-11 11:16:45 +0800

    修复查询条件 渠道扫码

--- weapp/wapi/member/qrcode_effect_info.py
+++ weapp/wapi/member/qrcode_effect_info.py
@@ -43,27 +43,28 @@ class QrcodeEffectInfo(api_resource.ApiResource):
 			setting_id2member_id = {}
 			setting_id2count = {}
 			for r in relations:
-				if r.is_new:
-					member = member_models.Member.objects.get(id=r.member_id)
-					if r.channel_qrcode_id in setting_id2count:
-						setting_id2count[r.channel_qrcode_id]['count'] += 1
-						setting_id2count[r.channel_qrcode_id]['pay_money'] += member.pay_money
-					else:
-						setting_id2count[r.channel_qrcode_id] = {}
-						setting_id2count[r.channel_qrcode_id]['count'] = 1
-						setting_id2count[r.channel_qrcode_id]['pay_money'] = member.pay_money
 
-					if r.channel_qrcode_id in setting_id2member_id:
-						setting_id2member_id[r.channel_qrcode_id].append(member.id)
-					else:
-						setting_id2member_id[r.channel_qrcode_id] =[]
-						setting_id2member_id[r.channel_qrcode_id].append(member.id)
+				member = member_models.Member.objects.get(id=r.member_id)
+				if r.channel_qrcode_id in setting_id2count:
+					setting_id2count[r.channel_qrcode_id]['count'] += 1
+					setting_id2count[r.channel_qrcode_id]['pay_money'] += member.pay_money
+				else:
+					setting_id2count[r.channel_qrcode_id] = {}
+					setting_id2count[r.channel_qrcode_id]['count'] = 1
+					setting_id2count[r.channel_qrcode_id]['pay_money'] = member.pay_money
+
+				if r.channel_qrcode_id in setting_id2member_id:
+					setting_id2member_id[r.channel_qrcode_id].append(member.id)
+				else:
+					setting_id2member_id[r.channel_qrcode_id] =[]
+					setting_id2member_id[r.channel_qrcode_id].append(member.id)
 			for sx,sy in setting_id2member_id.items():
 				webapp_users = member_models.WebAppUser.objects.filter(member_id__in=sy)
 				webapp_user_id2member_id = dict([(u.id, u.member_id) for u in webapp_users])
 				webapp_user_ids = set(webapp_user_id2member_id.keys())
+				setting_id_created_time = ChannelQrcodeSettings.objects.get(id = sx).created_at
 				orders = Order.by_webapp_user_id(webapp_user_ids).filter(status__in=(ORDER_STATUS_PAYED_SUCCESSED,ORDER_STATUS_PAYED_NOT_SHIP,
-																					 ORDER_STATUS_PAYED_SHIPED, ORDER_STATUS_SUCCESSED))
+																					 ORDER_STATUS_PAYED_SHIPED, ORDER_STATUS_SUCCESSED),created_at__gt = setting_id_created_time)
 				setting_id2count[sx]['cash'] = 0
 				setting_id2count[sx]['card'] = 0
 
