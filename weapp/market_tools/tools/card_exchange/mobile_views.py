41ec7b3bbd0d08a886900ebe613b9a0bc646578e
sunhan1024 <583013043@qq.com>
2016-03-14 17:44:55 +0800

    判断会员是否已经绑定过手机号

--- weapp/market_tools/tools/card_exchange/mobile_views.py
+++ weapp/market_tools/tools/card_exchange/mobile_views.py
@@ -8,6 +8,7 @@ from django.template import Context, RequestContext
 from django.shortcuts import render_to_response
 
 from mall.promotion.card_exchange import CardExchange
+from modules.member.models import MemberInfo
 
 template_path_items = os.path.dirname(__file__).split(os.sep)
 TEMPLATE_DIR = '%s/templates' % template_path_items[-1]
@@ -17,23 +18,28 @@ def get_page(request):
 	"""
 	手机端卡兑换页
 	"""
-	# webapp_id = request.user_profile.webapp_id
-	webapp_id = request.GET.get('webapp_id','')
+	print '-----========----------'
+	webapp_id = request.user_profile.webapp_id
 	#判断用户是否绑定手机号
-	# member_id = request.member.id
-	# member_integral = request.member.integral
-	# try:
-	#     member_info = MemberInfo.objects.get(member_id = member_id)
-	#     member_is_bind = member_info.is_bind
-	# except:
-	#     member_is_bind = False
+	member_id = request.member.id
+	member_integral = request.member.integral
+	phone_number = ''
+	try:
+		member_info = MemberInfo.objects.get(member_id = member_id)
+		member_is_bind = member_info.is_binded
+		if member_is_bind:
+			phone_number = member_info.phone_number
+	except Exception,e:
+		print e,'+++++++------++++++++'
+		member_is_bind = False
 
 	card_exchange_dic = CardExchange.get_can_exchange_cards(request,webapp_id)
 
 	c = RequestContext(request, {
 		'card_exchange_rule': card_exchange_dic,
-		# 'member_is_bind': member_is_bind,
-		# 'member_integral': member_integral
+		'member_is_bind': member_is_bind,
+		'member_integral': member_integral,
+		'phone_number': phone_number
 	})
 
 	return render_to_response('%s/card_exchange/webapp/m_card_exchange.html' % TEMPLATE_DIR, c)
