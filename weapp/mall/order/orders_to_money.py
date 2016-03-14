3c39c090c283861f9c66dae83390ed855ac391bf
陈蕾 <chenlei@weizoom.com>
2016-03-14 11:01:55 +0800

    mod

--- weapp/mall/order/orders_to_money.py
+++ weapp/mall/order/orders_to_money.py
@@ -32,7 +32,7 @@ class OrderList(resource.Resource):
     @login_required
     def api_get(request):
         import requests
-        r = requests.get('http://dev.money.com/weapp/api/orders_to_money/', data={"user_id": request.user.id})
+        r = requests.get('http://dev.money.com/weapp/api/orders_to_money/', params={"user_id": request.user.id})
         response = create_response(200)
         response.data = json.loads(r.text)
         return response.get_jsonp_response(request)
@@ -60,10 +60,43 @@ class OrderMoneyList(resource.Resource):
     @login_required
     def api_get(request):
         import requests
-        r = requests.get('http://dev.money.com/weapp/cost_order_list/', data={"user_id": request.user.id})
+        r = requests.get('http://dev.money.com/weapp/cost_order_list/', params={"user_id": request.user.id})
+        print r.url
         response = create_response(200)
         response.data = json.loads(r.text)
         return response.get_jsonp_response(request)
+
+class OrderMoneyList(resource.Resource):
+    """
+    订单列表资源
+    """
+    app = "mall2"
+    resource = "re_submit_list"
+
+    @login_required
+    def get(request):
+        """
+        显示订单列表
+
+        """
+        cost_id = request.GET.get('cost_id', '')
+        c = RequestContext(request, {
+            'first_nav_name': export.ORDER_FIRST_NAV,
+            'second_navs': export.get_mall_order_second_navs(request),
+            'second_nav_name': export.ORDER_MONEY,
+            'cost_id': cost_id
+        })
+        return render_to_response('mall/editor/create_orders_to_money.html', c)
+
+    @login_required
+    def api_get(request):
+        import requests
+        cost_id = request.POST.get('cost_id', '')
+        r = requests.get('http://dev.money.com/weapp/re_submit_list/', params={"user_id": request.user.id, "cost_id": cost_id})
+        response = create_response(200)
+        response.data = json.loads(r.text)
+        return response.get_jsonp_response(request)
+
 
 
 
