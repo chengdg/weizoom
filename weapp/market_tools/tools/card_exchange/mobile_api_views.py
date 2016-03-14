8e29211dfd3974f69e56058b3891fe450f83dde7
sunhan1024 <583013043@qq.com>
2016-03-15 02:01:55 +0800

    卡兑换

--- weapp/market_tools/tools/card_exchange/mobile_api_views.py
+++ weapp/market_tools/tools/card_exchange/mobile_api_views.py
@@ -3,16 +3,10 @@
 __author__ = 'aix'
 
 from core.jsonresponse import create_response
-
-def bind_phone(request):
-	"""
-	绑定手机的api请求
-	@param request:
-	@return:
-	"""
-	#TODO
-	response = create_response(200)
-	return response.get_response()
+from webapp.modules.user_center import request_api_util
+from market_tools.tools.weizoom_card import models as card_models
+from mall.promotion import models as promotion_models
+from mall.promotion import card_exchange
 
 def exchange_card(request):
 	"""
@@ -20,6 +14,29 @@ def exchange_card(request):
 	@param request:
 	@return:
 	"""
-	#TODO
+	print '222###############111111111111'
+	s_num = request.POST.get('s_num','')
+	end_num = request.POST.get('end_num','')
+	owner_id = request.webapp_owner_id
+
+	card_ids_list = card_exchange.get_can_exchange_cards_list(s_num,end_num,owner_id)
+
+	webapp_id = request.user_profile.webapp_id
+	member_id = request.member.id
+	owner_name = request.member.username_for_html
+
+	promotion_models.CardHasExchanged.objects.create(
+ 		webapp_id = webapp_id,
+ 		card_id = card_ids_list[0],
+ 		owner_id = member_id,
+ 		owner_name = owner_name 
+ 	)
+
 	response = create_response(200)
 	return response.get_response()
+
+def send_captcha(request):
+	return request_api_util.send_captcha(request)
+
+def binding_phone(request):
+	return request_api_util.binding_phone(request)
