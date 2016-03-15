6ced77c734a95484964b433b9b1edde053d0c5c2
陈蕾 <chenlei@weizoom.com>
2016-03-15 09:24:34 +0800

    mod

--- weapp/mall/order/orders_to_money.py
+++ weapp/mall/order/orders_to_money.py
@@ -7,6 +7,8 @@ from django.template import RequestContext
 from mall import export
 from django.shortcuts import render_to_response
 from core.jsonresponse import JsonResponse, create_response
+from weapp import settings
+import requests
 
 
 class OrderList(resource.Resource):
@@ -31,10 +33,13 @@ class OrderList(resource.Resource):
 
     @login_required
     def api_get(request):
-        import requests
-        r = requests.get('http://dev.money.com/weapp/api/orders_to_money/', params={"user_id": request.user.id})
-        response = create_response(200)
-        response.data = json.loads(r.text)
+        r = requests.get(settings.MONEY_HOST + '/weapp/api/orders_to_money/', params={"user_id": request.user.id})
+        if r.status_code == 200:
+            response = create_response(200)
+            response.data = json.loads(r.text)
+        else:
+            response = create_response(500)
+            response.data.erMg = u'请求失败'
         return response.get_jsonp_response(request)
 
 class OrderMoneyList(resource.Resource):
@@ -60,13 +65,12 @@ class OrderMoneyList(resource.Resource):
     @login_required
     def api_get(request):
         import requests
-        r = requests.get('http://dev.money.com/weapp/cost_order_list/', params={"user_id": request.user.id})
-        print r.url
+        r = requests.get(settings.MONEY_HOST + '/weapp/cost_order_list/', params={"user_id": request.user.id})
         response = create_response(200)
         response.data = json.loads(r.text)
         return response.get_jsonp_response(request)
 
-class OrderMoneyList(resource.Resource):
+class ReOrderMoneyList(resource.Resource):
     """
     订单列表资源
     """
@@ -92,7 +96,44 @@ class OrderMoneyList(resource.Resource):
     def api_get(request):
         import requests
         cost_id = request.GET.get('cost_id', '')
-        r = requests.get('http://dev.money.com/weapp/re_submit_list/', params={"user_id": request.user.id, "cost_id": cost_id})
+        r = requests.get(settings.MONEY_HOST + '/weapp/re_submit_list/', params={"user_id": request.user.id, "cost_id": cost_id})
+        response = create_response(200)
+        response.data = json.loads(r.text)
+        return response.get_jsonp_response(request)
+
+
+class OrderMoneyViewList(resource.Resource):
+    """
+    订单列表资源
+    """
+    app = "mall2"
+    resource = "cost_view_list"
+
+    @login_required
+    def get(request):
+        """
+        显示订单列表
+
+        """
+        cost_id = request.GET.get('cost_id', '')
+        cost_status = request.GET.get('cost_status','')
+        is_weapp = request.GET.get('is_weapp', '')
+        c = RequestContext(request, {
+            'first_nav_name': export.ORDER_FIRST_NAV,
+            'second_navs': export.get_mall_order_second_navs(request),
+            'second_nav_name': export.ORDER_MONEY,
+            'cost_id': cost_id,
+            'cost_status': cost_status,
+            'is_weapp': is_weapp
+        })
+        return render_to_response('mall/editor/create_orders_to_money.html', c)
+
+    @login_required
+    def api_get(request):
+        cost_id = request.GET.get('cost_id', '')
+        cost_status = request.GET.get('cost_status', '')
+        is_weapp = request.GET.get('is_weapp', '')
+        r = requests.get(settings.MONEY_HOST + '/weapp/cost_view_list/', params={"user_id": request.user.id, "cost_id": cost_id, "cost_status": cost_status,"is_weapp": is_weapp})
         response = create_response(200)
         response.data = json.loads(r.text)
         return response.get_jsonp_response(request)
