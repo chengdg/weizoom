715d6a20e2b3f16cffcc3e74c8210660cb4747aa
陈蕾 <chenlei@weizoom.com>
2016-03-16 14:22:29 +0800

    微众卡添加满额使用

--- weapp/market_tools/tools/weizoom_card/models.py
+++ weapp/market_tools/tools/weizoom_card/models.py
@@ -46,6 +46,7 @@ class WeizoomCardRule(models.Model):
 	shop_limit_list = models.CharField(max_length=2048, default='-1') #专属商家
 	shop_black_list = models.CharField(max_length=2048, default='-1') #不能使用微众卡的商家
 	is_new_member_special = models.BooleanField(default=False) #是否为新会员专属卡
+	valid_restrictions = models.DecimalField(max_digits=65, decimal_places=2) #订单满多少可以使用规则
 
 	@staticmethod
 	def get_all_weizoom_card_rules_list(user):
