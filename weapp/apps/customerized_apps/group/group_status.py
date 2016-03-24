6d08c23fc17684018323e1921c5a495f179f65f7
lihanyi <lihanyi@narzh.com>
2016-03-24 18:16:13 +0800

    错误print

--- weapp/apps/customerized_apps/group/group_status.py
+++ weapp/apps/customerized_apps/group/group_status.py
@@ -71,7 +71,7 @@ class GroupStatus(resource.Resource):
 					member_info_list = [{"member_id": group_detail.grouped_member_id, "order_id": group_detail.order_id} for group_detail in group_details]
 					send_group_template_message(activity_info, member_info_list)
 				except:
-					print(u'发送拼团成功模板消息失败')
+					print(u'发送拼团失败模板消息失败')
 
 		elif target_status == 'running':
 			#说明手动点击开启了
