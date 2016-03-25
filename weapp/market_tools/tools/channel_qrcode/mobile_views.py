bb8f460e5eeb41a2f4d3d37254cffac4852f724e
sunhan1024 <583013043@qq.com>
2016-03-25 14:00:58 +0800

    update

--- weapp/market_tools/tools/channel_qrcode/mobile_views.py
+++ weapp/market_tools/tools/channel_qrcode/mobile_views.py
@@ -243,8 +243,8 @@ def get_settings_detail(request):
                     'pay_money': '%.2f' %  pay_money,
                     'payed_count': len(set(payed_member)),
                     'bind_phone_members_count': len(bind_phone_members),
-                    'startDate': startDate,
-                    'endDate': endDate
+                    'startDate': startDate.replace('-','/').replace(' ','/')[:-3] if startDate else None,
+                    'endDate': endDate.replace('-','/').replace(' ','/')[:-3] if endDate else None
                 })
             return render_to_response('%s/channel_qrcode/webapp/channel_qrcode_members.html' % TEMPLATE_DIR, c)
 
