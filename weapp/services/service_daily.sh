0f85184d139d2db84df5c2962a9cff777f7087d0
侯庭飞 <houtingfei@weizoom.com>
2016-03-31 21:25:39 +0800

    [f_mall_member_detail][侯庭飞]修改定时任务：更新30天的购买次数

--- weapp/services/service_daily.sh
+++ weapp/services/service_daily.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 export PATH=/usr/local/bin:$PATH
+# to be called minutely
+DATE="`date +%Y%m%d`"
+TIME="`date +%Y%m%d%H%M%S`"
+ROOT_DIR="/weapp/web/weapp"
+#ROOT_DIR="`pwd`"
+LOG_DIR="$ROOT_DIR/../log/services/$DATE"
+mkdir -p $LOG_DIR
+
+LOG="$LOG_DIR/minute_$TIME.log"
+echo "called time: $TIME" > $LOG
+echo "LOG_DIR=$LOG_DIR"
+
+echo "ROOT_DIR=$ROOT_DIR"
+cd $ROOT_DIR
+echo "========================================================" >> $LOG
 
 
-echo ">> calling 'apps_powerme_timer_task'"
+echo ">> calling 'update_member_purchase_frequency'"
 echo "--------------------------------------------------------" >> $LOG
 python manage.py update_member_purchase_frequency
