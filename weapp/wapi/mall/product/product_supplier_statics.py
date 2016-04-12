3badfc0d019f704f8ba0533fd4c7f81a85352c16
lei <352773277@qq.com>
2016-04-12 19:26:42 +0800

    更改返回的数据格式

--- weapp/wapi/mall/product/product_supplier_statics.py
+++ weapp/wapi/mall/product/product_supplier_statics.py
@@ -22,14 +22,13 @@ class ProductSupplierStatics(api_resource.ApiResource):
 		@param supplier_user_id 同步商品的所属商家ID
 		"""
 		product_ids = args.get('product_ids', None)
-		return_list = {}
+		data = {}
 		if product_ids:
 			order_has_products =mall_models.OrderHasProduct.objects.filter(product_id__in=product_ids.split(','))
 			if order_has_products.count()>0:
 				order_dot_ids = [order.order_id for order in order_has_products]
 				orders_list = mall_models.Order.objects.filter(id__in=order_dot_ids,status__in=[3,4,5]).values('payment_time').annotate(today_order_num=Count('payment_time'))
 				print "zl--------------------------",orders_list
-				data = {}
 				for order in orders_list:
 					order_pay_time = datetime.strftime(order['payment_time'],'%Y-%m-%d')
 					if order_pay_time in data:
@@ -37,8 +36,15 @@ class ProductSupplierStatics(api_resource.ApiResource):
 					else:
 						data[order_pay_time] = order['today_order_num']
 
+		data= sorted(data.iteritems(), key=lambda d:d[0])
+		# data 更改为了list类型
+		datalist = []
+		vallist = []
+		for d in data:
+			datalist.append(d[0])
+			vallist.append(d[1])
 
-		print "zl----------------------",data
 		return {
-					'data':data
+					'datelist':datalist,
+					 'vallist':vallist
 				}
