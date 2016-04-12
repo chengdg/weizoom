a7ddf5cb23956aab59cf1792e44310ff727f2dd8
chenlei <chenlei@weizoom.com>
2016-04-12 11:12:40 +0800

    修改setp

--- static/component/card/limit_cards/LimitCardsPage.react.js
+++ static/component/card/limit_cards/LimitCardsPage.react.js
@@ -73,8 +73,7 @@ var OrdinaryCardsPage = React.createClass({
 		<div className="mt15">
 			<Reactman.TablePanel>
 				<Reactman.TableActionBar>
-					<Reactman.TableActionButton text="导出" icon="plus" href="#" />
-					<Reactman.TableActionButton text="创建新卡" icon="plus" href="/card/create_limit/" />
+					<Reactman.TableActionButton text="创建新卡" icon="plus" href="/card/limit/" />
 				</Reactman.TableActionBar>
 				<Reactman.Table resource={productsResource} formatter={this.rowFormatter} pagination={true} ref="table">
 					<Reactman.TableColumn name="卡号" field="weizoom_card_id" />
