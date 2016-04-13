37ac20187da4a1352c535bb4f1611ab34fc25297
zhoukang <1120429511@qq.com>
2016-04-13 16:11:20 +0800

    小功能

--- static/component/order/approval_card/ApprovalDialog.react.js
+++ static/component/order/approval_card/ApprovalDialog.react.js
@@ -8,6 +8,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Action = require('.././rule_order/Action');
 var Reactman = require('reactman');
+var CardTable = require('./ApprovalDialogTable.react');
 
 
 var ApprovalDialog = Reactman.createDialog({
@@ -80,63 +81,5 @@ var ApprovalDialog = Reactman.createDialog({
 		)
 	}
 })
-//
-var CardTable = React.createClass({
-	rowFormatter: function(field, value, data) {
-		// if (field === 'models') {
-		// 	var models = value;
-		// 	var modelEls = models.map(function(model, index) {
-		// 		return (
-		// 			<div key={"model"+index}>{model.name} - {model.stocks}</div>
-		// 		)
-		// 	});
-		// 	return (
-		// 		<div style={{color:'red'}}>{modelEls}</div>
-		// 	);
-		// } else if (field === 'name') {
-		// 	return (
-		// 		<a href={'/outline/data/?id='+data.id}>{value}</a>
-		// 	)
-		// }else if (field === 'action') {
-		// 	return (
-		// 	<div>
-		// 		<a className="btn btn-link btn-xs" onClick={this.onClickDelete} data-product-id={data.id}>删除</a>
-		// 		<a className="btn btn-link btn-xs mt5" href={'/outline/data/?id='+data.id}>编辑</a>
-		// 		<a className="btn btn-link btn-xs mt5" onClick={this.onClickComment} data-product-id={data.id}>备注</a>
-		// 	</div>
-		// 	);
-		// } else {
-		// 	return value;
-		// }
-		if (field=='action') {
-			return (
-				<a className="btn btn-link btn-xs mt5" onClick={this.handleChoice}>选择</a>
-				)
-		} else {
-			return value;
-		}
-	},
-	render: function() {
-		var cardruletype = this.props.cardruletype;
-		var cardrulesResource= {
-			resource: 'order.approval_card',
-			data: {
-				cardruletype:cardruletype,
-			}
-		};
-		return (
-			<Reactman.TablePanel>
-				<Reactman.TableActionBar></Reactman.TableActionBar>
-				<Reactman.Table resource={cardrulesResource} formatter={this.rowFormatter} pagination={true} countPerPage={2} ref="table">
-					<Reactman.TableColumn name="卡名称" field="name" width="120px" />
-					<Reactman.TableColumn name="面值" field="money" />
-					<Reactman.TableColumn name="库存" field="storage_count" width="120px"/>
-					<Reactman.TableColumn name="卡类型" field="card_kind" width="80px" />
-					<Reactman.TableColumn name="卡号区间" field="card_range" />
-					<Reactman.TableColumn name="操作" field="action" width="80px" />
-				</Reactman.Table>
-			</Reactman.TablePanel>
-		);
-	}
-});
+
 module.exports = ApprovalDialog;
