72d4ec037a060e35767be90319c708605d95592b
Mark24 <mark.zhangyoung@qq.com>
2016-04-15 13:40:17 +0800

    审核通过，删除

--- weapp/features/apps/shvote/shvote_delete.feature
+++ weapp/features/apps/shvote/shvote_delete.feature
@@ -7,18 +7,19 @@ Feature: 后台操作
 		2.报名详情中该微信用户提交高级投票申请状态为“待审核”；
 		3.管理员删除该用户提交的申请；
 		4.活动详情中的报名申请记录消失；
-	
+
 
 	"""
 Background:
 	Given jobs登录系统
-	When jobs新建微信高级投票活动
+	When jobs新建高级微信投票活动
 		"""
 		[{
 			"title":"微信高级投票-进行中",
-			"group":["初中组","高中组"],
+			"groups":["初中组","高中组"],
+			"rule": "高级投票规则",
 			"desc":"高级投票活动介绍",
-			"start_date":"2天前",
+			"start_date":"今天",
 			"end_date":"2天后",
 			"pic":"3.jpg"
 		}]
@@ -31,8 +32,8 @@ Background:
 				"url": "/standard_static/test_resource_img/hangzhou1.jpg"
 			}],
 			"cover_in_the_text":"true",
-			"summary":"微信高级投票",
-			"content":"微信高级投票",
+			"summary":"微信高级投票摘要",
+			"content":"微信高级投票内容",
 			"jump_url":"微信高级投票"
 		}]
 		"""
@@ -40,61 +41,77 @@ Background:
 		"""
 		[{
 			"rules_name":"规则1",
-			"keyword": 
+			"keyword":
 				[{
 					"keyword": "微信高级投票",
 					"type": "equal"
 				}],
-			"keyword_reply": 
+			"keyword_reply":
 				[{
-					"reply_content":"微信高级投票",
+					"reply_content":"高级投票活动1单图文",
 					"reply_type":"text_picture"
 				}]
-		
+
 		}]
 		"""
+	When 清空浏览器
 	When bill关注jobs的公众号
 	When bill访问jobs的webapp
+	When bill在微信中向jobs的公众号发送消息'微信高级投票'
+	Then bill收到自动回复'高级投票活动1单图文'
+	When bill点击图文"高级投票活动1单图文"进入高级微信投票活动页面
 	When bill参加高级投票报名活动
 	"""
-		[{
+		{
+			"headImg":"bill_head.jpg",
 			"name":"bill",
-			"group":"初中组",
-			"number":"001"
-		}]
+			"group":["初中组"],
+			"number":"001",
+			"details":"bill的产品好",
+			"detail_pic":["pic1.jpg","pic2.jpg"]
+		}
 	"""
 	When dill关注jobs的公众号
 	When dill访问jobs的webapp
+	When dill在微信中向jobs的公众号发送消息'微信高级投票'
+	Then dill收到自动回复'高级投票活动1单图文'
+	When dill点击图文"高级投票活动1单图文"进入高级微信投票活动页面
 	When dill参加高级投票报名活动
 	"""
-		[{
+		{
+			"headImg":"dill_head.jpg",
 			"name":"dill",
-			"group":"高中组",
-			"number":"002"
-		}]
+			"group":["高中组"],
+			"number":"002",
+			"details":"dill的产品好",
+			"detail_pic":["pic3.jpg","pic4.jpg"]
+		}
 	"""
 
-@mall2 @apps @shvote @shvote_delete
+@mall2 @apps @shvote @shvote_delete @yang2
 Scenario:1 管理员删除微信用户提交高级投票申请，列表中消失
 	Given jobs登录系统
-	When jobs审核通过'bill'
-	When jobs删除'dill'
-	Then jobs能获得报名详情列表
+	When jobs于高级微信投票活动审核通过'bill'
+	When jobs于高级微信投票活动删除'dill'
+	Then jobs获得报名详情列表
 		"""
 		[{
+			"headImg":"bill_head.jpg",
 			"player":"bill",
 			"votes":0,
 			"number":"001",
-			"status":"审核通过"
+			"start_date":"今天",
+			"status":"审核通过",
+			"actions":["查看"]
 		}]
 		"""
 	Then jobs获得微信高级投票活动列表
 		"""
 		[{
 			"name":"微信高级投票-进行中",
-			"vote_count":0,
+			"vote_count":1,
 			"participant_count":1,
-			"start_date":"2天前",
+			"start_date":"今天",
 			"end_date":"2天后",
 			"status":"进行中",
 			"actions": ["关闭","链接","预览","报名详情","查看结果"]
---
+++ weapp/features/steps/apps_review_player_steps.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'mark24'
+
+from behave import *
+from test import bdd_util
+from collections import OrderedDict
+
+from features.testenv.model_factory import *
+import steps_db_util
+from modules.member import module_api as member_api
+from utils import url_helper
+import datetime as dt
+import termite.pagestore as pagestore_manager
+from apps.customerized_apps.shvote.models import Shvote, ShvoteParticipance, ShvoteControl,MEMBER_STATUS
+from weixin.message.material import models as material_models
+from modules.member.models import Member, SOURCE_MEMBER_QRCODE
+from utils.string_util import byte_to_hex
+import json
+import re
+
+import time
+from django.core.management.base import BaseCommand, CommandError
+from utils.cache_util import SET_CACHE
+from modules.member.models import Member
+
+
+def __date2time(date_str):
+	"""
+	字符串 今天/明天……
+	转化为字符串 "%Y-%m-%d %H:%M"
+	"""
+	cr_date = date_str
+	p_date = bdd_util.get_date_str(cr_date)
+	p_time = "{} 00:00".format(bdd_util.get_date_str(cr_date))
+	return p_time
+
+def __status2name(status_num):
+	"""
+	微助力：状态值 转 文字
+	"""
+	status2name_dic = {-1:u"全部",0:u"待审核",1:u"审核通过"}
+	return status2name_dic[status_num]
+
+def __name2status(name):
+	"""
+	微助力： 文字 转 状态值
+	"""
+	if name:
+		name2status_dic = {u"全部":-1,u"待审核":0,u"审核通过":1}
+		return name2status_dic[name]
+	else:
+		return -1
+
+def __get_actions(status):
+	"""
+	根据输入微助力状态
+	返回对于操作列表
+	"""
+	actions_list = [u"查看"]
+	if status == u"待审核":
+		actions_list = ["审核通过","删除"]+actions_list
+	elif status=="审核通过":
+		pass
+	return actions_list
+
+def __ReviewShvoteApply(context,player):
+	'''
+	审核通过
+	'''
+	design_mode = 0
+	version = 1
+	webapp_owner_id = str(context.webapp_owner_id)
+	shvote = Shvote.objects.get(owner_id=context.webapp_owner_id)
+	shvote_id = str(shvote.id)
+
+	params = {
+		"belong_to":shvote_id,
+		"name":player
+	}
+	# update_data = {}
+	# update_data['set__status'] = MEMBER_STATUS['PASSED']
+	# shvote_participance = ShvoteParticipance.objects(**params).update(**update_data)
+	one_shvote_participance = ShvoteParticipance.objects(**params)[0]
+	participance_id = str(one_shvote_participance.id)
+
+
+
+
+	review_url = "/apps/shvote/api/shvote_registrators/?design_mode={}&version={}".format(design_mode,version)
+	review_args = {
+		"id":participance_id
+	}
+
+	return context.client.post(review_url,review_args)
+
+def __DeleteShvoteApply(context,player):
+	'''
+	审核通过
+	'''
+	design_mode = 0
+	version = 1
+	webapp_owner_id = str(context.webapp_owner_id)
+	shvote = Shvote.objects.get(owner_id=context.webapp_owner_id)
+	shvote_id = str(shvote.id)
+
+	params = {
+		"belong_to":shvote_id,
+		"name":player
+	}
+	# update_data = {}
+	# update_data['set__status'] = MEMBER_STATUS['PASSED']
+	# shvote_participance = ShvoteParticipance.objects(**params).update(**update_data)
+	one_shvote_participance = ShvoteParticipance.objects(**params)[0]
+	participance_id = str(one_shvote_participance.id)
+
+
+
+
+	review_url = "/apps/shvote/api/shvote_registrators/?_method=delete&design_mode={}&version={}".format(design_mode,version)
+	review_args = {
+		"id":participance_id
+	}
+
+	return context.client.post(review_url,review_args)
+
+
+@When(u"{webapp_user_name}于高级微信投票活动审核通过'{player}'")
+def step_impl(context,webapp_user_name,player):
+	review_response = __ReviewShvoteApply(context,player)
+	bdd_util.assert_api_call_success(review_response)
+
+@When(u"{webapp_user_name}于高级微信投票活动删除'{player}'")
+def step_impl(context,webapp_user_name,player):
+	review_response = __DeleteShvoteApply(context,player)
+	bdd_util.assert_api_call_success(review_response)
