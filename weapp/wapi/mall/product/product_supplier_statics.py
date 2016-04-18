f063c7ebea781b7d1d3fd7f95058ad00c7134a23
lei <352773277@qq.com>
2016-04-18 17:38:03 +0800

    修复订单统计图和订单数量不一致的问题

--- weapp/wapi/mall/product/product_supplier_statics.py
+++ weapp/wapi/mall/product/product_supplier_statics.py
@@ -24,10 +24,10 @@ class ProductSupplierStatics(api_resource.ApiResource):
 		product_ids = args.get('product_ids', None)
 		data = {}
 		if product_ids:
-			order_has_products =mall_models.OrderHasProduct.objects.filter(product_id__in=product_ids.split(',')).exclude(order_id__icontains="s")
+			order_has_products =mall_models.OrderHasProduct.objects.filter(product_id__in=product_ids.split(','))
 			if order_has_products.count()>0:
 				order_dot_ids = [order.order_id for order in order_has_products]
-				orders_list = mall_models.Order.objects.filter(id__in=order_dot_ids,status__in=[3,4,5]).values('payment_time').annotate(today_order_num=Count('payment_time'))
+				orders_list = mall_models.Order.objects.filter(id__in=order_dot_ids,status__in=[3,4,5]).exclude(order_id__icontains="s").values('payment_time').annotate(today_order_num=Count('payment_time'))
 				print "zl--------------------------",orders_list
 				for order in orders_list:
 					order_pay_time = datetime.strftime(order['payment_time'],'%Y-%m-%d')
