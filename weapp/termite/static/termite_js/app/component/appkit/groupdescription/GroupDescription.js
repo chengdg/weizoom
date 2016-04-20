efc55d7583c141183e348c0fdbe805405b1144e8
lihanyi <lihanyi@narzh.com>
2016-04-20 13:14:36 +0800

    修正新建团购时候的部分文字与展示数据

--- weapp/termite/static/termite_js/app/component/appkit/groupdescription/GroupDescription.js
+++ weapp/termite/static/termite_js/app/component/appkit/groupdescription/GroupDescription.js
@@ -40,9 +40,16 @@ W.component.appkit.GroupDescription = W.component.Component.extend({
             isUserProperty: false,
             default: ''
         },{
+            name: 'valid_time_title',
+            type: 'title_with_nothing',
+			validate:'data-validate=""',
+            displayName: '团购时间：',
+			annotation:'注：用来控制整个团购活动的时间',
+			isUserProperty: true
+        },{
             name: 'valid_time',
             type: 'date_range_selector',
-            displayName: '起止时间：',
+            displayName: '',
             isUserProperty: true,
             validate:'data-validate=""',
             default: ''
@@ -66,7 +73,7 @@ W.component.appkit.GroupDescription = W.component.Component.extend({
             type: 'title_with_nothing',
 			validate:'data-validate=""',
             displayName: '拼团人数：',
-			annotation:'注：1个团购可创建多种拼团人数供顾客选择',
+			annotation:'注：1个团购活动可选择不同拼团人数的小团，根据不同拼团人数设置相应小团的拼团时间和团购价，顾客可以根据自己需要选择相应的小团进行拼团。',
             style: "margin-bottom: -20px;",
 			isUserProperty:true
 
@@ -92,7 +99,7 @@ W.component.appkit.GroupDescription = W.component.Component.extend({
             type: 'title_with_nothing',
 			validate:'data-validate=""',
             displayName: '团购说明：',
-			annotation:'注：请修改【发货时间】、【开团截止日期】、【商品数量】顾客会查看团购说明，请谨慎填写。',
+			annotation:'注：请填写【发货时间】、【开团截止日期】、【商品数量】顾客会查看团购说明，请谨慎填写。',
 			isUserProperty:true
 
         },{
@@ -104,7 +111,7 @@ W.component.appkit.GroupDescription = W.component.Component.extend({
             validate: 'data-validate="require-notempty::选项不能为空"',
             annotation: '注：请修改【发货时间】、【开团截止日期】、【商品数量】顾客会查看团购说明，请谨慎填写。',
             placeholder: '请简略描述活动具体规则，以及活动起止时间，客服联系电话等。',
-            default: "1.开团和拼团的顾客需要先已团购价支付商品\n2.到达团购人数上限或到达团购指定时间后，团购结束。到达团购人数上限 - 团购成功。成功后,在该商品团购结束【20】天内进行发货。在指定时间未到达团购人数上线 - 团购失败。失败后, 【5~7】个工作日完成退款\n3.本次活动商品数量为【100】份，售完活动结束。开团截止日期【201x-0x-xx】\n4.已关注的用户可在 个人中心 - 全部订单 中找到支付的团购订单和进入团购活动的入口。"
+            default: "1.开团和拼团的顾客需要先以团购价支付商品\n2.到达团购人数上限或到达团购指定时间后，团购结束。\n    到达团购人数上限 - 团购成功。\n    成功后，在该商品团购结束(        )天内进行发货。\n    在指定时间未到达团购人数上线 - 团购失败。\n    失败后, 5~7个工作日完成退款\n3.本次活动商品数量为(        )份，售完活动结束。\n开团截止日期(                )\n4.已关注的用户可在 个人中心 - 全部订单 中找到支付的团购订单和进入团购活动的入口。"
         },{
 			name: 'material_image',
 			type: 'image_dialog_select_v2',
