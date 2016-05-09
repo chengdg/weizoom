4c6320f319f9e5d5e00d9621d9813a615e68ed1b
lihanyi <lihanyi@narzh.com>
2016-05-09 15:51:32 +0800

    编辑活动feature+steps

--- weapp/features/steps/apps_rebate_backend_steps.py
+++ weapp/features/steps/apps_rebate_backend_steps.py
@@ -21,11 +21,15 @@ def __get_material_id(context,title):
     material_id = News.objects.get(material_id__in=material_ids, title=title).material_id
     return material_id
 
+def __get_rebate_id(rebate_name):
+    id = rebate_models.Rebate.objects.get(name=rebate_name).id
+    return id
+
 def __change_select(select):
     return '1'if select == "true" else '0'
 
 @when(u'{user}新建返利活动')
-def step_add_red_envelope_rule(context, user):
+def step_impl(context, user):
     rules = json.loads(context.text)
     for rule in rules:
         params = {
@@ -48,9 +52,36 @@ def step_add_red_envelope_rule(context, user):
         else:
             params['reply_type'] = 2
             params['reply_detail'] = ''
-
             params['reply_material_id'] = __get_material_id(context,rule['scan_code_reply'])
         response = context.client.post('/apps/rebate/api/rebate/?_method=put', params)
+        bdd_util.assert_api_call_success(response)
+
+@when(u'{user}编辑返利活动"{rebate_name}"')
+def step_impl(context, user, rebate_name):
+    rules = json.loads(context.text)
+    for rule in rules:
+        params = {
+            'id': __get_rebate_id(rebate_name),
+            'name': rule.get('code_name', ''),
+            'start_time': date2time(rule.get('start_time', '')),
+            'end_time': date2time(rule.get('end_time', '')),
+            'permission': __change_select(rule.get('is_attention_in', '')),
+            'is_limit_first_buy': __change_select(rule.get('is_limit_first_buy', '')),
+            'is_limit_cash': __change_select(rule.get('is_limit_cash', '')),
+            'rebate_order_price': rule.get('order_rebate').get('rebate_order_price'),
+            'rebate_money': rule.get('order_rebate').get('rebate_money'),
+            'weizoom_card_id_from': rule.get('weizoom_card_id_from', ''),
+            'weizoom_card_id_to': rule.get('weizoom_card_id_to', '')
+        }
+        if rule['reply_type'] == "文字":
+            params['reply_type'] = 1
+            params['reply_detail'] = rule['scan_code_reply']
+            params['reply_material_id'] = 0
+        else:
+            params['reply_type'] = 2
+            params['reply_detail'] = ''
+            params['reply_material_id'] = __get_material_id(context,rule['scan_code_reply'])
+        response = context.client.post('/apps/rebate/api/rebate/?_method=post', params)
         bdd_util.assert_api_call_success(response)
 
 @then(u'{user}获得返利活动列表')
