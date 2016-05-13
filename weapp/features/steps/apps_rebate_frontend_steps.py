5cbaf230c39ddffa188eb4b2ce89fa15767c7201
lihanyi <lihanyi@weizoom.com>
2016-05-13 15:21:22 +0800

    Then bill能获得返利微众卡

--- weapp/features/steps/apps_rebate_frontend_steps.py
+++ weapp/features/steps/apps_rebate_frontend_steps.py
@@ -6,7 +6,7 @@ import time
 import random
 from behave import *
 from mall.models import Order
-from modules.member.models import Member
+from modules.member.models import Member,MemberInfo
 
 from test import bdd_util
 from features.testenv.model_factory import *
@@ -73,5 +73,54 @@ def step_impl(context, user, record_name):
         if item['name'] == expected_data['code_name']:
             actual_data = item
             break
-
     bdd_util.assert_dict(expected_data, actual_data)
+
+@then(u'{webapp_user_name}能获得返利微众卡')
+def step_impl(context, webapp_user_name):
+    expected = json.loads(context.text)
+    actual = []
+
+    #获取微众卡
+    webapp_owner_id = context.webapp_owner_id
+    user = User.objects.get(id=context.webapp_owner_id)
+    openid = "%s_%s" % (webapp_user_name, user.username)
+    url = '/workbench/jqm/preview/?module=market_tool:weizoom_card&model=weizoom_card_exchange_list&action=get&workspace_id=market_tool:weizoom_card&webapp_owner_id=%s&project_id=0&fmt=%s&opid=%s' % (webapp_owner_id, context.member.token, openid)
+    url = bdd_util.nginx(url)
+    response = context.client.get(url)
+    while response.status_code == 302:
+        print('[info] redirect by change fmt in shared_url')
+        redirect_url = bdd_util.nginx(response['Location'])
+        context.last_url = redirect_url
+        response = context.client.get(bdd_util.nginx(redirect_url))
+    if response.status_code == 200:
+        cards = response.context['cards']['card']
+        print('cards!!!!!!!!!!!!!!')
+        print(cards)
+        data_cards = []
+        for card in cards:
+            data_cards.append({
+                'id': card['card_id']
+            })
+        bdd_util.assert_list(expected, actual)
+    else:
+        print('[info] redirect error,response.status_code :')
+        print(response.status_code)
+
+@when(u"{user}绑定手机号'{phone_number}'")
+def step_impl(context,user,phone_number):
+    username_hexstr = byte_to_hex(user)
+    member_id = Member.objects.get(username_hexstr = username_hexstr).id
+    member_info = MemberInfo.objects.filter(member_id = member_id)
+    if member_info.count() > 0:
+        member_info[0].update(
+            phone_number = phone_number,
+            is_binded = True
+        )
+    else:
+        MemberInfo.objects.create(
+            member_id = member_id,
+            phone_number = phone_number,
+            is_binded = True,
+            sex = 0,
+            name = user
+        )
