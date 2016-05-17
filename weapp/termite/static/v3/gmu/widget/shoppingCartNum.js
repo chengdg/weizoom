69c41780975978648cc01f10d0e564d3019f4884
liupeiyu <liupeiyu@weizoom.com>
2016-05-17 16:24:04 +0800

    微页面，获取购物车数量js实现； 并去掉 页面渲染时的购物车数量查询

---
+++ weapp/termite/static/v3/gmu/widget/shoppingCartNum.js
@@ -0,0 +1,54 @@
+/*
+Copyright (c) 2011-2012 Weizoom Inc
+*/
+
+/**
+ * 购物车角标数量
+ * author: liupeiyu
+ * 
+ * 使用示例;
+ * 计算商品促销价或会员价
+ * CalculProductMemberPriceOrPromotionPrice
+ * $('.xui-products-original').setShoppingCartNum({
+        webappUserId: webappUserId,
+        updateShoppingCartNum: function(event, data){
+            data.count;
+        }
+    });
+ */
+(function($, undefined) {
+	gmu.define('SetShoppingCartNum', {
+		options: {
+		},
+
+		_init: function(options){
+			this.webappUserId = options.webappUserId;
+		},
+
+		_create: function() {
+			// 当页面中有 购物车脚本 时，才去取购物车数量信息
+			if (this.$el.length > 0) {
+				this.getShoppingCartNum();				
+			};
+		},
+
+		getShoppingCartNum: function(){
+			var _this = this;			
+			W.getApi().call({
+				app: 'mall2',
+				api: 'shopping_cart_count',
+				method: 'get',
+				args: {
+					webapp_user_id: _this.webappUserId
+				},
+				success: function(data) {
+					_this.trigger('updateShoppingCartNum', data);
+				},
+				error: function(data) {
+
+				}
+			});
+		}
+	});
+
+})( Zepto );
