a2ae93084026faed93cc8d52f08937efa6a4e6b3
侯庭飞 <houtingfei@weizoom.com>
2016-05-18 10:41:50 +0800

    [b_mall_9402][侯庭飞]修复权限管理问题

--- weapp/mall/export.py
+++ weapp/mall/export.py
@@ -260,12 +260,12 @@ MALL_ORDER_SECOND_NAV = {
             'name': ORDER_BATCH_DELIVERY,
             'title': u'批量发货',
             'url': 'javascript:void(0);',
-            'permission': 'manage_order_batch_delirvery'
+            'permission': 'manage_order_batch_delivery'
         },{
             'name': ORDER_MONEY,
             'title': u'结算管理',
             'url': '/mall2/orders_to_money/',
-            'permission': 'manage_order_batch_delivery',
+            'permission': 'manage_order_settle',
         }
     ]
 }
