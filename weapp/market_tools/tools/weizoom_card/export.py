d55887bb151587c0afa8dc483a0388f6bf056b66
sunhan1024 <583013043@qq.com>
2016-05-31 16:11:56 +0800

    卡包fix

--- weapp/market_tools/tools/weizoom_card/export.py
+++ weapp/market_tools/tools/weizoom_card/export.py
@@ -36,12 +36,9 @@ def get_link_targets(request):
 def get_webapp_usage_link(webapp_owner_id, member):
 	workspace_template_info = 'workspace_id=market_tool:weizoom_card&webapp_owner_id=%d&project_id=0' % webapp_owner_id
 	#个人中心微众卡钱包展示名称
-	print webapp_owner_id,'webapp_owner_id 555555555555555555555555555'
 	user = User.objects.filter(id=webapp_owner_id)
-	print user, 'user 6666666666666666666666666666'
 	if user.count() > 0:
 		username = user[0].username
-		print username, 'username 777777777777777777777777777'
 		if username in ['jobs','weshop','ceshi01']:
 			return {
 				'name': u'我的卡包',
