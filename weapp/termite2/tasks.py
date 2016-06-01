c9c893a15f982ac7ee3ae7c193b1542f214375a4
bert <1597594211@qq.com>
2016-06-01 09:51:38 +0800

    update tasks

--- weapp/termite2/tasks.py
+++ weapp/termite2/tasks.py
@@ -14,7 +14,7 @@ def purge_webapp_page_from_varnish_cache(self, woid, project_id=0):
 
             #try:
             home_project = webapp_models.Project.objects.filter(owner=woid, is_active=True)[0]
-            url = "http://{}/termite2/webapp_page/?ngx_rd=1workspace_id=home_page&webapp_owner_id={}&workspace_id={}&project_id=0".format(settings.DOMAIN, woid,home_project.workspace_id)
+            url = "http://{}/termite2/webapp_page/?workspace_id=home_page&webapp_owner_id={}&workspace_id={}&project_id=0".format(settings.DOMAIN, woid,home_project.workspace_id)
             print "home_page_url++++>>>>>", url
             request = urllib2.Request(url)
             request.get_method = lambda: 'BAN'
@@ -23,7 +23,7 @@ def purge_webapp_page_from_varnish_cache(self, woid, project_id=0):
             #     pass
             # for project in webapp_models.Project.objects.filter(owner=woid):
             #     #try:
-            #     url = "http://{}/termite2/webapp_page/?ngx_rd=1workspace_id=home_page&webapp_owner_id={}&project_id={}".format(settings.DOMAIN, woid, project.id)
+            #     url = "http://{}/termite2/webapp_page/?orkspace_id=home_page&webapp_owner_id={}&project_id={}".format(settings.DOMAIN, woid, project.id)
             #     print "project1++++>>>>>", url
             #     request = urllib2.Request(url)
             #     #request.get_method = lambda: 'BAN'
@@ -34,18 +34,18 @@ def purge_webapp_page_from_varnish_cache(self, woid, project_id=0):
             project = webapp_models.Project.objects.get(id=project_id)
             if project.is_active:
                 home_project = webapp_models.Project.objects.filter(owner=woid, is_active=True)[0]
-                url = "http://{}/termite2/webapp_page/?ngx_rd=1&workspace_id=home_page&webapp_owner_id={}&workspace_id={}&project_id=0".format(settings.DOMAIN, woid, home_project.workspace_id)
+                url = "http://{}/termite2/webapp_page/?workspace_id=home_page&webapp_owner_id={}&workspace_id={}&project_id=0".format(settings.DOMAIN, woid, home_project.workspace_id)
                 print "home_page_url++++>>>>>", url
                 request = urllib2.Request(url)
                 request.get_method = lambda: 'PURGE'
                 x = urllib2.urlopen(request)
-            url = "http://{}/termite2/webapp_page/?ngx_rd=1&workspace_id=home_page&project_id={}&webapp_owner_id={}".format(settings.DOMAIN, project.id, woid)
+            url = "http://{}/termite2/webapp_page/?workspace_id=home_page&project_id={}&webapp_owner_id={}".format(settings.DOMAIN, project.id, woid)
             print "project++++>>>>>", url
             request = urllib2.Request(url)
             request.get_method = lambda: 'PURGE'
             x = urllib2.urlopen(request)
 
-            url = "http://{}/termite2/webapp_page/?ngx_rd=1&workspace_id=home_page&pwebapp_owner_id={}&roject_id={}".format(settings.DOMAIN,  woid, project.id)
+            url = "http://{}/termite2/webapp_page/?workspace_id=home_page&pwebapp_owner_id={}&roject_id={}".format(settings.DOMAIN,  woid, project.id)
             print "project++++>>>>>", url
             request = urllib2.Request(url)
             request.get_method = lambda: 'PURGE'
