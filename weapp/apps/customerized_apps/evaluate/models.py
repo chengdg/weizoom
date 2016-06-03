949c2013a398d4aa52a04682958825c019d6b406
aix <aixiang@weizoom.com>
2016-06-03 17:30:56 +0800

    迁移model

--- weapp/apps/customerized_apps/evaluate/models.py
+++ weapp/apps/customerized_apps/evaluate/models.py
@@ -4,59 +4,43 @@ from datetime import datetime
 
 import mongoengine as models
 
-class EvaluateParticipance(models.Document):
-	webapp_user_id= models.LongField(default=0) #参与者id
-	member_id= models.LongField(default=0) #参与者id
-	belong_to = models.StringField(default="", max_length=100) #对应的活动id
-	tel = models.StringField(default="", max_length=100)
-	termite_data = models.DynamicField(default="") #termite数据
-	prize = models.DynamicField(default="") #活动奖励
-	created_at = models.DateTimeField() #创建时间
+SCORE = [0, 1, 2, 3, 4, 5]
+STATUS2NAME = [u'待审核', u'已通过', u'通过并置顶', u'已屏蔽']
+STATUS_DENIED = -1
+STATUS_WAITTING = 0
+STATUS_PASSED = 1
+STATUS_TOP = 2
+
+class Evaluates(models.Document):
+	"""
+	所有用户的商品评价
+	"""
+	owner_id = models.LongField()
+	member_id= models.LongField() #评价用户
+	order_id = models.StringField(default='', max_length=50)  # Order.order_id ！！！
+	product_id = models.IntField() #商品id
+	score = models.IntField(default=SCORE[5])  # 商品评分
+	detail = models.DynamicField()  # 评价详情
+	pics = models.ListField() #上传的图片
+	created_at = models.DateTimeField()  # 评价时间
+	top_time = models.DateTimeField() # 置顶时间
+	status = models.IntField(default=STATUS_WAITTING)  # 审核状态
 
 	meta = {
-		'collection': 'evaluate_evaluate_participance'
+		'collection': 'evaluate_evaluates'
 	}
 
 
-STATUS_NOT_START = 0
-STATUS_RUNNING = 1
-STATUS_STOPED = 2
-class Evaluate(models.Document):
+TEMPLATE_ORDINARY = 0
+TEMPLATE_CUSTOMIZED = 1
+class EvaluateTemplateSetting(models.Document):
+	"""
+	手机端评价页面模版
+	"""
 	owner_id = models.LongField() #创建人id
-	name = models.StringField(default="", max_length=100) #名称
-	start_time = models.DateTimeField() #开始时间
-	end_time = models.DateTimeField() #结束时间
-	status = models.IntField(default=0) #状态
-	participant_count = models.IntField(default=0) #参与者数量
-	related_page_id = models.StringField(default="", max_length=100) #termite page的id
-	created_at = models.DateTimeField() #创建时间
-	
+	template_type = models.IntField(default=TEMPLATE_ORDINARY)
+	related_page_id = models.StringField(default='', max_length=100) #组件page_id
+
 	meta = {
-		'collection': 'evaluate_evaluate'
+		'collection': 'evaluate_evaluate_template_setting'
 	}
-	
-	@property
-	def status_text(self):
-		if self.status == STATUS_NOT_START:
-			return u'未开始'
-		elif self.status == STATUS_RUNNING:
-			now = datetime.today()
-			if now >= self.end_time:
-				return u'已结束'
-			else:
-				return u'进行中'
-		elif self.status == STATUS_STOPED:
-			return u'已结束'
-		else:
-			return u'未知'
-	
-	@property
-	def is_finished(self):
-		status_text = self.status_text
-		if status_text == u'已结束':
-			return True
-		else:
-			return False
-
-
-	
