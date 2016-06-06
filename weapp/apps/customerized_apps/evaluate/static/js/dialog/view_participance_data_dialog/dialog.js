648bc27d0ffe924ae09cc6b71fcfb0b300655f25
刘家瑞 <jary328@qq.com>
2016-06-06 15:51:31 +0800

    商品评价弹框样式

--- weapp/apps/customerized_apps/evaluate/static/js/dialog/view_participance_data_dialog/dialog.js
+++ weapp/apps/customerized_apps/evaluate/static/js/dialog/view_participance_data_dialog/dialog.js
@@ -13,8 +13,17 @@ W.dialog.app.evaluate.ViewParticipanceDataDialog = W.dialog.Dialog.extend({
 	templates: {
 		dialogTmpl: '#app-evaluate-viewParticipanceDataDialog-dialog-tmpl'
 	},
-	
+
 	onInitialize: function(options) {
+		//创建富文本编辑器
+        var editor = new W.view.common.RichTextEditor({
+            el: 'textarea',
+            type: 'text',
+            width:837,
+            height:100,
+            pasteplain: true
+        });
+		editor.render();
 	},
 	
 	beforeShow: function(options) {
@@ -24,7 +33,7 @@ W.dialog.app.evaluate.ViewParticipanceDataDialog = W.dialog.Dialog.extend({
 		this.activityId = options.activityId;
 	},
 	
-	afterShow: function(options) {
+	afterShow: function(options) {	
 		if (this.activityId) {
 			W.getApi().call({
 				app: 'apps/evaluate',
@@ -39,7 +48,7 @@ W.dialog.app.evaluate.ViewParticipanceDataDialog = W.dialog.Dialog.extend({
 				error: function(resp) {
 				}
 			})
-		}
+		}	
 	},
 	
 	/**
