9a6ce6eca24e1b8cf4b8e23ade7182c4c839fc2c
aix <aixiang@weizoom.com>
2016-06-13 11:31:56 +0800

    update template and style

--- weapp/apps/customerized_apps/evaluate/m_evaluate.py
+++ weapp/apps/customerized_apps/evaluate/m_evaluate.py
@@ -17,6 +17,7 @@ import export
 from apps import request_util
 from mall.models import Order, OrderHasProduct
 from termite2 import pagecreater
+from cache import webapp_cache
 import weixin.user.models as weixin_models
 
 class MEvaluate(resource.Resource):
@@ -52,10 +53,10 @@ class MEvaluate(resource.Resource):
 			'resource': "m_evaluate",
 			'is_hide_weixin_option_menu': True,
 			'template_type': template_type,
-			'order_has_product_id': param.get('order_has_product_id', ''),
-			'order_id': param.get('order_id', ''),
-			'product_id': param.get('product_id', ''),
-			'product_model_name': param.get('product_model_name', '')
+			'order_has_product_id': param.get('order_has_product_id', '1'),
+			'order_id': param.get('order_id', '1'),
+			'product_id': param.get('product_id', '1'),
+			'product_model_name': param.get('product_model_name', 'test')
 		})
 
 		return render_to_response('evaluate/templates/webapp/m_evaluate.html', c)
@@ -63,16 +64,63 @@ class MEvaluate(resource.Resource):
 	def api_get(request):
 		"""
 		获取商品以及评价数据
-		@return:
 		"""
+		response = create_response(500)
+		member = request.member
+		if not member:
+			response.errMsg = u'会员信息出错'
+			return response.get_response()
 		param = request.GET
-		order_has_product_id = param.get('order_has_product_id', ''),
-		order_id = param.get('order_id', ''),
-		product_id = param.get('product_id', ''),
-		product_model_name = param.get('product_model_name', '')
+		order_id = param.get('order_id', None)
+		product_model_name = param.get('product_model_name', None)
+		order_has_product_id = int(param.get('order_has_product_id', None))
+		product_id = param.get('product_id', None)
 
-		order = Order.objects.get(order_id=order_id)
-		order_has_products = OrderHasProduct.objects.filter(order=order, origin_order_id=0)
+		order_review_count = app_models.OrderEvaluates.objects(
+			owner_id=member.id,
+			order_id=order_id,
+		).count()
+		has_order_review = order_review_count > 0
+
+		# 得到商品信息, 如果商品已不存在（下架..）, 返回错误
+		try:
+			product = webapp_cache.get_webapp_product_detail(request.webapp_owner_id,product_id)
+			product = product
+			product.fill_specific_model(product_model_name, product.models)
+		except:
+			response.errMsg = u'商品信息出错'
+			return response.get_response()
+
+		created = True
+		#检查是否已评价过
+		product_review_dict = {}
+		try:
+			product_review = app_models.ProductEvaluates.objects.get(order_has_product_id=order_has_product_id)
+			product_review_dict = {
+				'id': product_review.id,
+				'product_score': product_review.product_score,
+				'review_detail': product_review.review_detail,
+				'has_pic': len(product_review.pics) > 0
+			}
+			created = False
+		except:
+			pass
+
+		response = create_response(200)
+		response.data = {
+			'product': {
+				'thumbnails_url': product.thumbnails_url,
+				'id': product.id,
+				'name': product.name,
+				'props': ' / '.join([model.property_value for model in product.custom_model_properties]) if product.custom_model_properties else ''
+			},
+			'product_review': product_review_dict,
+			'order_has_product_id': order_has_product_id,
+			'has_order_review': has_order_review,
+			'order_id': order_id,
+			'created': created
+		}
+		return response.get_response()
 
 def render_deleted_page(request):
 	c = RequestContext(request,{
