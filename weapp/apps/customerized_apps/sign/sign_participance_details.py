d3f50aa68264acd28668af4c374f0cdda00955da
cl <chenlei@weizoom.com>
2016-06-20 11:03:03 +0800

    签到详情的图标

--- weapp/apps/customerized_apps/sign/sign_participance_details.py
+++ weapp/apps/customerized_apps/sign/sign_participance_details.py
@@ -15,9 +15,12 @@ import models as app_models
 import export
 from modules.member.models import Member
 from utils.string_util import hex_to_byte, byte_to_hex
+from core import dateutil
+
+from mall.promotion import models as promotion_models
 
 FIRST_NAV = 'apps'
-COUNT_PER_PAGE = 30
+COUNT_PER_PAGE = 15
 
 class SignParticipances(resource.Resource):
 	app = 'apps/sign'
@@ -31,30 +34,55 @@ class SignParticipances(resource.Resource):
 		member_id = request.GET.get('member_id', None)
 		belong_to = request.GET.get('belong_to', None)
 		if member_id and belong_to:
-			items = app_models.SignDetails.objects(belong_to=belong_to, member_id=int(member_id)).order_by('-created_at')
+			#最近90天
+			total_days, low_date, cur_date, high_date = dateutil.get_date_range(dateutil.get_today(), str(89), 0)
+			date_list = dateutil.get_date_range_list(low_date,cur_date)
 
 			#进行分页
 			count_per_page = int(request.GET.get('count_per_page', COUNT_PER_PAGE))
 			cur_page = int(request.GET.get('page', '1'))
-			pageinfo, items = paginator.paginate(items, cur_page, count_per_page, query_string=request.META['QUERY_STRING'])
+			date_list.reverse()
+			pageinfo, date_list = paginator.paginate(date_list, cur_page, count_per_page, query_string=request.META['QUERY_STRING'])
+
+			start_date = low_date.strftime("%Y-%m-%d %H:%M:%S")
+			#现在的时间
+			end_date = cur_date.strftime("%Y-%m-%d 23:59:59")
+
+			items = app_models.SignDetails.objects(belong_to=belong_to, member_id=int(member_id),created_at__gte=start_date,created_at__lte=end_date).order_by('-created_at')
+
 			sign_member_ids = [item.member_id for item in items]
 			member_id2info = {m.id: {'is_subscribed': m.is_subscribed, 'member_name': m.username_for_html} for m in Member.objects.filter(id__in=sign_member_ids)}
 			returnDataList = []
+			time2prize = {}
+			coupon_ids = []
 			for t in items:
-				prize_str = u''
-				if t['prize'].get('integral', None):
-					prize_str += u'积分+%s' % str(t['prize']['integral'])
 				if t['prize'].get('coupon', None):
 					if t['prize']['coupon']['id']:
-						prize_str += u'%s' % str(t['prize']['coupon']['name'])
-				returnDataDict = {
-					"member_id": t.member_id,
-					"member_name": member_id2info[t.member_id]['member_name'],
-					"created_at": t.created_at.strftime("%Y.%m.%d %H:%M:%S"),
-					"type": t.type,
-					"prize": prize_str
+						coupon_ids.append(t['prize']['coupon']['id'])
+			coupon_id2name = {c.id: c.name for c in promotion_models.CouponRule.objects.filter(id__in=coupon_ids)}
+
+			for item in items:
+				prize_str = u''
+				if item['prize'].get('integral', None):
+					prize_str += u'积分+%s' % str(t['prize']['integral'])
+				if item['prize'].get('coupon', None):
+					if item['prize']['coupon']['id']:
+						prize_str += u'%s' % coupon_id2name.get(item['prize']['coupon']['id'],'')
+				time2prize[item.created_at.strftime("%Y.%m.%d")] = {
+					"created_at": item.created_at.strftime("%Y.%m.%d %H:%M:%S"),
+					"prize": prize_str,
+					"status": 1
 				}
-				returnDataList.append(returnDataDict)
+			for date_d in date_list:
+				date_ymd = date_d.strftime("%Y.%m.%d")
+				if time2prize.get(date_ymd,None):
+					returnDataList.append(time2prize[date_ymd])
+				else:
+					returnDataList.append({
+						"created_at": date_d.strftime("%Y.%m.%d %H:%M:%S"),
+						"prize": "",
+						"status": 0
+					})
 		else:
 			returnDataList = []
 		response = create_response(200)
