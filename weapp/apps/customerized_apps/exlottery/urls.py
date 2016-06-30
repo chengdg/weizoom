575efe9514cac8d3dfeaa4596d2a60eaa665495d
刘家瑞 <jary328@qq.com>
2016-06-30 15:39:45 +0800

    活动主页

--- weapp/apps/customerized_apps/exlottery/urls.py
+++ weapp/apps/customerized_apps/exlottery/urls.py
@@ -4,6 +4,7 @@ import exlottery
 import exlottery_participance
 import exlottery_participances
 import m_exlottery
+import m_exlottery_page
 import exlottery_status
 import exlotteries
 import exlottery_code_store
