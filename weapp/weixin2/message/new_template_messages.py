9e5b8b1333b5fae0853b05dc97643db272d1bc87
aix <aixiang@weizoom.com>
2016-07-21 12:01:22 +0800

    update html

--- weapp/weixin2/message/new_template_messages.py
+++ weapp/weixin2/message/new_template_messages.py
@@ -64,10 +64,11 @@ class NewTemplateMessages(resource.Resource):
 
             setting = id2template.get(template_id, None)
             if setting:
+                print '----'
                 item['status'] = setting.status
                 item['first'] = setting.first
                 item['remark'] = setting.remark
-
+            print item
             items.append(item)
         response = create_response(200)
         response.data = items
