0361c84a43ee3decf59bb00cc66cba9651b4695f
朱天琦 <zhutianqi@weizoom.com>
2016-07-27 16:00:50 +0800

    修复DummyUserProfile

--- weapp/wapi/mall/product/dummy_utils.py
+++ weapp/wapi/mall/product/dummy_utils.py
@@ -1,5 +1,5 @@
 #coding: utf8
-
+from account.models import UserProfile
 
 class DummyUserProfile:
 	"""
@@ -8,6 +8,12 @@ class DummyUserProfile:
 	def __init__(self, webapp_id=0, user_id=0):
 		self.webapp_id = webapp_id
 		self.user_id = user_id
+		up = UserProfile.objects.filter(user_id=user_id).first()
+		# todo 优化,外部传值
+		if up:
+			self.webapp_type = up.webapp_type
+		else:
+			self.webapp_type = 0
 
 
 class DummyRequest(object):
