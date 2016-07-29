a2c8dde88036a07f99cafbd1816feb512b2f657e
陈蕾 <chenlei@weizoom.com>
2016-07-29 16:58:45 +0800

    订单概况

--- weapp/wapi/qrcode/order_outline.py
+++ weapp/wapi/qrcode/order_outline.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import json
 
+from django.db.models import Q
+
 from core import api_resource, paginator
 from market_tools.tools.channel_qrcode.models import ChannelQrcodeSettings, ChannelQrcodeHasMember
 from wapi.decorators import param_required
@@ -23,6 +25,7 @@ class QrcodeOrderOutline(api_resource.ApiResource):
 		start = time.time()
 		channel_qrcode_id = int(args.get('channel_qrcode_id', 0))
 		channel_qrcode_ids = json.loads(args.get('channel_qrcode_ids', "[]"))
+		channel_qrcode_id2user_created_at = json.loads(args.get('channel_qrcode_id2user_created_at', "[]"))
 		if channel_qrcode_ids:
 			q_filter_data_args = {
 				"channel_qrcode_id__in": channel_qrcode_ids
@@ -32,11 +35,38 @@ class QrcodeOrderOutline(api_resource.ApiResource):
 				"channel_qrcode_id": channel_qrcode_id
 			}
 
+		total_channel_members = ChannelQrcodeHasMember.objects.filter(**q_filter_data_args).order_by(
+			'-created_at')
+		channel_qrcode_id2member_id = {}
+		curr_qrcode_id2member_id = {}
+		member_ids = []
+		q_has_member_ids = []
+		for member_log in total_channel_members:
+			q_has_member_ids.append(member_log.member_id)
+			member_ids.append(member_log.member_id)
+			if not channel_qrcode_id2member_id.has_key(member_log.channel_qrcode_id):
+				channel_qrcode_id2member_id[member_log.channel_qrcode_id] = [member_log.member_id]
+			else:
+				channel_qrcode_id2member_id[member_log.channel_qrcode_id].append(member_log.member_id)
+			if not curr_qrcode_id2member_id.has_key(member_log.channel_qrcode_id):
+				curr_qrcode_id2member_id[member_log.channel_qrcode_id] = [member_log.member_id]
+			else:
+				curr_qrcode_id2member_id[member_log.channel_qrcode_id].append(member_log.member_id)
 
-		total_channel_members = ChannelQrcodeHasMember.objects.filter(**q_filter_data_args).order_by('-created_at')
-		total_member_ids = [tcm.member_id for tcm in total_channel_members]
-		webappusers = WebAppUser.objects.filter(member_id__in=total_member_ids)
-		webapp_user_id2member_id = {webappuser.id: webappuser.member_id for webappuser in webappusers}
+		q_settings = ChannelQrcodeSettings.objects.filter(Q(id__in=channel_qrcode_ids) | Q(bing_member_id__in=member_ids))
+		member_id2created_at = {}
+		channel_qrcode_id2bing_member_id = {}
+		for qs in q_settings:
+			if qs.is_bing_member:
+				member_ids.append(qs.bing_member_id)
+				if not channel_qrcode_id2member_id.has_key(qs.id):
+					channel_qrcode_id2member_id[qs.id] = [qs.bing_member_id]
+				else:
+					channel_qrcode_id2member_id[qs.id].append(qs.bing_member_id)
+				member_id2created_at[qs.bing_member_id] = qs.created_at.strftime('%Y-%m-%d %H:%M:%S')
+				channel_qrcode_id2bing_member_id[qs.id] = qs.bing_member_id
+
+		webapp_user_id2member_id = {webappuser.id: webappuser.member_id for webappuser in WebAppUser.objects.filter(member_id__in=member_ids)}
 
 		start_date = args.get('start_date', None)
 		end_date = args.get('end_date', None)
@@ -57,12 +87,40 @@ class QrcodeOrderOutline(api_resource.ApiResource):
 		sale_price = 0
 		final_price = 0
 		for order in orders:
-			if order.status not in [ORDER_STATUS_CANCEL, ORDER_STATUS_GROUP_REFUNDING, ORDER_STATUS_GROUP_REFUNDED, ORDER_STATUS_REFUNDING, ORDER_STATUS_REFUNDED]:
-				sale_price += order.final_price + order.coupon_money + order.integral_money + order.weizoom_card_money + order.promotion_saved_money + order.edit_money
-				final_price += order.final_price
-				increase_orders.append(order)
-			if order.is_first_order and order.status != ORDER_STATUS_NOT:
-				first_orders.append(order)
+			webapp_user_id = order.webapp_user_id
+			member_id = webapp_user_id2member_id.get(webapp_user_id)
+			created_at = member_id2created_at.get(member_id)
+			for channel_qrcode_id, member_ids in channel_qrcode_id2member_id.items():
+				if member_id in member_ids:
+					flag = False
+					if created_at:
+						if member_id in q_has_member_ids:
+							if channel_qrcode_id2user_created_at.get(str(channel_qrcode_id)):
+								if channel_qrcode_id2user_created_at.get(
+										str(channel_qrcode_id)) <= order.created_at.strftime('%Y-%m-%d %H:%M:%S'):
+									if channel_qrcode_id2bing_member_id.get(channel_qrcode_id):
+										if curr_qrcode_id2member_id.get(
+												channel_qrcode_id) and member_id in curr_qrcode_id2member_id.get(
+											channel_qrcode_id):
+											if order.created_at.strftime('%Y-%m-%d %H:%M:%S') <= created_at:
+												flag = True
+										else:
+											if order.created_at.strftime('%Y-%m-%d %H:%M:%S') > created_at:
+												flag = True
+						else:
+							if channel_qrcode_id2user_created_at.get(
+									str(channel_qrcode_id)) <= order.created_at.strftime(
+								'%Y-%m-%d %H:%M:%S'):
+								flag = True
+					else:
+						flag = True
+					if flag:
+						if order.status not in [ORDER_STATUS_CANCEL, ORDER_STATUS_GROUP_REFUNDING, ORDER_STATUS_GROUP_REFUNDED, ORDER_STATUS_REFUNDING, ORDER_STATUS_REFUNDED]:
+							sale_price += order.final_price + order.coupon_money + order.integral_money + order.weizoom_card_money + order.promotion_saved_money + order.edit_money
+							final_price += order.final_price
+							increase_orders.append(order)
+						if order.is_first_order and order.status != ORDER_STATUS_NOT:
+							first_orders.append(order)
 
 		order_outline_info = {
 			"increase_order_count": len(increase_orders),
