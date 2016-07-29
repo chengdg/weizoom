c1a0d5c1cbbec6ffa92c796a93209358fbdde8b4
朱天琦 <zhutianqi@weizoom.com>
2016-07-29 10:25:16 +0800

    使用eaglet的watchdog

--- weapp/watchdog/utils.py
+++ weapp/watchdog/utils.py
@@ -19,6 +19,8 @@ import weapp.settings as weapp_settings
 from .models import WATCHDOG_ALERT, WATCHDOG_DEBUG,WATCHDOG_EMERGENCY,WATCHDOG_ERROR,WATCHDOG_FATAL,WATCHDOG_INFO,WATCHDOG_NOTICE,WATCHDOG_WARNING
 import logging
 
+from eaglet.core import watchdog
+
 __author__ = 'victor'
 
 def message_json(user_id, message, type):
@@ -35,6 +37,7 @@ def watchdog_debug(message, type='WEB', user_id='0', db_name='default'):
 
 	异步方式调用 weapp.services.send_watchdog_service.send_watchdog()
 	"""
+	watchdog.debug(message=message, log_type=type, user_id=user_id, server_name='weapp')
 	result = None
 	if weapp_settings.TASKQUEUE_ENABLED:
 		if WATCHDOG_DEBUG >= settings.WATCH_DOG_LEVEL:
@@ -43,7 +46,7 @@ def watchdog_debug(message, type='WEB', user_id='0', db_name='default'):
 		if WATCHDOG_DEBUG >= settings.WATCH_DOG_LEVEL:
 			_watchdog(type, message, WATCHDOG_DEBUG, user_id, db_name)
 
-	logging.DEBUG(message_json(user_id,message, type))
+	# logging.DEBUG(message_json(user_id,message, type))
 	return result
 
 
@@ -53,6 +56,7 @@ def watchdog_info(message, type='WEB', user_id='0', db_name='default'):
 
 	异步方式调用 weapp.services.send_watchdog_service.send_watchdog()
 	"""
+	watchdog.info(message=message, log_type=type, user_id=user_id, server_name='weapp')
 	result = None
 	#print("weapp_settings.TASKQUEUE_ENABLED: {}".format(weapp_settings.TASKQUEUE_ENABLED))
 	if weapp_settings.TASKQUEUE_ENABLED:
@@ -61,7 +65,7 @@ def watchdog_info(message, type='WEB', user_id='0', db_name='default'):
 	else:
 		if WATCHDOG_INFO >= settings.WATCH_DOG_LEVEL:
 			_watchdog(type, message, WATCHDOG_INFO, user_id, db_name)
-	logging.info(message_json(user_id, message, type))
+	# logging.info(message_json(user_id, message, type))
 	return result
 
 
@@ -71,6 +75,7 @@ def watchdog_notice(message, type='WEB', user_id='0', db_name='default'):
 
 	异步方式调用 weapp.services.send_watchdog_service.send_watchdog()
 	"""
+	watchdog.warning(message=message, log_type=type, user_id=user_id, server_name='weapp')
 	result = None
 	if weapp_settings.TASKQUEUE_ENABLED:
 		if WATCHDOG_NOTICE>= settings.WATCH_DOG_LEVEL:
@@ -78,7 +83,7 @@ def watchdog_notice(message, type='WEB', user_id='0', db_name='default'):
 	else:
 		if WATCHDOG_NOTICE >= settings.WATCH_DOG_LEVEL:
 			_watchdog(type, message, WATCHDOG_NOTICE, user_id, db_name)
-	logging.info(message_json(user_id, message, type))
+	# logging.info(message_json(user_id, message, type))
 	return result
 
 
@@ -88,6 +93,7 @@ def watchdog_warning(message, type='WEB', user_id='0', db_name='default'):
 
 	异步方式调用 weapp.services.send_watchdog_service.send_watchdog()
 	"""
+	watchdog.warning(message=message, log_type=type, user_id=user_id, server_name='weapp')
 	result = None
 	if weapp_settings.TASKQUEUE_ENABLED:
 		if WATCHDOG_WARNING>= settings.WATCH_DOG_LEVEL:
@@ -95,13 +101,14 @@ def watchdog_warning(message, type='WEB', user_id='0', db_name='default'):
 	else:
 		if WATCHDOG_NOTICE >= settings.WATCH_DOG_LEVEL:
 			_watchdog(type, message, WATCHDOG_NOTICE, user_id, db_name)
-	logging.warning(message_json(user_id, message, type))
+	# logging.warning(message_json(user_id, message, type))
 	return result
 
 
 def watchdog_error(message, type='WEB', user_id='0', noraise=False, db_name='default'):
 	result = None
-	logging.error(message_json(user_id, message, type))
+	# logging.error(message_json(user_id, message, type))
+	watchdog.error(message=message, log_type=type, user_id=user_id, server_name='weapp')
 	if weapp_settings.TASKQUEUE_ENABLED:
 		if WATCHDOG_ERROR >= settings.WATCH_DOG_LEVEL:
 			result = send_watchdog.delay(type, message, WATCHDOG_ERROR, user_id, db_name)
@@ -122,7 +129,8 @@ def watchdog_error(message, type='WEB', user_id='0', noraise=False, db_name='def
 
 def watchdog_fatal(message, type='WEB', user_id='0', noraise=False, db_name='default'):
 	result = None
-	logging.error(message_json(user_id, message, type))
+	# logging.error(message_json(user_id, message, type))
+	watchdog.error(message=message, log_type=type, user_id=user_id, server_name='weapp')
 	if weapp_settings.TASKQUEUE_ENABLED:
 		if WATCHDOG_FATAL >= settings.WATCH_DOG_LEVEL:
 			send_watchdog.delay(type, message, WATCHDOG_FATAL, user_id, db_name)
@@ -141,7 +149,8 @@ def watchdog_fatal(message, type='WEB', user_id='0', noraise=False, db_name='def
 
 
 def watchdog_alert(message, type='WEB', user_id='0', db_name='default'):
-	logging.critical(message_json(user_id, message, type))
+	# logging.critical(message_json(user_id, message, type))
+	watchdog.alert(message=message, log_type=type, user_id=user_id, server_name='weapp')
 	result = None
 	if weapp_settings.TASKQUEUE_ENABLED:
 		if WATCHDOG_ALERT >= settings.WATCH_DOG_LEVEL:
@@ -153,7 +162,8 @@ def watchdog_alert(message, type='WEB', user_id='0', db_name='default'):
 
 
 def watchdog_js_analysis(message, type='JS_Analysis', user_id='0', db_name='default'):
-	logging.critical(message_json(user_id, message, type))
+	# logging.critical(message_json(user_id, message, type))
+	watchdog.info(message=message, log_type=type, user_id=user_id, server_name='weapp')
 	result = None
 	if weapp_settings.TASKQUEUE_ENABLED:
 		if WATCHDOG_ALERT >= settings.WATCH_DOG_LEVEL:
@@ -165,7 +175,8 @@ def watchdog_js_analysis(message, type='JS_Analysis', user_id='0', db_name='defa
 
 
 def watchdog_emergency(message, type='WEB', user_id='0', db_name='default'):
-	logging.critical(message_json(user_id, message, type))
+	# logging.critical(message_json(user_id, message, type))
+	watchdog.critical(message=message, log_type=type, user_id=user_id, server_name='weapp')
 	result = None
 	if weapp_settings.TASKQUEUE_ENABLED:
 		if WATCHDOG_EMERGENCY >= settings.WATCH_DOG_LEVEL:
