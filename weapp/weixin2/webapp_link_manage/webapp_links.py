793919c9fc98fd5a1a37eba413fed82bdfa6527c
aix <aixiang@weizoom.com>
2016-08-04 18:40:21 +0800

    update link

--- weapp/weixin2/webapp_link_manage/webapp_links.py
+++ weapp/weixin2/webapp_link_manage/webapp_links.py
@@ -79,17 +79,18 @@ class WebappItemLinks(resource.Resource):
 		selected_id, is_selected_type = webapp_link_utils.get_selected_by_link_target(request, menu_type, link_type, selected_link_target)
 		request.selected_id = selected_id
 
+		#首先请求marketapp
 		URL = "http://%s/apps/export/api/get_app_items/?webapp_owner_id=%s&link_type=%s&query=%s" % (settings.MARKETAPP_DOMAIN, str(request.manager.id), link_type, query)
-
-
 		try:
 			api_resp_text = requests.get(URL).text
 			print URL, 'marketapp get_app_items===============>>>', api_resp_text
 			api_resp = json.loads(api_resp_text)
 		except:
-			api_resp = {}
-			notify_message = u"api request marketapp get_app_link failed:::: ".format(unicode_full_stack())
+			notify_message = u"从marketapp获取活动列表失败，cause: \n{}".format(unicode_full_stack())
 			watchdog_error(notify_message)
+			response = create_response(500)
+			response.errMsg = u'网络问题，获取活动列表失败'
+			return response.get_response()
 
 		if api_resp.get('data', None):
 			api_data = api_resp['data']
@@ -103,7 +104,15 @@ class WebappItemLinks(resource.Resource):
 				'data': {},
 				'type': link_type
 			}
-		elif api_resp.get('errMsg', None) and 'invalid' == api_resp['errMsg']:
+		elif api_resp.get('errMsg', None) and 'invalid appname' == api_resp['errMsg']:
+			temp_data = getLocalLinkData(request, link_type)
+			if temp_data:
+				response = create_response(200)
+				response.data = temp_data
+			else:
+				response = create_response(500)
+				response.errMsg = u'活动不存在'
+		else:
 			objects, menu_item = webapp_link_utils.get_webapp_link_objectes_for_type(request, link_type, query, order_by)
 
 			if link_type == "shengjing_app":
@@ -129,7 +138,8 @@ class WebappItemLinks(resource.Resource):
 				for item in objects:
 					data = dict()
 					if link_type == 'red_envelope':
-						items.append(item)
+						# items.append(item)
+						pass
 					else:
 						data['id'] = item.id
 						data['created_at'] = item.created_at if isinstance(item.created_at, str) else item.created_at.strftime('%Y-%m-%d %H:%M:%S')
@@ -140,10 +150,10 @@ class WebappItemLinks(resource.Resource):
 							# 微页面
 							data['name'] = item.site_title
 
-						if link_type == 'lottery':
-							# 抽奖
-							data['type'] = WebappItemLinks.LOTTER_TYPE[item.type]
-							data['valid'] = u'{} 至 {}'.format(item.start_at.strftime("%Y-%m-%d"), item.end_at.strftime("%Y-%m-%d"))
+						# if link_type == 'lottery':
+						# 	# 抽奖
+						# 	data['type'] = WebappItemLinks.LOTTER_TYPE[item.type]
+						# 	data['valid'] = u'{} 至 {}'.format(item.start_at.strftime("%Y-%m-%d"), item.end_at.strftime("%Y-%m-%d"))
 
 						if link_type == 'coupon':
 							# 优惠券
@@ -154,9 +164,9 @@ class WebappItemLinks(resource.Resource):
 							data['valid'] = u'{} 至 {}'.format(data['created_at'], data['end_date'])
 							data['link'] = menu_item['link_template'].format(item.detail['id'])
 
-						if link_type == 'activity':
-							# 活动
-							data['valid'] = u'{} 至 {}'.format(item.start_date, item.end_date)
+						# if link_type == 'activity':
+						# 	# 活动
+						# 	data['valid'] = u'{} 至 {}'.format(item.start_date, item.end_date)
 						items.append(data)
 
 				response = create_response(200)
@@ -167,7 +177,23 @@ class WebappItemLinks(resource.Resource):
 					'data': {},
 					'type': link_type
 				}
-		else:
-			response = create_response(500)
-			response.errMsg = 'get %s datas failed' % link_type
 		return response.get_response()
+
+def getLocalLinkData(request, link_type):
+	apps_dir = os.path.join(settings.PROJECT_HOME, '../apps/customerized_apps')
+	if os.path.isdir(os.path.join(apps_dir, link_type)):
+		#如果是app
+		app_name = link_type
+		export_module_path = 'apps.customerized_apps.%s.export' % app_name
+		export_module = __import__(export_module_path, {}, {}, ['*',])
+		func = getattr(export_module, 'get_link_targets', None)
+		if func:
+			pageinfo, link_targets = func(request)
+			return {
+				'items': link_targets,
+				'pageinfo': paginator.to_dict(pageinfo),
+				'sortAttr': '-id',
+				'data': {},
+				'type': app_name
+			}
+	return None
