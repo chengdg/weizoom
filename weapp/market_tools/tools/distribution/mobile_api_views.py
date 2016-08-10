e94610bbe2fecd1d0f7a0f7cd25ca3d8d4e313c6
shaojiaqi <shaojiaqi@weizoom.com>
2016-08-10 11:36:26 +0800

    1、为移动端5个页面添加py支持；

--- weapp/market_tools/tools/distribution/mobile_api_views.py
+++ weapp/market_tools/tools/distribution/mobile_api_views.py
@@ -4,3 +4,15 @@ __author__ = 'aix'
 
 import json
 from core.jsonresponse import create_response
+from market_tools.tools.distribution import models
+
+def change_state(request):
+	"""
+	修改订单状态
+	"""
+	member_id = request.POST.get('member_id','')
+	models.ChannelDistributionQrcodeSettings.objects.get(bing_member_id=member_id).update (
+		state = 1
+	)
+
+	return response.get_response()
--- weapp/market_tools/tools/distribution/mobile_views.py
+++ weapp/market_tools/tools/distribution/mobile_views.py
@@ -7,6 +7,7 @@ import os
 from django.template import Context, RequestContext
 from django.shortcuts import render_to_response
 from market_tools.tools.distribution import models
+from static.modules.member import models as member_models
 template_path_items = os.path.dirname(__file__).split(os.sep)
 TEMPLATE_DIR = '%s/templates' % template_path_items[-1]
 
@@ -25,6 +26,7 @@ def get_page(request):
 	diff_reward = commission_return_standard - will_return_reward  #还差多少元可以取现
 	return_standard = propotion_data.return_standard  #多少天的计算方式
 	user_icon = request.member.user_icon
+	valid = will_return_reward >= commission_return_standard
 
 	c = RequestContext(request, {
 		'propotion_data': propotion_data,
@@ -36,6 +38,8 @@ def get_page(request):
 		'return_standard': return_standard,
 		'user_icon': user_icon,
 		'webapp_id': webapp_id,
+		'member_id': member_id,
+		'valid': valid,
 		'member_id': member_id
 	})
 
@@ -45,7 +49,7 @@ def get_process(request):
 	"""
 	获取提取进度页面
 	"""
-	member_id = request.member_id
+	member_id = request.member.id
 	cur_list = models.ChannelDistributionQrcodeSettings.objects.get(bing_member_id=member_id)
 	prev_datas = models.ChannelDistributionDetail.objects.get(member_id=member_id, order_id=0).order_by('-created_at')[0:10]
 	prev_lists = []
@@ -60,3 +64,60 @@ def get_process(request):
 		"prev_lists": prev_lists
 	})
 	return render_to_response('%s/distribution/webapp/m_process.html' % TEMPLATE_DIR, c)
+
+def get_vip_message(request):
+	"""
+	获取已有会员页面
+	"""
+	webapp_id = request.user_profile.webapp_id
+	member_id = request.member.id
+	vip_lists = models.ChannelDistributionQrcodeHasMember.objects.get(member_id=member_id)
+	for vip_list in vip_lists:
+		vip_list={
+			'nick_name': member_models.Member.objects.get(id=member_id).username_for_html
+			'cost_money': vip_list.cost_money,  #消费金额
+			'commission': vip_list.commission,  #带来的佣金
+			'buy_times': vip_list.buy_times,  #购买次数
+			'created_at': vip_list.created_at  #关注时间
+		}
+		vip_lists.append(vip_list)
+
+	c = RequestContext(request, {
+		'vip_lists': vip_lists
+	})
+	return render_to_response('%s/distribution/webapp/m_vip.html' % TEMPLATE_DIR, c)
+
+def get_details(request):
+	"""
+	获取交易明细页面
+	"""
+	member_id = request.member.id
+	will_return_reward = models.ChannelDistributionQrcodeSettings.objects.get(bing_member_id=member_id).will_return_reward  #已获得奖励
+	details_lists = models.ChannelDistributionDetail.objects.get(member_id=member_id)
+	for details_list in details_lists:
+		details_list={
+			'will_return_reward': will_return_reward,
+			'order_id': details_list.order_id  #订单id，id为0，则为提取
+			'money': details_list.money  #操作金额
+			'created_at': details_list.created_at  #添加时间
+		}
+		details_lists.append(details_list)
+
+	c = RequestContext(request, {
+		'details_lists': details_lists
+	})
+	return render_to_response('%s/distribution/webapp/m_details.html' % TEMPLATE_DIR, c)
+
+def get_weixin_code(request):
+	"""
+	获取二维码推广页面
+	"""
+	member_id = request.member.id
+	nick_name = request.member.username_for_html  #当前登入用户的昵称
+	weixin_code = models.ChannelDistributionQrcodeSettings.objects.get(bing_member_id=member_id).ticket  #二维码
+	c = RequestContext(request, {
+		'nick_name': nick_name,
+		'weixin_code': weixin_code
+	})
+	return render_to_response('%s/distribution/webapp/m_weixin_promotion.html' % TEMPLATE_DIR, c)
+
