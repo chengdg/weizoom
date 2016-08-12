ab203a742984a441bf1fd93975243cf54853c135
shaojiaqi <shaojiaqi@weizoom.com>
2016-08-12 17:44:37 +0800

    1、细化分销推广可能的情况； 2、为img标签添加data属性；

--- weapp/market_tools/tools/distribution/mobile_api_views.py
+++ weapp/market_tools/tools/distribution/mobile_api_views.py
@@ -3,16 +3,24 @@
 __author__ = 'aix'
 
 import json
+import datetime
 from core.jsonresponse import create_response
 from market_tools.tools.distribution import models
+from django.db.models import F
 
 def change_state(request):
 	"""
 	修改订单状态
 	"""
 	member_id = request.POST.get('member_id','')
-	models.ChannelDistributionQrcodeSettings.objects.get(bing_member_id=member_id).update (
-		state = 1
-	)
+	qrocde = models.ChannelDistributionQrcodeSettings.objects.filter(bing_member_id=member_id)
+	if qrcode[0].commission_return_standard  < qrcode[0].will_return_reward \
+		and  not qrcode[0].extraction_money :
+		qrcode.update (
+			state = 1,
+			commit_time = datetime.datetime.now(),
+			extraction_money = F('will_return_reward')
+		)
 
-	return response.get_response()
+		response = create_response(200)
+		return response.get_response()
