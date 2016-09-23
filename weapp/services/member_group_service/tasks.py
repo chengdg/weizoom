5c28d4a496fd939c81acb25654666134e8c75011
nanxuezhi <nanxuezhi@weizoom.com>
2016-09-23 10:19:07 +0800

    看购会员自动分组代码优化

--- weapp/services/member_group_service/tasks.py
+++ weapp/services/member_group_service/tasks.py
@@ -23,7 +23,7 @@ ORDER_STATUS_SUCCESSED = 5  # 已完成：自下单10日后自动置为已完成
 ORDER_STATUS_TYPE = [ORDER_STATUS_PAYED_NOT_SHIP,ORDER_STATUS_PAYED_SHIPED,ORDER_STATUS_SUCCESSED]
 
 # WESHOP_DING_GROUP_ID = '105507196'  #微众商城FT团队钉钉id
-WESHOP_DING_GROUP_ID = '80035247'  #发消息测试群
+# WESHOP_DING_GROUP_ID = '80035247'  #发消息测试群
 
 @task
 def deliver_member_group(request, args):
@@ -42,54 +42,66 @@ def deliver_member_group(request, args):
 			origin_order_id__lt=0
 		)
 	# print "orderList",len(orderList)
-	anonymous_member_list = []
-	anonymous_order_list = []
-	anonymous_order_money = 0
+	cancel_subscribed_members = []
+	cancel_subscribed_orders = 0
+	cancel_subscribed_price = 0
+	not_subscribed_members = []
+	not_subscribed_orders = 0
+	not_subscribed_price = 0
 	
 	for order in orderList:
 		# 获取会员信息
 		member = member_models.WebAppUser.get_member_by_webapp_user_id(order.webapp_user_id)
+		# print "member",member.id,member.webapp_id,order.order_id
 		# 判断是否存在这样的会员
 		if member:
 			# print "member",member.id,member.webapp_id,order.order_id
-			# 判断订单的weapp_id 与 会员的weapp_id 是否相同
-			if member.webapp_id == order.webapp_id:
-				# 判断是否关注
-				if member.is_subscribed == True:
-					# 获取分组是 看购会员分组 的id,返回list 
-					member_tag_id = member_models.MemberTag.objects.filter(webapp_id=member.webapp_id, name="看购会员分组")
-					# 遍历list
-					for member_tag in member_tag_id:
-						# 查询是否已经加入看购分组
-						kangou_records = member_models.MemberHasTag.objects.filter(member_id=member.id, member_tag_id=member_tag.id)
-						if kangou_records.count() == 0:
-							try:
-								# 添加看购分组记录
-								member_models.MemberHasTag.objects.create(member_id=member.id, member_tag_id=member_tag.id)
-							except Exception, e:
-								message = u'分组失败'
-								logging.info(message)
-								logging.info(e)
-				# 匿名用户
-				else:
-					# print "---",member.id
-					if member.id not in anonymous_member_list:
-						anonymous_member_list.append(member.id)
-					anonymous_order_list.append(order.final_price)
+			# 判断是否关注
+			if member.is_subscribed == True:
+				# 获取分组是 看购会员分组 的id,返回list 
+				member_tag_id = member_models.MemberTag.objects.filter(webapp_id=member.webapp_id, name="看购会员分组")
+				# 遍历list
+				for member_tag in member_tag_id:
+					# 查询是否已经加入看购分组
+					kangou_records = member_models.MemberHasTag.objects.filter(member_id=member.id, member_tag_id=member_tag.id)
+					if kangou_records.count() == 0:
+						try:
+							# 添加看购分组记录
+							member_models.MemberHasTag.objects.create(member_id=member.id, member_tag_id=member_tag.id)
+						except Exception, e:
+							message = u'加入看购分组失败，会员id: %d分组id: %d' % (member.id, member_tag.id)
+							logging.error(message)
+							logging.error(e)
+			# 匿名用户
 			else:
-				message = u"信息不匹配"
-				logging.info(message)
+				# 关注后取消了的用户
+				if member.status == 1:
+					# 统计未关注的会员数
+					if member.id not in cancel_subscribed_members:
+						cancel_subscribed_members.append(member.id)
+					# 统计订单总数
+					cancel_subscribed_orders += 1
+					# 统计订单总金额
+					cancel_subscribed_price += order.final_price
+				# 从未关注的用户
+				else:
+					# 统计未关注的会员数
+					if member.id not in not_subscribed_members:
+						not_subscribed_members.append(member.id)
+					# 统计订单总数
+					not_subscribed_orders += 1
+					# 统计订单总金额
+					not_subscribed_price += order.final_price
 		else:
-			message = u"会员信息获取失败"
+			message = u"会员信息获取失败,订单id: %d\n" % (order.order_id)
 			logging.info(message)
 
-	# 统计匿名购买者的总订单数与总金额
-	for order_list in anonymous_order_list:
-		anonymous_order_money += order_list
-
-	print "count(kangou order money):",len(anonymous_member_list),len(anonymous_order_list),anonymous_order_money
-	message = u'未关注的看购会员数: %d\n订单总数: %d\n订单总金额: %.2f' % (len(anonymous_member_list), len(anonymous_order_list), anonymous_order_money)
-	logging.info(message)
+	# print "count cancel_subscribed (kangou order money):",len(cancel_subscribed_members), cancel_subscribed_orders, cancel_subscribed_price
+	# print "count not_subscribed (kangou order money):",len(not_subscribed_members), not_subscribed_orders, not_subscribed_price
+	message_cancel = u'已取消关注的看购会员数: %d\n订单总数: %d\n订单总金额: %.2f' % (len(cancel_subscribed_members), cancel_subscribed_orders, cancel_subscribed_price)
+	message_not = u'从未关注的看购会员数: %d\n订单总数: %d\n订单总金额: %.2f' % (len(not_subscribed_members), not_subscribed_orders, not_subscribed_price)
+	logging.info(message_cancel)
+	logging.info(message_not)
 	# ding_util.send_to_ding(message, WESHOP_DING_GROUP_ID)
 
 	return 'OK {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
