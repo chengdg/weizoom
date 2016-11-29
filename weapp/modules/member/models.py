cea02dd226c3dee12928f3e2ed54691c119a3a41
duhao <duhao@weizoom.com>
2016-11-29 12:30:25 +0800

    更新db

--- weapp/modules/member/models.py
+++ weapp/modules/member/models.py
@@ -1342,25 +1342,21 @@ class MilekeLog(models.Model):
 		unique_together = (('mileke', 'member'),)
 
 
-MEMBER_YEAR_CARD = 1
-MEMBER_STU_CARD = 2
 class MemberCard(models.Model):
 	"""
 	会员卡
 	"""
-	owner_id = models.IntegerField() #owner_id
+	owner_id = models.IntegerField() #会员id
 	member_id = models.IntegerField() #会员id
-	#member_name = models.CharField(max_length=1024) #会员名称
+	batch_id = models.CharField(max_length=50, default="") #微众卡批次id alter table member_card  add column batch_id varchar(50) default '';
 	card_number = models.CharField(max_length=50) #微众卡卡号
 	card_password = models.CharField(max_length=512) #微众卡密码
-	type = models.IntegerField(default=MEMBER_YEAR_CARD) #卡类型 1:会员年卡 2:学生卡
-	card_name = models.CharField(max_length=512) #微众卡名次
+	card_name = models.CharField(max_length=512) #微众卡名字
+	is_active = models.BooleanField(default=True) #会员身份是否有效 以后扩展的备用字段
 	created_at = models.DateTimeField(auto_now_add=True) #发放时间
 
 	class Meta(object):
 		db_table = 'member_card'
-		verbose_name = 'member_card'
-		verbose_name_plural = 'member_card'
 
 class MemberCardLog(models.Model):
 	"""
@@ -1368,13 +1364,13 @@ class MemberCardLog(models.Model):
 	"""
 	member_card = models.ForeignKey(MemberCard)  #member card id
 	price = models.FloatField(default=0.0)  # 浮动金额
+	trade_id = models.CharField(max_length=50, default="") #支付流水号alter table member_card_log  add column trade_id varchar(50) default '';
+	order_id = models.CharField(max_length=200, default="") #订单号 alter table member_card_log  add column order_id varchar(200) default '';
 	reason = models.CharField(max_length=512)  # 原因
 	created_at = models.DateTimeField(auto_now_add=True) #时间
 	
 	class Meta(object):
 		db_table = 'member_card_log'
-		verbose_name = 'member_card_log'
-		verbose_name_plural = 'member_card_log'
 
 class AdClicked(models.Model):
 	"""
@@ -1385,6 +1381,21 @@ class AdClicked(models.Model):
 	
 	class Meta(object):
 		db_table = 'ad_clicked'
-		verbose_name = 'ad_clicked'
-		verbose_name_plural = 'ad_clicked'
 
+
+class MemberCardPayOrder(models.Model):
+	"""
+	会员卡支付订单 duhao
+	"""
+	owner_id = models.IntegerField() #商家id
+	member_id = models.IntegerField() #会员id
+	batch_id = models.CharField(max_length=50, default="") #微众卡批次id
+	order_id = models.CharField(max_length=50) #支付订单id
+	batch_name = models.CharField(max_length=200) #会员卡名称
+	price = models.FloatField(default=0.0)  #支付金额
+	is_paid = models.BooleanField(default=False)  #是否支付成功
+	created_at = models.DateTimeField(auto_now_add=True) #创建时间
+	paid_at = models.DateTimeField(default="") #支付时间
+	
+	class Meta(object):
+		db_table = 'member_card_pay_order'
