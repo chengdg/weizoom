3409e0f3523934f976b6717398ef10de49e798b2
likunlun <likunlun@weizoom.com>
2016-12-08 18:38:59 +0800

    增加分类特殊资质

--- weapp/mall/models.py
+++ weapp/mall/models.py
@@ -3015,6 +3015,17 @@ class ClassificationHasProduct(models.Model):
 		db_table = "mall_classification_has_product"
 
 
+class ClassificationQualification(models.Model):
+	"""
+	商品分类配置的特殊资质
+	"""
+	classification = models.ForeignKey(Classification)
+	name = models.CharField(max_length=48, default='') #资质名称
+	created_at = models.DateTimeField(auto_now_add=True) #创建时间
+
+	class Meta(object):
+		db_table = 'mall_classification_qualification'
+
 
 class SupplierDivideRebateInfo(models.Model):
     """
