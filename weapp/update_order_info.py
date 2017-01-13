4fc596fa3bb40f14cdc9f2cba0f7726071b5ea38
“chengdegong” <chengdegong@weizoom.com>
2017-01-13 19:33:14 +0800

    修改更新数据库的代码

--- weapp/update_order_info.py
+++ weapp/update_order_info.py
@@ -16,8 +16,8 @@ from eaglet.utils.resource_client import Resource
 
 
 ##########################################处理数据更新############################################################
-def update_info(index_start,index_stop):
-    relations = mall_models.OrderHasProduct.objects.filter(id__range=[index_start,index_stop])
+def update_info():
+    relations = mall_models.OrderHasProduct.objects.filter(created_at__gte='2016-12-01 00:00:00')
     product_ids = [r.product_id for r in relations]
     id2product = dict([(product.id, product) for product in mall_models.Product.objects.filter(id__in=product_ids)])
     n = 0
@@ -26,7 +26,7 @@ def update_info(index_start,index_stop):
         product.fill_specific_model(relation.product_model_name)
         l = []
         if product.custom_model_properties:
-            for pro in product.custom_model_properties:
+            for pro in product.custom_model_properties if product.custom_model_properties else []:
                 l.append(pro['property_value'])
         mall_models.OrderHasProduct.objects.filter(id=relation.id).update(
             weight=product.weight,
@@ -39,12 +39,7 @@ def update_info(index_start,index_stop):
 
 
 if __name__ == "__main__":
-    from django.db.models import Max, Min
-    max_id = mall_models.OrderHasProduct.objects.filter(created_at__gte='2016-12-01 00:00:00').aggregate(Max('id'))
-    print max_id,'=============================='
-    min_id = mall_models.OrderHasProduct.objects.filter(created_at__gte='2016-12-01 00:00:00').aggregate(Min('id'))
-    print min_id,'=============================='
-    update_info(int(min_id['id__min']),int(max_id['id__max']))
+    update_info()
     print "=============================done"
 
 ######################################################################################################
