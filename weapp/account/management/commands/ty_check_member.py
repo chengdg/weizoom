5e17ee6463115426f81a22b4f35e219ca2f80d6c
aix <aixiang@weizoom.com>
2017-02-21 17:35:43 +0800

    add check member script

---
+++ weapp/account/management/commands/ty_check_member.py
@@ -0,0 +1,53 @@
+﻿# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+
+from mall.models import *
+from modules.member.models import *
+
+FIRST_LIMIT = 50
+SECOND_LIMIT = 100
+
+class Command(BaseCommand):
+	help = ''
+	args = ''
+	
+	def handle(self, key, **options):
+		relations = TengyiMemberRelation.objects.all()
+		member_ids = [r.member_id for r in relations]
+		member_id2rec_id = {t.member_id: t.recommend_by_member_id for t in relations}
+
+		webapp_users = WebAppUser.objects.filter(member_id__in=member_ids)
+
+		all_webapp_user_ids = [w.id for w in webapp_users]
+
+		webapp_user_id2member_id = {w.id: w.member_id for w in webapp_users}
+
+		orders = Order.objects.filter(webapp_user_id__in=all_webapp_user_ids)
+
+		for order in orders:
+			if order.is_first_order:
+				if order.final_price >= FIRST_LIMIT:
+					member_id = webapp_user_id2member_id[order.webapp_user_id]
+					recommend_by_member_id = member_id2rec_id[member_id]
+					if order.final_price >= SECOND_LIMIT:
+						TengyiMember.create(
+							member_id = member_id,
+							recommend_by_member_id = recommend_by_member_id,
+							level = 2
+						)
+					else:
+						TengyiMember.create(
+							member_id=member_id,
+							recommend_by_member_id=recommend_by_member_id,
+							level=1
+						)
+
+		for rec_member_id in set(member_id2rec_id.values()):
+			if TengyiMemberRebateCycle.objects.filter(member_id=rec_member_id).count() > 0:
+				continue
+
+
+		
+
+
