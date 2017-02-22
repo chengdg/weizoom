10d744c7c1a802e9b59a3e515834e3f5360e8d92
aix <aixiang@weizoom.com>
2017-02-22 17:10:23 +0800

    update script

--- weapp/account/management/commands/ty_check_member.py
+++ weapp/account/management/commands/ty_check_member.py
@@ -13,6 +13,10 @@ class Command(BaseCommand):
 	args = ''
 	
 	def handle(self, key, **options):
+		"""
+		定时检查腾易会员扫码关注的新会员，是否达到星级会员的要求
+		如果达到要求则同时加入到对应的分组中: 一星会员、二星会员
+		"""
 		relations = TengyiMemberRelation.objects.all()
 		member_ids = [r.member_id for r in relations]
 		member_id2rec_id = {t.member_id: t.recommend_by_member_id for t in relations}
@@ -23,22 +27,52 @@ class Command(BaseCommand):
 
 		webapp_user_id2member_id = {w.id: w.member_id for w in webapp_users}
 
+		member_id2webapp_id = {w.member_id: w.webapp_id for w in webapp_users}
+		one_tag_webapp_ids = [m.webapp_id for m in MemberTag.objects.filter(name=u'一星会员')]
+		two_tag_webapp_ids = [m.webapp_id for m in MemberTag.objects.filter(name=u'二星会员')]
+
 		orders = Order.objects.filter(webapp_user_id__in=all_webapp_user_ids)
+
+
 
 		for order in orders:
 			if order.is_first_order:
 				if order.final_price >= FIRST_LIMIT:
 					member_id = webapp_user_id2member_id[order.webapp_user_id]
+					webapp_id = member_id2webapp_id[member_id]
 					recommend_by_member_id = member_id2rec_id[member_id]
 					if order.final_price >= SECOND_LIMIT:
-						TengyiMember.create(
+						TengyiMember.objects.create(
 							member_id = member_id,
 							recommend_by_member_id = recommend_by_member_id,
 							level = 2
 						)
+
+						if webapp_id not in one_tag_webapp_ids:
+							member_tag = MemberTag.objects.create(
+								webapp_id = webapp_id,
+								name = u'一星会员'
+							)
+						else:
+							member_tag = MemberTag.objects.filter(webapp_id=webapp_id, name=u'一星会员').first()
+
+						if member_tag and MemberHasTag.objects.filter(member_id=member_id, member_tag_id=member_tag.id).count() == 0:
+							MemberHasTag.objects.create(member_id=member_id, member_tag_id=member_tag.id)
+
 					else:
-						TengyiMember.create(
+						TengyiMember.objects.create(
 							member_id=member_id,
 							recommend_by_member_id=recommend_by_member_id,
 							level=1
 						)
+
+						if webapp_id not in two_tag_webapp_ids:
+							member_tag = MemberTag.objects.create(
+								webapp_id = webapp_id,
+								name = u'二星会员'
+							)
+						else:
+							member_tag = MemberTag.objects.filter(webapp_id=webapp_id, name=u'二星会员').first()
+
+						if member_tag and MemberHasTag.objects.filter(member_id=member_id, member_tag_id=member_tag.id).count() == 0:
+							MemberHasTag.objects.create(member_id=member_id, member_tag_id=member_tag.id)
