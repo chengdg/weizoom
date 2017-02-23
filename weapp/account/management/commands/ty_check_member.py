41f48433aca708b435cb25f6cc96e2395601b755
aix <aixiang@weizoom.com>
2017-02-23 09:49:10 +0800

    add print

--- weapp/account/management/commands/ty_check_member.py
+++ weapp/account/management/commands/ty_check_member.py
@@ -9,8 +9,8 @@ from eaglet.utils.resource_client import Resource
 from mall.models import *
 from modules.member.models import *
 
-FIRST_LIMIT = 50
-SECOND_LIMIT = 100
+FIRST_LIMIT = 50.0
+SECOND_LIMIT = 100.0
 
 class Command(BaseCommand):
 	help = ''
@@ -37,8 +37,6 @@ class Command(BaseCommand):
 
 		orders = Order.objects.filter(webapp_user_id__in=all_webapp_user_ids)
 
-
-
 		for order in orders:
 			if order.is_first_order:
 				if order.final_price >= FIRST_LIMIT:
@@ -46,30 +44,13 @@ class Command(BaseCommand):
 					webapp_id = member_id2webapp_id[member_id]
 					recommend_by_member_id = member_id2rec_id[member_id]
 					if order.final_price >= SECOND_LIMIT:
+						print 'two star'
 						tengyi_member = TengyiMember.objects.create(
 							member_id = member_id,
 							recommend_by_member_id = recommend_by_member_id,
 							level = 2
 						)
 
-
-						if webapp_id not in one_tag_webapp_ids:
-							member_tag = MemberTag.objects.create(
-								webapp_id = webapp_id,
-								name = u'一星会员'
-							)
-						else:
-							member_tag = MemberTag.objects.filter(webapp_id=webapp_id, name=u'一星会员').first()
-
-						if member_tag and MemberHasTag.objects.filter(member_id=member_id, member_tag_id=member_tag.id).count() == 0:
-							MemberHasTag.objects.create(member_id=member_id, member_tag_id=member_tag.id)
-
-					else:
-						tengyi_member = TengyiMember.objects.create(
-							member_id=member_id,
-							recommend_by_member_id=recommend_by_member_id,
-							level=1
-						)
 
 						if webapp_id not in two_tag_webapp_ids:
 							member_tag = MemberTag.objects.create(
@@ -80,6 +61,27 @@ class Command(BaseCommand):
 							member_tag = MemberTag.objects.filter(webapp_id=webapp_id, name=u'二星会员').first()
 
 						if member_tag and MemberHasTag.objects.filter(member_id=member_id, member_tag_id=member_tag.id).count() == 0:
+							print 'add to two star tag'
+							MemberHasTag.objects.create(member_id=member_id, member_tag_id=member_tag.id)
+
+					else:
+						print 'one star'
+						tengyi_member = TengyiMember.objects.create(
+							member_id = member_id,
+							recommend_by_member_id = recommend_by_member_id,
+							level = 1
+						)
+
+						if webapp_id not in one_tag_webapp_ids:
+							member_tag = MemberTag.objects.create(
+								webapp_id = webapp_id,
+								name = u'一星会员'
+							)
+						else:
+							member_tag = MemberTag.objects.filter(webapp_id=webapp_id, name=u'一星会员').first()
+
+						if member_tag and MemberHasTag.objects.filter(member_id=member_id, member_tag_id=member_tag.id).count() == 0:
+							print 'add to one star tag'
 							MemberHasTag.objects.create(member_id=member_id, member_tag_id=member_tag.id)
 
 					#创建卡
@@ -99,3 +101,4 @@ class Command(BaseCommand):
 						card_number = resp['data']['card_number']
 						tengyi_member.card_number = card_number
 						tengyi_member.save()
+						print 'created card', card_number
