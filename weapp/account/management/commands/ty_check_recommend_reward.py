7fd9c863ea139426675727a94a35252ae4dcb47f
likunlun <likunlun@weizoom.com>
2017-02-27 18:26:25 +0800

    分利返利计算脚本 增加推广返利发放

---
+++ weapp/account/management/commands/ty_check_recommend_reward.py
@@ -0,0 +1,121 @@
+﻿# -*- coding: utf-8 -*-
+
+__author__ = 'jiangzhe'
+
+import time
+from datetime import datetime, timedelta
+import array
+
+from django.core.management.base import BaseCommand, CommandError
+
+from utils import cache_util
+from bson import json_util
+import json
+
+from mall.models import *
+from modules.member.models import *
+
+date_fmt = "%Y-%m-%d"
+LEVEL_INFO = {
+	1: {
+		'order_money': 50,
+		'consumer_rebates': 20
+	}, 
+	2: {
+		'order_money': 100,
+		'consumer_rebates': 40
+	},
+}
+
+class Command(BaseCommand):
+	help = "json.dumps cached value"
+	args = ''
+	
+	def handle(self, today_str='today', **options):
+		print '===service start==='
+		all_tengyi_members = TengyiMember.objects.all()
+		member_id2level = dict([(m.member_id, m.level) for m in all_tengyi_members])
+		member_id2recommend_by_member_id = dict([(m.member_id, m.recommend_by_member_id) for m in all_tengyi_members])
+		member_id2card_number = dict([(m.member_id, m.card_number) for m in all_tengyi_members])
+
+		if today_str == 'today':
+			today = datetime.today().date()
+		else:
+			today = datetime.strptime(today_str, date_fmt).date()
+		#计算会员的推荐返利
+		#每月1号去结算上月所有推荐人的返利情况
+		this_month_first_day = today.replace(day=1)
+		last_month_last_day = this_month_first_day - timedelta(1)
+		last_month_first_day = last_month_last_day.replace(day=1)
+		recommend_members = TengyiMember.objects.exclude(recommend_by_member_id=0)
+		#获取所有上线（有推荐）会员
+		recommend_member_ids = set([m.recommend_by_member_id for m in recommend_members])
+		print 'need all recommend_member count', len(recommend_member_ids)
+		for recommend_member_id in recommend_member_ids:
+			#获取上线的信息
+			tengyi_member = TengyiMember.objects.get(member_id=recommend_member_id)
+			cur_member_id = tengyi_member.member_id
+			cur_member_created_at = tengyi_member.created_at
+
+			if tengyi_member.level == 1:
+				recommend_member_rebate_money = 10
+			#由于不同阶段返款不同，特殊处理
+			elif tengyi_member.level == 2:
+				recommend_member_rebate_money = 10
+				#获取会员入会6个月后的日期
+				after_six_month_first_day = (cur_member_created_at.date() + timedelta(6*31)).replace(day=1)
+				if after_six_month_first_day <= this_month_first_day:
+					recommend_member_rebate_money = 20
+			else:
+				recommend_member_rebate_money = 0
+
+			print '>>>cur_member_id', cur_member_id
+			#获取上月内所有该返利的人
+			all_recommend_members = TengyiMember.objects.filter(recommend_by_member_id=cur_member_id)
+			all_recommend_member_ids = [m.member_id for m in all_recommend_members]
+			print 'all_recommend_member_ids', all_recommend_member_ids
+			all_recommend_member_rebate_sycle = TengyiMemberRebateCycle.objects.filter(member_id__in=all_recommend_member_ids, receive_reward_at__range=(last_month_first_day, this_month_first_day), is_receive_reward=True, is_recommend_member_receive_reward=False)
+			print 'need recharge',all_recommend_member_rebate_sycle.count()
+			#返利
+			for recommend_member_rebate_sycle in all_recommend_member_rebate_sycle:
+				#标记被推荐人已收到返利
+				recommend_member_rebate_sycle.is_recommend_member_receive_reward = True
+				recommend_member_rebate_sycle.save()
+				#将发奖记录保存在返利记录中
+				TengyiRebateLog.objects.create(
+					member_id = cur_member_id,
+					is_self_order = False,
+					supply_member_id = recommend_member_rebate_sycle.member_id,
+					is_exchanged = False,
+					rebate_money = recommend_member_rebate_money
+					)
+
+
+		#根据未充值的返利记录，最终调用接口充值
+		need_recharge_rebate_logs = TengyiRebateLog.objects.filter(is_exchanged=False)
+		for need_recharge_rebate_log in need_recharge_rebate_logs:
+			member_id = need_recharge_rebate_log.member_id
+			rebate_money = need_recharge_rebate_log.rebate_money
+			supply_member_id = need_recharge_rebate_log.supply_member_id
+			is_self_order = need_recharge_rebate_log.is_self_order
+			card_number = member_id2card_number[member_id]
+
+			if is_self_order:
+				remark = u'购物返利'
+			else:
+				remark = u'推荐返利'
+			# from eaglet.utils.resource_client import Resource
+			# params = {
+			# 	'card_number': card_number,
+			# 	'money': rebate_money,
+			# 	'remark': remark
+			# }
+			# resp = Resource.use('card_apiserver').post({
+			# 	'resource': 'card.recharged_card',
+			# 	'data': params
+			# })
+			# if resp and resp['code'] == 200:
+			# 	print u'为%s充值%d' % (member_id, rebate_money)
+			need_recharge_rebate_log.is_exchanged = True
+			need_recharge_rebate_log.save()
+			print u'为%s充值%d' % (member_id, rebate_money)
--- weapp/account/management/commands/ty_check_reward.py
+++ weapp/account/management/commands/ty_check_reward.py
@@ -49,14 +49,14 @@ class Command(BaseCommand):
 		all_tengyi_member_rebate_sycle = TengyiMemberRebateCycle.objects.filter(is_receive_reward=False, start_time__lt=today)
 		print 'need consumer_rebates count', all_tengyi_member_rebate_sycle.count()
 		for tengyi_member_sycle in all_tengyi_member_rebate_sycle:
-			cur_membser_id = tengyi_member_sycle.member_id
-			cur_webapp_user_id = WebAppUser.objects.get(member_id=cur_membser_id).id
+			cur_member_id = tengyi_member_sycle.member_id
+			cur_webapp_user_id = WebAppUser.objects.get(member_id=cur_member_id).id
 			start_at = tengyi_member_sycle.start_time.date()
 			end_at = tengyi_member_sycle.end_time.date() #查订单时候需要把截止日期后延一天
 			#获取区间内下单、状态为已支付、待发货、已发货、已完成的订单
 			orders = Order.objects.filter(webapp_user_id=cur_webapp_user_id, created_at__range=(start_at, end_at), status__in=[3,4,5], origin_order_id__lte=0, payment_time__lt=seven_days_ago_date)
 			order_ids = list(orders.values_list('order_id', flat=True))
-			print '>>>cur_membser_id', cur_membser_id
+			print '>>>cur_member_id', cur_member_id
 			print 'order_ids',order_ids
 			# #检查订单支付时间是否是7天前完成的
 			# status_logs = OrderStatusLog.objects.filter(order_id__in=order_ids, to_status=ORDER_STATUS_PAYED_NOT_SHIP, created_at__lt=seven_days_ago_date)
@@ -68,7 +68,7 @@ class Command(BaseCommand):
 			for order in orders:
 				money_sum += order.final_price
 
-			cur_member_level = member_id2level[cur_membser_id]
+			cur_member_level = member_id2level[cur_member_id]
 			order_money = LEVEL_INFO[cur_member_level]['order_money']
 			consumer_rebates = LEVEL_INFO[cur_member_level]['consumer_rebates']
 			#TODO特殊处理 需改进
@@ -82,7 +82,7 @@ class Command(BaseCommand):
 				tengyi_member_sycle.save()
 				#将发奖记录保存在返利记录中
 				TengyiRebateLog.objects.create(
-					member_id = cur_membser_id,
+					member_id = cur_member_id,
 					is_self_order = True,
 					supply_member_id = 0,
 					is_exchanged = False,
@@ -91,38 +91,6 @@ class Command(BaseCommand):
 
 		print '===end of consumer_rebates==='
 
-		#计算会员的推荐返利
-		#因：需求是结算周期的后一天去结算周期内所有推荐人的返利情况
-		#故：统计当前日期的前一天和cycle中end_time在同一天的会员的所有推荐者的信息
-		all_tengyi_member_rebate_sycle = TengyiMemberRebateCycle.objects.filter(end_time__startswith=yesterday)
-		# member_ids = list(all_tengyi_member_rebate_sycle.values_list('member_id', flat=True))
-		print 'need recommend_member_rebate count', all_tengyi_member_rebate_sycle.count()
-		for tengyi_member_sycle in all_tengyi_member_rebate_sycle:
-			cur_membre_id = tengyi_member_sycle.member_id
-			print '>>>cur_membre_id', cur_membre_id
-			recommend_member_rebate_money = tengyi_member_sycle.recommend_member_rebate_money
-			#获取当前会员推广的所有会员
-			all_recommend_members = TengyiMember.objects.filter(recommend_by_member_id=cur_membre_id)
-			all_recommend_member_ids = [m.member_id for m in all_recommend_members]
-			print 'all_recommend_member_ids', all_recommend_member_ids
-			#获取在账期内所有的获得购物返利的推广会员
-			start_at = tengyi_member_sycle.start_time.date()
-			end_at = yesterday
-			all_recommend_member_rebate_sycle = TengyiMemberRebateCycle.objects.filter(member_id__in=all_recommend_member_ids, receive_reward_at__range=(start_at, end_at), is_receive_reward=True, is_recommend_member_receive_reward=False)
-			print 'need recharge',all_recommend_member_rebate_sycle.count()
-			#返利
-			for recommend_member_rebate_sycle in all_recommend_member_rebate_sycle:
-				#标记被推荐人已收到返利
-				recommend_member_rebate_sycle.is_recommend_member_receive_reward = True
-				recommend_member_rebate_sycle.save()
-				#将发奖记录保存在返利记录中
-				TengyiRebateLog.objects.create(
-					member_id = cur_membre_id,
-					is_self_order = False,
-					supply_member_id = recommend_member_rebate_sycle.member_id,
-					is_exchanged = False,
-					rebate_money = recommend_member_rebate_money
-					)
 
 		#根据未充值的返利记录，最终调用接口充值
 		need_recharge_rebate_logs = TengyiRebateLog.objects.filter(is_exchanged=False)
@@ -132,7 +100,7 @@ class Command(BaseCommand):
 			supply_member_id = need_recharge_rebate_log.supply_member_id
 			is_self_order = need_recharge_rebate_log.is_self_order
 			card_number = member_id2card_number[member_id]
-			# card_number = '111000006'
+
 			if is_self_order:
 				remark = u'购物返利'
 			else:
