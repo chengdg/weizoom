4a7df0d9e7e694fc77f9c41a737e150d5aafb8ca
duhao <duhao@weizoom.com>
2017-03-27 18:59:43 +0800

    修正拼写错误

--- weapp/third_party_pay/models.py
+++ weapp/third_party_pay/models.py
@@ -17,8 +17,8 @@ class JinGeCard(models.Model):
 	card_number = models.CharField(max_length=20, default='')  #卡号
 	card_password = models.CharField(max_length=256, default='')  #RSA加密后的密码
 	token = models.CharField(max_length=256, default='')  #用户在饭卡数据系统中的身份标识
-	name = models.CharField(max_length=32)  #持卡人姓名
-	company = models.CharField(max_length=64)  #持卡人公司名称
+	name = models.CharField(max_length=32, default='')  #持卡人姓名
+	company = models.CharField(max_length=64, default='')  #持卡人公司名称
 	is_deleted = models.BooleanField(default=False) #是否删除/解绑
 	created_at = models.DateTimeField(auto_now_add=True) #添加时间
 
@@ -32,7 +32,7 @@ class JinGeCardLog(models.Model):
 	"""
 	锦歌饭卡交易记录
 	"""
-	jinge_card = models.ForeignKey(JenGeCard)  #jinge card id
+	jinge_card = models.ForeignKey(JinGeCard)  #jinge card id
 	price = models.FloatField(default=0.0)  # 浮动金额
 	trade_id = models.CharField(max_length=50, default='') #支付流水号
 	order_id = models.CharField(max_length=32, default='') #订单号
